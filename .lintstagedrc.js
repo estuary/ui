e83483c542ebf7a6c73f744cd2f97586cf839cd9
Travis Jenkins <travis@estuary.dev>
2021-11-05 12:21:20 -0400

    removing unused files

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    '*.{js,jsx,ts,tsx}': [
-        'eslint --max-warnings=0',
-        'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
-        () => 'tsc-files --noEmit',
-    ],
-    '*.{js,jsx,ts,tsx,json,js,md}': ['prettier --write'],
-};
