a9d9ad1549a10d073d313c31ca175cbdadd1f123
Travis Jenkins <travis@estuary.dev>
2021-11-09 16:21:32 -0500

    Fetching types

--- src/components/endpointCreation/SourceTypeSelect.tsx
+++ src/components/endpointCreation/SourceTypeSelect.tsx
@@ -1,64 +1,93 @@
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
 const SourceTypePropTypes = {
     id: PropTypes.string.isRequired,
+    onSourceChange: PropTypes.func.isRequired,
+    type: PropTypes.string.isRequired,
 };
 SourceTypeSelect.propTypes = SourceTypePropTypes;
 type SourceTypeProps = PropTypes.InferProps<typeof SourceTypePropTypes>;
 
-const sourceTypes = [
-    {
-        label: 'Iterable',
-        key: 'prefix_iterable_source_v-1.1.1',
-    },
-    {
-        label: 'Google Sheets',
-        key: 'prefix_Google-Sheets_source_v-1.1.1',
-    },
-    {
-        label: 'MySQL',
-        key: 'prefix_my-sql_source_v-1.1.1',
-    },
-    {
-        label: 'Shopify',
-        key: 'prefix_Shopify_source_v-1.1.1',
-    },
-    {
-        label: 'Lorem',
-        key: 'prefix_Lorem_source_v-1.1.1',
-    },
-    {
-        label: 'Ipsum',
-        key: 'prefix_Ipsum_source_v-1.1.1',
-    },
-];
-
 function SourceTypeSelect(props: SourceTypeProps) {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [sourceTypes, setSourceTypes] = useState([]);
+
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+        fetch('http://localhost:3001/source/all')
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setSourceTypes(result);
+                },
+                (error) => {
+                    console.warn(
+                        'There was an issue fetching the Source Types',
+                        error.stack
+                    );
+                    setIsLoaded(true);
+                    setError(error.message);
+                }
+            );
+    }, []);
+
     return (
-        <Autocomplete
-            id={props.id}
-            sx={{ width: 300 }}
-            options={sourceTypes}
-            autoHighlight
-            getOptionLabel={(option) => option.label}
-            renderOption={(props, option) => (
-                <Box component="li" {...props}>
-                    {option.label}
+        <>
+            <Autocomplete
+                id={props.id}
+                sx={{ width: 300 }}
+                options={sourceTypes}
+                autoHighlight
+                openOnFocus
+                blurOnSelect="mouse"
+                noOptionsText={
+                    error !== null
+                        ? 'Unable to fetch source types'
+                        : 'No Options'
+                }
+                loading={!isLoaded}
+                onChange={function (event, reason: any) {
+                    if (reason !== null) {
+                        const key = reason.key;
+                        props.onSourceChange(key);
+                    } else {
+                        props.onSourceChange('');
+                    }
+                }}
+                renderOption={(props, option: any) => (
+                    <Box component="li" {...props}>
+                        {option.label}
+                    </Box>
+                )}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Source type"
+                        error={error !== null}
+                        inputProps={{
+                            ...params.inputProps,
+                            autoComplete: 'new-password', // disable autocomplete and autofill
+                        }}
+                    />
+                )}
+            />
+
+            {error !== null ? (
+                <Box border="solid" p={2}>
+                    <Typography variant="subtitle1">
+                        Whoops! There was an error. Maybe this will help:
+                    </Typography>
+                    <Typography variant="body2"> {error} </Typography>
                 </Box>
-            )}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label="Source type"
-                    inputProps={{
-                        ...params.inputProps,
-                        autoComplete: 'new-password', // disable autocomplete and autofill
-                    }}
-                />
-            )}
-        />
+            ) : null}
+        </>
     );
 }
 
