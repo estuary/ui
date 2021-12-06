a06a42f8b383c587c2eadc6d39e2d09fc02e7cc6
Travis Jenkins <travis@estuary.dev>
2021-12-06 10:52:31 -0500

    typo

--- src/forms/renderers/EstuaryInputControl.tsx
+++ src/forms/renderers/EstuaryInputControl.tsx
@@ -11,7 +11,7 @@ import { Grid } from '@mui/material';
 import { DescriptionWithLinks } from './DescriptionWithLinks';
 const { MaterialTextControl } = Unwrapped;
 
-// TODO - none of this is usable yet. Will need to try agai nonce MUI 5 update is done? Maybe?
+// TODO - none of this is usable yet. Will need to try again once MUI 5 update is done? Maybe?
 export const estuaryInputControl = (props: ControlProps) => {
     return (
         <Grid container>
