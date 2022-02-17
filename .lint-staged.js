f74654f5b636c3a0932bbb3e8285789dd9d8df95
travjenkins <travis@estuary.dev>
2022-02-17 15:59:29 -0500

    Updating actions

--- .lint-staged.js
+++ .lint-staged.js
@@ -2,7 +2,7 @@ module.exports = {
     '*.{js,jsx,ts,tsx,json}': [
         'npm run format',
         'npm run lint --max-errors=0',
+        'npm run typecheck',
         'react-app-rewired test --bail --watchAll=false --findRelatedTests --passWithNoTests',
-        () => 'tsc-files --noEmit',
     ],
 };
