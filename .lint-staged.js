dcf6d9239c04d96f3aef1e2216be79300dd8388f
travjenkins <travis@estuary.dev>
2022-02-17 16:02:38 -0500

    fix linting issues on commit

--- .lint-staged.js
+++ .lint-staged.js
@@ -1,7 +1,7 @@
 module.exports = {
     '*.{js,jsx,ts,tsx,json}': [
         'npm run format',
-        'npm run lint --max-errors=0',
+        'npm run lint:fix --max-errors=0',
         'npm run typecheck',
         'react-app-rewired test --bail --watchAll=false --findRelatedTests --passWithNoTests',
     ],
