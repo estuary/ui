019437c1076eb73aa02519f08be723dc87ff26f0
travjenkins <travis@estuary.dev>
2022-02-17 15:32:02 -0500

    Removing duplicate prettier

--- .lint-staged.js
+++ .lint-staged.js
@@ -1,9 +1,8 @@
 module.exports = {
-    '*.{js,jsx,ts,tsx}': [
+    '*.{js,jsx,ts,tsx,json}': [
         'npm run format',
         'npm run lint --max-errors=0',
         'react-app-rewired test --bail --watchAll=false --findRelatedTests --passWithNoTests',
         () => 'tsc-files --noEmit',
     ],
-    '*.{js,jsx,ts,tsx,json}': ['prettier --write'],
 };
