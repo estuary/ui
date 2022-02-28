7cfd08b722b5ee843e8de6e9cfde9ebd9fa345c6
kiahna-tucker <tucker.kiahna@gmail.com>
2022-02-28 09:16:32 -0500

    Create foundation for state persistence

--- src/stores/ChangeSetStore.ts
+++ src/stores/ChangeSetStore.ts
@@ -1,27 +1,41 @@
 import create from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 
 // TODO: Create a distinct capture state slice that is spread into the change set store.
-
 export interface CaptureState<T = any> {
     addCapture: (key: string, newCapture: T) => void;
     captures: { [key: string]: T };
+    hydrateCaptureState: () => void;
 }
 
+const name = 'change-set-state';
+
+// TODO: Look into a better way to hydrate the state.
 const useChangeSetStore = create<CaptureState>(
     devtools(
-        (set) => ({
-            addCapture: (key, newCapture) =>
-                set(
-                    (state) => ({
-                        captures: { ...state.captures, [key]: newCapture },
-                    }),
-                    false,
-                    'New Capture Added'
-                ),
-            captures: {},
-        }),
-        { name: 'ChangeSetState' }
+        persist(
+            (set, get) => ({
+                addCapture: (key, newCapture) =>
+                    set(
+                        (state) => ({
+                            captures: { ...state.captures, [key]: newCapture },
+                        }),
+                        false,
+                        'New Capture Added'
+                    ),
+                captures: {},
+                hydrateCaptureState: () =>
+                    set(
+                        (state) => ({
+                            captures: { ...state.captures, ...get().captures },
+                        }),
+                        false,
+                        'Capture State Hydrated'
+                    ),
+            }),
+            { name }
+        ),
+        { name }
     )
 );
 
