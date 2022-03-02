bc7271f60f85f975d8e16efaa35ceae900b399ca
travjenkins <travis@estuary.dev>
2022-03-02 16:10:07 -0500

    Removing pre push and making pre commit smaller

--- .lint-staged-push.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    '*.{js,jsx,ts,tsx,json}': [
-        'prettier --write',
-        'eslint --fix',
-        () => 'tsc-files --noEmit',
-        'react-app-rewired test --bail --watchAll=false --findRelatedTests --passWithNoTests',
-    ],
-};
