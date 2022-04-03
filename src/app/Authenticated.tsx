75ae339fb6f8595d58f6a96801491d444e1e1ca1
Johnny <johnny@estuary.dev>
2022-04-03 01:19:55 +0000

    add supabase-swr and use it for the ConnectorsTable admin component

--- src/app/Authenticated.tsx
+++ src/app/Authenticated.tsx
@@ -3,6 +3,8 @@ import AppLayout from 'AppLayout';
 import Home from 'pages/Home';
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router';
+import { supabase } from 'services/supabase';
+import { SwrSupabaseContext } from 'supabase-swr';
 import Error from '../pages/Error';
 
 const Admin = lazy(() => import('../pages/Admin'));
@@ -22,31 +24,33 @@ const Collections = lazy(() => import('../pages/Collections'));
 const Authenticated = () => {
     return (
         <Suspense fallback={<Skeleton animation="wave" />}>
-            <Routes>
-                <Route element={<AppLayout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/test/builds" element={<Builds />} />
-                    <Route path="/dashboard" element={<Home />} />
-                    <Route path="collections" element={<Collections />} />
-                    <Route path="captures" element={<Captures />} />
-                    <Route path="capture">
-                        <Route path="create" element={<CaptureCreate />} />
-                    </Route>
+            <SwrSupabaseContext.Provider value={supabase}>
+                <Routes>
+                    <Route element={<AppLayout />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/test/builds" element={<Builds />} />
+                        <Route path="/dashboard" element={<Home />} />
+                        <Route path="collections" element={<Collections />} />
+                        <Route path="captures" element={<Captures />} />
+                        <Route path="capture">
+                            <Route path="create" element={<CaptureCreate />} />
+                        </Route>
 
-                    <Route
-                        path="materializations"
-                        element={<Materializations />}
-                    />
-                    <Route path="materialization">
                         <Route
-                            path="create"
-                            element={<MaterializationCreate />}
+                            path="materializations"
+                            element={<Materializations />}
                         />
+                        <Route path="materialization">
+                            <Route
+                                path="create"
+                                element={<MaterializationCreate />}
+                            />
+                        </Route>
+                        <Route path="admin/*" element={<Admin />} />
                     </Route>
-                    <Route path="admin/*" element={<Admin />} />
-                </Route>
-                <Route path="*" element={<Error />} />
-            </Routes>
+                    <Route path="*" element={<Error />} />
+                </Routes>
+            </SwrSupabaseContext.Provider>
         </Suspense>
     );
 };
