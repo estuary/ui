105714be2994df59695dbcbf724564eddafe4df9
travjenkins <travis@estuary.dev>
2022-04-19 10:50:44 -0400

    Removing lazy loading for now

--- src/app/Authenticated.tsx
+++ src/app/Authenticated.tsx
@@ -1,5 +1,4 @@
 import { Collections } from '@mui/icons-material';
-import { Skeleton } from '@mui/material';
 import AppLayout from 'AppLayout';
 import CaptureCreate from 'components/capture/create';
 import CaptureEdit from 'components/capture/edit';
@@ -10,7 +9,6 @@ import Captures from 'pages/Captures';
 import PageNotFound from 'pages/error/PageNotFound';
 import Home from 'pages/Home';
 import Materializations from 'pages/Materializations';
-import { Suspense } from 'react';
 import { Route, Routes } from 'react-router';
 
 export const homeRoute = {
@@ -70,47 +68,42 @@ export const materializationsRoute = {
 
 const Authenticated = () => {
     return (
-        <Suspense fallback={<Skeleton animation="wave" />}>
-            <Routes>
-                <Route element={<AppLayout />}>
-                    <Route path={homeRoute.path} element={<Home />} />
-                    <Route path={buildsRoute.path} element={<Builds />} />
+        <Routes>
+            <Route element={<AppLayout />}>
+                <Route path={homeRoute.path} element={<Home />} />
+                <Route path={buildsRoute.path} element={<Builds />} />
+                <Route path={collectionsRoute.path} element={<Collections />} />
+
+                <Route path={capturesRoute.path} element={<Captures />} />
+                <Route path={captureRoute.root}>
                     <Route
-                        path={collectionsRoute.path}
-                        element={<Collections />}
+                        path={captureRoute.create.path}
+                        element={<CaptureCreate />}
                     />
-
-                    <Route path={capturesRoute.path} element={<Captures />} />
-                    <Route path={captureRoute.root}>
-                        <Route
-                            path={captureRoute.create.path}
-                            element={<CaptureCreate />}
-                        />
-                        <Route
-                            path={captureRoute.edit.path}
-                            element={<CaptureEdit />}
-                        />
-                    </Route>
-
                     <Route
-                        path={materializationsRoute.path}
-                        element={<Materializations />}
-                    />
-                    <Route path={materializationRoute.root}>
-                        <Route
-                            path={materializationRoute.create.path}
-                            element={<NewMaterialization />}
-                        />
-                    </Route>
-
-                    <Route path={adminRoute.path} element={<Admin />} />
-                    <Route
-                        path={pageNotFoundRoute.path}
-                        element={<PageNotFound />}
+                        path={captureRoute.edit.path}
+                        element={<CaptureEdit />}
                     />
                 </Route>
-            </Routes>
-        </Suspense>
+
+                <Route
+                    path={materializationsRoute.path}
+                    element={<Materializations />}
+                />
+                <Route path={materializationRoute.root}>
+                    <Route
+                        path={materializationRoute.create.path}
+                        element={<NewMaterialization />}
+                    />
+                </Route>
+
+                <Route path={adminRoute.path} element={<Admin />} />
+                <Route
+                    path={pageNotFoundRoute.path}
+                    element={<PageNotFound />}
+                />
+            </Route>
+        </Routes>
     );
 };
 
