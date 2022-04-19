02aadffdbb536798664643c0d34967e9d8b49d92
travjenkins <travis@estuary.dev>
2022-04-19 11:31:12 -0400

    not used

--- src/services/routes.ts
+++
@@ -1,6 +0,0 @@
-export const TITLE_REPLACEMENT = '%s%';
-export const TITLE_TEMPLATE = `Estuary Flow ${TITLE_REPLACEMENT}`;
-
-export const getTitle = (title: string) => {
-    return TITLE_TEMPLATE.replace(TITLE_REPLACEMENT, title);
-};
