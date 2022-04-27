1cc473d3af075ea68ef388f2f11ebdb39ef3f19a
Travis Jenkins <travis@estuary.dev>
2022-04-27 12:22:23 -0400

    starting on custom renderer. not working.

--- src/forms/renderers/Connectors.tsx
+++ src/forms/renderers/Connectors.tsx
@@ -1,16 +1,54 @@
-import { RankedTester, rankWith, schemaTypeIs } from '@jsonforms/core';
+import { RankedTester, rankWith, scopeEndsWith } from '@jsonforms/core';
 import { withJsonFormsLayoutProps } from '@jsonforms/react';
-import { Hidden } from '@mui/material';
+import { Autocomplete, TextField } from '@mui/material';
+
+const SCOPE = 'image';
 
 export const connectorTypeTester: RankedTester = rankWith(
     999,
-    schemaTypeIs('null')
+    scopeEndsWith(SCOPE)
 );
 
 // This is blank on purpose. For right now we can just show null settings are nothing
-const ConnectorTypeRenderer = (props: any) => {
-    const { visible } = props;
-    return <Hidden xsUp={!visible}> </Hidden>;
+const ConnectorTypeRenderer = ({
+    handleChange,
+    id,
+    data,
+    path,
+    schema,
+    uischema,
+}: any) => {
+    return (
+        <Autocomplete
+            options={schema.properties.image.oneOf}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    id={id}
+                    label={uischema.label}
+                    required={schema.required.includes(SCOPE)}
+                    inputProps={{
+                        ...params.inputProps,
+                        autoComplete: 'off',
+                    }}
+                />
+            )}
+            value={data}
+            onChange={(e: any) => handleChange(path, e.target.value)}
+            onSelect={(value) => handleChange(path, value)}
+
+            // renderItem={(item: any, isHighlighted: any) => (
+            //     <div
+            //         key={item}
+            //         style={{
+            //             background: isHighlighted ? 'lightgray' : 'white',
+            //         }}
+            //     >
+            //         {item}
+            //     </div>
+            // )}
+        />
+    );
 };
 
 export const ConnectorType = withJsonFormsLayoutProps(ConnectorTypeRenderer);
