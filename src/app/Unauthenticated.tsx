73b7490d7bdb5970a6e820d0605e53233d83b2c4
Travis Jenkins <travis@estuary.dev>
2022-05-03 14:22:15 -0400

    Don't dynamic load login page so not needed.

--- src/app/Unauthenticated.tsx
+++ src/app/Unauthenticated.tsx
@@ -1,15 +1,11 @@
-import { Skeleton } from '@mui/material';
 import Login from 'pages/Login';
-import { Suspense } from 'react';
 import { Route, Routes } from 'react-router';
 
 const Unauthenticated = () => {
     return (
-        <Suspense fallback={<Skeleton animation="wave" />}>
-            <Routes>
-                <Route path="*" element={<Login />} />
-            </Routes>
-        </Suspense>
+        <Routes>
+            <Route path="*" element={<Login />} />
+        </Routes>
     );
 };
 
