23eeb3bcf43a588df8b8ca7777e2c4269deb944f
Travis Jenkins <travis@estuary.dev>
2022-05-04 15:23:45 -0400

    Renaming foo to Entity. Now much better name but a little closer to the final name.

--- src/components/shared/Entity/DetailsForm.tsx
+++ src/components/shared/Entity/DetailsForm.tsx
@@ -2,11 +2,11 @@ import { materialCells } from '@jsonforms/material-renderers';
 import { JsonForms } from '@jsonforms/react';
 import { Alert, Stack, Typography } from '@mui/material';
 import { routeDetails } from 'app/Authenticated';
-import { ConnectorTag } from 'components/shared/foo/query';
+import { ConnectorTag } from 'components/shared/Entity/query';
 import useFooState, {
     EntityStoreState,
     FormStatus,
-} from 'components/shared/foo/Store';
+} from 'components/shared/Entity/Store';
 import { useEffect, useMemo } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { useSearchParams } from 'react-router-dom';
