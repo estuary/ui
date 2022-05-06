d0a74f77ac45b275abec3a4321b3780f390ac4ea
Phil <phil@estuary.dev>
2022-05-05 23:58:54 -0400

    Allow rendering JSON forms from wacky schemas
    The EndpointConfigForm and ResourceConfigForm previously would barf
    whenever they encountered a non-compliant JSON schema. This commit makes
    the AJV validator much more permissive in the schemas that it will
    accept.
    
    This also removes the `json-refs` dependency and assumes that all json
    schemas are fully self-contained. This property is ensured by the build
    process for flow collection schemas. For endpoint and resource schemas,
    it seems to also generally hold.

--- src/components/materialization/ResourceConfigForm.tsx
+++ src/components/materialization/ResourceConfigForm.tsx
@@ -1,4 +1,3 @@
-import { createAjv } from '@jsonforms/core';
 import { materialCells } from '@jsonforms/material-renderers';
 import { JsonForms } from '@jsonforms/react';
 import { StyledEngineProvider } from '@mui/material';
@@ -9,9 +8,12 @@ import useEntityStore, {
     fooSelectors,
     FormStatus,
 } from 'components/shared/Entity/Store';
-import JsonRefs from 'json-refs';
+import {
+    createJSONFormDefaults,
+    setDefaultsValidator,
+} from 'components/shared/Entity/EndpointConfigForm';
 import { isEmpty } from 'lodash';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import {
     defaultOptions,
     defaultRenderers,
@@ -24,8 +26,6 @@ type Props = {
     resourceSchema: any;
 };
 
-const defaultAjv = createAjv({ useDefaults: true });
-
 const stateSelectors: StoreSelector<CreationState> = {
     formData: (state) => state.resourceConfig.data,
     setConfig: (state) => state.setResourceConfig,
@@ -37,63 +37,42 @@ function NewMaterializationResourceConfigForm({ resourceSchema }: Props) {
     const displayValidation = useEntityStore(fooSelectors.displayValidation);
     const status = useEntityStore(fooSelectors.formStateStatus);
 
-    const [deReffedSchema, setDeReffedSchema] = useState<any | null>(null);
-
     // Resolve Refs & Hydrate the object
     //  This will hydrate the default values for us as we don't want JSONForms to
     //  directly update the state object as it caused issues when switching connectors.
     useEffect(() => {
-        async function resolveSchemaRefs(endpointResponse: any) {
-            const processedSchema = await JsonRefs.resolveRefs(
-                endpointResponse
-            );
+        setConfig({
+            data: createJSONFormDefaults(resourceSchema),
+        });
+    }, [resourceSchema, setConfig]);
 
-            const hydrateAndValidate = defaultAjv.compile(
-                processedSchema.resolved
-            );
-            const defaultValues = {};
+    const uiSchema = generateCustomUISchema(resourceSchema);
+    const showValidationVal = showValidation(displayValidation);
 
-            hydrateAndValidate(defaultValues);
+    const handlers = {
+        onChange: (form: any) => {
+            if (!isEmpty(form.data)) {
+                setConfig(form);
+            }
+        },
+    };
 
-            setDeReffedSchema(processedSchema.resolved);
-            setConfig({
-                data: defaultValues,
-            });
-        }
-
-        void resolveSchemaRefs(resourceSchema);
-    }, [resourceSchema, setConfig, setDeReffedSchema]);
-
-    if (deReffedSchema) {
-        const uiSchema = generateCustomUISchema(deReffedSchema);
-        const showValidationVal = showValidation(displayValidation);
-
-        const handlers = {
-            onChange: (form: any) => {
-                if (!isEmpty(form.data)) {
-                    setConfig(form);
-                }
-            },
-        };
-
-        return (
-            <StyledEngineProvider injectFirst>
-                <JsonForms
-                    schema={deReffedSchema}
-                    uischema={uiSchema}
-                    data={formData}
-                    renderers={defaultRenderers}
-                    cells={materialCells}
-                    config={defaultOptions}
-                    readonly={status !== FormStatus.IDLE}
-                    validationMode={showValidationVal}
-                    onChange={handlers.onChange}
-                />
-            </StyledEngineProvider>
-        );
-    } else {
-        return null;
-    }
+    return (
+        <StyledEngineProvider injectFirst>
+            <JsonForms
+                schema={resourceSchema}
+                uischema={uiSchema}
+                data={formData}
+                renderers={defaultRenderers}
+                cells={materialCells}
+                config={defaultOptions}
+                readonly={status !== FormStatus.IDLE}
+                validationMode={showValidationVal}
+                onChange={handlers.onChange}
+                ajv={setDefaultsValidator}
+            />
+        </StyledEngineProvider>
+    );
 }
 
 export default NewMaterializationResourceConfigForm;
