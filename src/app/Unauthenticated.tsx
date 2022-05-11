4885fea07c15c0979240908535c316066be7c86a
Travis Jenkins <travis@estuary.dev>
2022-05-10 22:54:01 -0400

    Adding a logout path

--- src/app/Unauthenticated.tsx
+++ src/app/Unauthenticated.tsx
@@ -1,9 +1,20 @@
 import Login from 'pages/Login';
 import { Route, Routes } from 'react-router';
 
+export const logoutRoutes = {
+    path: 'logout',
+    params: {
+        reason: 'reason',
+    },
+};
+
 const Unauthenticated = () => {
     return (
         <Routes>
+            <Route
+                path={`${logoutRoutes.path}:${logoutRoutes.params.reason}`}
+                element={<Login />}
+            />
             <Route path="*" element={<Login />} />
         </Routes>
     );
