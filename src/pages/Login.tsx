bcf05014a8c4109e46b8690ce4d97447f201212b
Kiahna Tucker <77648584+kiahna-tucker@users.noreply.github.com>
2022-05-12 15:21:45 -0400

    Update Entry Page Styling (#128)
    * Update the styling of the sign in page
    
    * Rename image folder
    
    * Remove logo svg
    
    * Correct layout adjustment
    
    * Export base colors
    
    * Changes requested in PR thread
    
    * Update registration page styling
    
    * Swap github and google btns
    
    * Merge with main
    
    * Update expected login test string

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,13 @@
-import {
-    Alert,
-    Card,
-    CardContent,
-    Grid,
-    Snackbar,
-    Typography,
-} from '@mui/material';
+import { Alert, Box, Paper, Snackbar, Typography } from '@mui/material';
 import { Auth } from '@supabase/ui';
 import { logoutRoutes } from 'app/Unauthenticated';
-import Topbar from 'components/header/Topbar';
 import { useClient } from 'hooks/supabase-swr';
 import useBrowserTitle from 'hooks/useBrowserTitle';
 import { FormattedMessage } from 'react-intl';
 import { useSearchParams } from 'react-router-dom';
 import useConstant from 'use-constant';
 import { getLoginSettings } from 'utils/env-utils';
+import lightLogo from '../images/light-horizontal/estuary-logo-light.png';
 
 export enum LogoutReasons {
     JWT = 'jwt_expired',
@@ -33,14 +26,14 @@ const Login = () => {
     const loginSettings = getLoginSettings();
 
     return (
-        <Grid
-            container
-            spacing={0}
-            direction="column"
+        <Box
             sx={{
-                alignItems: 'center',
+                width: '100%',
                 height: '100vh',
+                display: 'flex',
+                alignItems: 'center',
                 justifyContent: 'center',
+                my: 6,
             }}
         >
             <Snackbar
@@ -55,35 +48,50 @@ const Login = () => {
                     Your JWT Token expired. Please login again.
                 </Alert>
             </Snackbar>
-            <Topbar isNavigationOpen={false} />
-            <Grid item xs={3}>
-                <Card elevation={24} sx={{ maxWidth: 400, minHeight: 300 }}>
-                    <CardContent>
-                        <Typography
-                            gutterBottom
-                            variant="h6"
-                            component="h2"
-                            sx={{ paddingTop: '1rem', textAlign: 'center' }}
-                        >
-                            <FormattedMessage id="productName" />
-                        </Typography>
-                    </CardContent>
 
-                    <CardContent>
-                        <FormattedMessage id="login.oidc.message" />
-                    </CardContent>
-                    <CardContent>
-                        <Auth
-                            providers={['google']}
-                            supabaseClient={supabaseClient}
-                            socialColors={true}
-                            onlyThirdPartyProviders={!loginSettings.showEmail}
-                            redirectTo={redirectTo}
-                        />
-                    </CardContent>
-                </Card>
-            </Grid>
-        </Grid>
+            <Paper
+                sx={{
+                    minWidth: 360,
+                    maxHeight: 750,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    p: 3,
+                    background:
+                        'linear-gradient(159.03deg, rgba(172, 199, 220, 0.18) 2.23%, rgba(172, 199, 220, 0.12) 40.69%)',
+                    boxShadow: '0px 4px 24px -1px rgba(0, 0, 0, 0.2)',
+                    borderRadius: 5,
+                }}
+            >
+                <img
+                    src={lightLogo}
+                    style={{ marginBottom: 16 }}
+                    width={200}
+                    alt="Estuary Logo"
+                />
+
+                <Typography sx={{ mb: 5 }}>
+                    <FormattedMessage id="login.oidc.message" />
+                </Typography>
+
+                <Box>
+                    <Auth
+                        providers={['github', 'google']}
+                        supabaseClient={supabaseClient}
+                        socialColors={true}
+                        onlyThirdPartyProviders={!loginSettings.showEmail}
+                        redirectTo={redirectTo}
+                        style={{
+                            minWidth: 310,
+                            padding: 12,
+                            backgroundColor: '#FFFFFF',
+                            borderRadius: 10,
+                        }}
+                    />
+                </Box>
+            </Paper>
+        </Box>
     );
 };
 
