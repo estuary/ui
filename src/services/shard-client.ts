ab2f56cb9beea1f69cc644cab8b15f3ba3efb92f
kiahna-tucker <tucker.kiahna@gmail.com>
2022-05-24 14:27:14 -0400

    Create shard client token-related error flags

--- src/services/shard-client.ts
+++ src/services/shard-client.ts
@@ -2,6 +2,14 @@ import { ShardClient, ShardSelector } from 'data-plane-gateway';
 import { SetShards } from 'stores/ShardDetail';
 import { LiveSpecsExtBaseQuery } from 'types';
 
+// TODO: Follow up on the data plane gateway. These changes should be available on the latest branch as of 2:24PM on 5/24/2022. Confirmation
+// and testing required.
+enum ErrorFlags {
+    TOKEN_NOT_FOUND = 'Unauthenticated',
+    TOKEN_INVALID = 'Authentication failed',
+    OPERATION_INVALID = 'Unauthorized',
+}
+
 const getShardList = <T extends LiveSpecsExtBaseQuery>(
     baseUrl: URL,
     authToken: string,
@@ -15,8 +23,6 @@ const getShardList = <T extends LiveSpecsExtBaseQuery>(
         .map((spec) => spec.catalog_name)
         .forEach((name) => taskSelector.task(name));
 
-    // TODO: Determine how frequently this data should be polled and an approach to do so. This function should be broken out
-    // from the useGatewayAuthToken hook and placed in its own hook driven by useEffect.
     shardClient
         .list(taskSelector)
         .then((result) => {
@@ -26,7 +32,16 @@ const getShardList = <T extends LiveSpecsExtBaseQuery>(
                 setShards(shards);
             }
         })
-        .catch((error) => Promise.reject(error));
+        .catch((error: Error) => {
+            // TODO: Create and call a gateway-auth-token service to generate a new authentication token. The polling done by the useEffect
+            // caller for the shard-client will handle making a follow-up call to the shards list API. This error is currently silent. Should
+            // the user be notified that a service error is present after a certain number of resolution attempts?
+            if (error.message.includes(ErrorFlags.TOKEN_INVALID)) {
+                console.log('Call a service that will update the JWT token');
+            }
+
+            return Promise.reject(error);
+        });
 };
 
 export default getShardList;
