3114c7ef6285a92e628aeb82bd66682e74956a2c
Travis Jenkins <travis@estuary.dev>
2022-05-31 15:45:02 -0400

    Adding hook for tasks

---
+++ src/hooks/tasks/useTasksByDay.ts
@@ -0,0 +1,70 @@
+import { TABLES } from 'services/supabase';
+import { ENTITY } from 'types';
+import { useQuery, useSelect } from '../supabase-swr/';
+
+export interface Meta {
+    uuid: string;
+}
+
+export interface Out {
+    bytesTotal: number;
+    docsTotal: number;
+}
+
+export interface Shard {
+    keyBegin: string;
+    kind: string;
+    name: string;
+    rClockBegin: string;
+}
+
+interface CaptureDetails {
+    out: Out;
+    right: Out;
+}
+
+export interface TaskFlowDocument {
+    _meta: Meta;
+    capture: {
+        [k: string]: CaptureDetails;
+    };
+    openSecondsTotal: number;
+    shard: Shard;
+    ts: Date;
+    txnCount: number;
+}
+
+export interface TasksByDayQuery {
+    name: string;
+    ts: Date;
+    kind: ENTITY;
+    flow_document: TaskFlowDocument;
+}
+
+const TASK_BY_DAY_COLS = ['name', 'ts', 'kind', 'flow_document'];
+const TASK_BY_DAY_QUERY = TASK_BY_DAY_COLS.join(', ');
+
+function useTasksByDay(name: string | null, kind: ENTITY | null) {
+    const taskByDayQuery = useQuery<TasksByDayQuery>(
+        TABLES.TASKS_BY_DAY,
+        {
+            columns: TASK_BY_DAY_QUERY,
+            filter: (query) =>
+                query.eq('name', name as string).eq('kind', kind as string),
+        },
+        [name, kind]
+    );
+
+    const { data, error, mutate, isValidating } = useSelect(
+        name && kind ? taskByDayQuery : null
+    );
+
+    return {
+        tasksStats: data ? data.data : [],
+        error,
+        mutate,
+        isValidating,
+    };
+}
+
+export default useTasksByDay;
