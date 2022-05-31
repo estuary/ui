4aa8fdffbc41ac4bc7a559f9e10b8cd84ee15587
Travis Jenkins <travis@estuary.dev>
2022-05-31 18:11:48 -0400

    Memoing the client

--- src/hooks/useShardsList.ts
+++ src/hooks/useShardsList.ts
@@ -1,6 +1,7 @@
 import { Auth } from '@supabase/ui';
 import { usePreFetchData } from 'context/PreFetchData';
 import { ShardClient, ShardSelector } from 'data-plane-gateway';
+import { useMemo } from 'react';
 import getGatewayAuthConfig from 'services/gateway-auth-config';
 import useSWR from 'swr';
 import { LiveSpecsExtBaseQuery } from 'types';
@@ -25,10 +26,17 @@ const useShardsList = <T extends LiveSpecsExtBaseQuery>(specs: T[]) => {
 
     const gatewayConfig = getStoredGatewayAuthConfig();
 
-    if (gatewayConfig?.gateway_url && gatewayConfig.token && session) {
-        const authToken = gatewayConfig.token;
-        const baseUrl = new URL(gatewayConfig.gateway_url);
-        const shardClient = new ShardClient(baseUrl, authToken);
+    const shardClient = useMemo(() => {
+        if (gatewayConfig?.gateway_url && gatewayConfig.token) {
+            const authToken = gatewayConfig.token;
+            const baseUrl = new URL(gatewayConfig.gateway_url);
+            return new ShardClient(baseUrl, authToken);
+        } else {
+            return null;
+        }
+    }, [gatewayConfig]);
+
+    if (shardClient !== null && session) {
         const taskSelector = new ShardSelector();
 
         specs
@@ -71,11 +79,15 @@ const useShardsList = <T extends LiveSpecsExtBaseQuery>(specs: T[]) => {
                 });
         };
 
-        return useSWR(gatewayConfig.gateway_url, fetcher, {
-            errorRetryInterval: INTERVAL / 2,
-            refreshInterval: INTERVAL,
-            revalidateOnFocus: false, //We're already refreshing and these status do not change often
-        });
+        return useSWR(
+            gatewayConfig?.gateway_url ?? '__missing_gatewway_url__',
+            fetcher,
+            {
+                errorRetryInterval: INTERVAL / 2,
+                refreshInterval: INTERVAL,
+                revalidateOnFocus: false, //We're already refreshing and these status do not change often
+            }
+        );
     } else {
         throw Error('Unable to fetch shards due to missing data');
     }
