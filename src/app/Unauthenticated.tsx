10c2779dba5ab44306fd7cc47f21b819b4109b4a
Travis Jenkins <travis@estuary.dev>
2022-06-09 17:37:37 -0400

    wip: making logout remove the url you are at for now

--- src/app/Unauthenticated.tsx
+++ src/app/Unauthenticated.tsx
@@ -2,13 +2,12 @@ import Login from 'pages/Login';
 import { Route, Routes } from 'react-router';
 
 export const logoutRoutes = {
-    path: 'logout',
+    path: '',
 };
 
 const Unauthenticated = () => {
     return (
         <Routes>
-            <Route path={logoutRoutes.path} element={<Login />} />
             <Route path="*" element={<Login />} />
         </Routes>
     );
--- src/context/User.tsx
+++ src/context/User.tsx
@@ -1,5 +1,6 @@
 import { Auth } from '@supabase/ui';
 import { routeDetails } from 'app/Authenticated';
+import { logoutRoutes } from 'app/Unauthenticated';
 import { useClient } from 'hooks/supabase-swr';
 import { useNavigate } from 'react-router';
 import { useEffectOnce } from 'react-use';
@@ -10,10 +11,11 @@ export const UserProvider = ({ children }: BaseComponentProps) => {
     const navigate = useNavigate();
 
     useEffectOnce(() => {
-        supabaseClient.auth.onAuthStateChange((event, session) => {
-            console.log('Auth state changed', { event, session });
+        supabaseClient.auth.onAuthStateChange((event) => {
             if (event === 'PASSWORD_RECOVERY') {
                 navigate(routeDetails.user.passwordReset.fullPath);
+            } else if (event === 'SIGNED_OUT') {
+                navigate(logoutRoutes.path);
             }
         });
     });
