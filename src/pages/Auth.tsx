ebb5e3d7c47400c3cd3d2165e1aa03b12f8b4e15
Travis Jenkins <travis@estuary.dev>
2022-06-10 12:53:45 -0400

    wip: adding comments

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -5,6 +5,11 @@ import useBrowserTitle from 'hooks/useBrowserTitle';
 import { useSnackbar } from 'notistack';
 import { useNavigate } from 'react-router-dom';
 
+// This is the main "controller" for checking auth status for Supabase when coming into the app
+//    - This is used for both OIDC auth AND magic link auth.
+//    - We call their API to check for errors so we don't have to pull that logic into our app.
+//    - The error message Supabase puts in the URL is the one we display... should probably handle custom messages sometime.
+//    - If there are ANY issues logging in we will log the user out.
 const Auth = () => {
     useBrowserTitle('browserTitle.loginLoading');
 
