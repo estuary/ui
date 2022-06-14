18734f335b94204812ad83e74c74421592d92521
kiahna-tucker <tucker.kiahna@gmail.com>
2022-06-14 14:23:21 -0400

    Extend context

--- src/context/Authenticated.tsx
+++ src/context/Authenticated.tsx
@@ -1,12 +1,15 @@
 import ConfirmationModalContextProvider from 'context/Confirmation';
 import { PreFetchDataProvider } from 'context/PreFetchData';
+import { ZustandProvider } from 'hooks/useZustand';
 import { BaseComponentProps } from 'types';
 
 // This is for contexts that should only be added to the app after the user has authenticated
 function AuthenticatedOnlyContext({ children }: BaseComponentProps) {
     return (
         <ConfirmationModalContextProvider>
-            <PreFetchDataProvider>{children}</PreFetchDataProvider>
+            <PreFetchDataProvider>
+                <ZustandProvider>{children}</ZustandProvider>
+            </PreFetchDataProvider>
         </ConfirmationModalContextProvider>
     );
 }
