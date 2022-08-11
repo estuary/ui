a3d7df40d6504ecfd50c2e4ab37c84234946a938
Travis Jenkins <travis@estuary.dev>
2022-08-11 11:03:16 -0400

    init: adding default button, removing in dev warning, and tweaking UX (#281)
    * init: adding default button, removing in dev warning, and tweaking UX
    
    * wip: movin errors, cleaning logs
    
    * wip: remove comments
    
    * fin: messaging, better errors
    
    * fin: better key name

--- src/lang/en-US.ts
+++ src/lang/en-US.ts
@@ -476,7 +476,12 @@ const ShardStatus: ResolvedIntlConfig['messages'] = {
 const OAuth: ResolvedIntlConfig['messages'] = {
     'oauth.instructions': `Authenticate your {provider} account by clicking below. A pop up will open where you can authorize access. No data will be accessed during authorization.`,
     'oauth.fetchAuthURL.error': `We were unable to fetch the proper URL to start OAuth. ${Error['error.tryAgain']}`,
+    'oauth.authentication.denied': `To use OAuth as your authentication you must allow our app access to your {provider} account.`,
     'oauth.accessToken.error': `There was an issue attempting to get the access token from {provider}. ${Error['error.tryAgain']}`,
+    'oauth.authenticated': `Authenticated`,
+    'oauth.unauthenticated': `Not Authenticated`,
+    'oauth.authenticate': `Authenticate your {provider} account`,
+    'oauth.remove': `Remove`,
 };
 
 const enUSMessages: ResolvedIntlConfig['messages'] = {
