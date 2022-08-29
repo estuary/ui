baddea210672d68d4d497acc32bc62bfd088466c
Kiahna Tucker <77648584+kiahna-tucker@users.noreply.github.com>
2022-08-29 15:58:24 -0400

    Add Connector Request Tile to Connectors Page (#302)
    * Add connector request tile to connectors page
    
    * Create connector tile loading skeletons
    
    * Remove unnecessary tile prop
    
    * Enlarge unmatched filter container
    
    * Remove skeleton variant resulting in ts error
    
    * Pull json forms service from main
    
    * Remove json forms changes due to autosave
    
    * pr: put this stuff back in that previously was needing to be removed
    
    Co-authored-by: Travis Jenkins <travis@estuary.dev>

--- src/components/ConnectorTiles.tsx
+++ src/components/ConnectorTiles.tsx
@@ -1,11 +1,13 @@
-import { Cable, OpenInNew } from '@mui/icons-material';
+import { AddBox, Cable, OpenInNew } from '@mui/icons-material';
 import {
     Box,
     Button,
     Grid,
-    LinearProgress,
     Paper,
+    Skeleton,
     Stack,
+    SxProps,
+    Theme,
     Typography,
     useMediaQuery,
     useTheme,
@@ -15,7 +17,6 @@ import ConnectorToolbar from 'components/ConnectorToolbar';
 import {
     darkGlassBkgColor,
     darkGlassBkgColorIntensified,
-    darkGlassBkgWithoutBlur,
     slate,
 } from 'context/Theme';
 import { useQuery, useSelect } from 'hooks/supabase-swr';
@@ -23,8 +24,8 @@ import {
     ConnectorWithTagDetailQuery,
     CONNECTOR_WITH_TAG_QUERY,
 } from 'hooks/useConnectorWithTagDetail';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { FormattedDate, FormattedMessage } from 'react-intl';
+import { ReactNode, useEffect, useMemo, useRef, useState } from 'react';
+import { FormattedDate, FormattedMessage, useIntl } from 'react-intl';
 import { useNavigate } from 'react-router-dom';
 import { CONNECTOR_NAME, defaultTableFilter, TABLES } from 'services/supabase';
 import {
@@ -36,10 +37,14 @@ import {
 import { getPathWithParam, hasLength } from 'utils/misc-utils';
 import { getEmptyTableHeader, getEmptyTableMessage } from 'utils/table-utils';
 
-interface Props {
+interface ConnectorTilesProps {
     cardWidth: number;
     cardsPerRow: number;
     gridSpacing: number;
+}
+
+interface TileProps {
+    children: ReactNode;
 }
 
 const intlConfig: TableIntlConfig = {
@@ -47,14 +52,63 @@ const intlConfig: TableIntlConfig = {
     message: 'connectors.main.message2',
 };
 
-function ConnectorTile({ cardWidth, cardsPerRow, gridSpacing }: Props) {
+const imageBackgroundSx: SxProps<Theme> = {
+    width: '100%',
+    height: 125,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 2,
+    borderRadius: 5,
+    background: (theme) =>
+        theme.palette.mode === 'dark'
+            ? 'rgba(172, 199, 220, 0.30)' // Brighter than desired to improve MySQL visibility.
+            : slate[25],
+};
+
+function Tile({ children }: TileProps) {
+    return (
+        <Paper
+            elevation={0}
+            sx={{
+                'height': '100%',
+                'borderRadius': 5,
+                'background': (theme) =>
+                    theme.palette.mode === 'dark'
+                        ? darkGlassBkgColor
+                        : slate[50],
+                'padding': 1,
+                '&:hover': {
+                    background: (theme) =>
+                        theme.palette.mode === 'dark'
+                            ? darkGlassBkgColorIntensified
+                            : 'rgba(172, 199, 220, 0.45)',
+                },
+            }}
+        >
+            <Stack
+                style={{
+                    height: '100%',
+                    justifyContent: 'space-between',
+                }}
+            >
+                {children}
+            </Stack>
+        </Paper>
+    );
+}
+
+function ConnectorTiles({
+    cardWidth,
+    cardsPerRow,
+    gridSpacing,
+}: ConnectorTilesProps) {
     const navigate = useNavigate();
     const isFiltering = useRef(false);
+    const intl = useIntl();
 
     const theme = useTheme();
     const belowMd = useMediaQuery(theme.breakpoints.down('md'));
-
-    const { background: darkGlassBkg } = darkGlassBkgWithoutBlur;
 
     const [protocol, setProtocol] = useState<string | null>(null);
     const [searchQuery, setSearchQuery] = useState<string | null>(null);
@@ -99,11 +153,16 @@ function ConnectorTile({ cardWidth, cardsPerRow, gridSpacing }: Props) {
         } else {
             setTableState({ status: TableStatuses.NO_EXISTING_DATA });
         }
-    }, [selectData]);
+    }, [selectData, isValidating]);
 
     const gridContainerWidth = belowMd
         ? '100%'
         : cardWidth * cardsPerRow + 8 * gridSpacing * (cardsPerRow + 1);
+
+    const skeletonTileCount = useMemo(
+        () => 12 / (belowMd ? 6 : 12 / cardsPerRow),
+        [belowMd, cardsPerRow]
+    );
 
     return (
         <Grid
@@ -125,47 +184,16 @@ function ConnectorTile({ cardWidth, cardsPerRow, gridSpacing }: Props) {
             </Grid>
 
             {hasLength(selectData) ? (
-                selectData.map((row, index) => (
-                    <Grid key={index} item xs={6} md={12 / cardsPerRow}>
-                        <Paper
-                            elevation={0}
-                            sx={{
-                                'height': '100%',
-                                'borderRadius': 5,
-                                'background':
-                                    theme.palette.mode === 'dark'
-                                        ? darkGlassBkgColor
-                                        : slate[50],
-                                'padding': 1,
-                                '&:hover': {
-                                    background:
-                                        theme.palette.mode === 'dark'
-                                            ? darkGlassBkgColorIntensified
-                                            : 'rgba(172, 199, 220, 0.45)',
-                                },
-                            }}
+                selectData
+                    .map((row, index) => (
+                        <Grid
+                            key={`connector-tile-${index}`}
+                            item
+                            xs={6}
+                            md={12 / cardsPerRow}
                         >
-                            <Stack
-                                style={{
-                                    height: '100%',
-                                    justifyContent: 'space-between',
-                                }}
-                            >
-                                <Box
-                                    sx={{
-                                        width: '100%',
-                                        height: 125,
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        marginBottom: 2,
-                                        borderRadius: 5,
-                                        background:
-                                            theme.palette.mode === 'dark'
-                                                ? 'rgba(172, 199, 220, 0.30)' // Brighter than desired to improve MySQL visibility.
-                                                : slate[25],
-                                    }}
-                                >
+                            <Tile>
+                                <Box sx={imageBackgroundSx}>
                                     {row.image ? (
                                         <img
                                             src={row.image}
@@ -289,55 +317,121 @@ function ConnectorTile({ cardWidth, cardsPerRow, gridSpacing }: Props) {
                                         )}
                                     </Button>
                                 </Stack>
-                            </Stack>
-                        </Paper>
-                    </Grid>
-                ))
+                            </Tile>
+                        </Grid>
+                    ))
+                    .concat(
+                        <Grid
+                            key="connector-request-tile"
+                            item
+                            xs={6}
+                            md={12 / cardsPerRow}
+                        >
+                            <Tile>
+                                <Box>
+                                    <Box sx={imageBackgroundSx}>
+                                        <AddBox sx={{ fontSize: '4rem' }} />
+                                    </Box>
+
+                                    <Typography align="center" marginBottom={1}>
+                                        <FormattedMessage id="connectorTable.data.connectorRequest" />
+                                    </Typography>
+
+                                    <Typography
+                                        component="p"
+                                        variant="caption"
+                                        marginBottom={2}
+                                        sx={{ px: 1 }}
+                                    >
+                                        <FormattedMessage id="connectors.main.message2.alt" />
+                                    </Typography>
+                                </Box>
+
+                                <Button
+                                    href={intl.formatMessage({
+                                        id: 'connectors.main.message2.docPath',
+                                    })}
+                                    target="_blank"
+                                    rel="noopener"
+                                    endIcon={<OpenInNew />}
+                                >
+                                    <FormattedMessage id="connectorTable.actionsCta.connectorRequest" />
+                                </Button>
+                            </Tile>
+                        </Grid>
+                    )
+            ) : isValidating || tableState.status === TableStatuses.LOADING ? (
+                Array(skeletonTileCount)
+                    .fill(
+                        <Tile>
+                            <Box>
+                                <Skeleton
+                                    variant="rectangular"
+                                    height={125}
+                                    sx={{ mb: 2, borderRadius: 3 }}
+                                />
+
+                                <Skeleton />
+
+                                <Skeleton />
+
+                                <Skeleton sx={{ mb: 5 }} />
+                            </Box>
+
+                            <Skeleton
+                                variant="rectangular"
+                                height={36}
+                                sx={{ borderRadius: 3 }}
+                            />
+                        </Tile>
+                    )
+                    .map((skeleton, index) => (
+                        <Grid
+                            key={`connector-skeleton-${index}`}
+                            item
+                            xs={6}
+                            md={12 / cardsPerRow}
+                        >
+                            {skeleton}
+                        </Grid>
+                    ))
             ) : (
                 <Grid item sx={{ width: '100%' }}>
-                    {isValidating ||
-                    tableState.status === TableStatuses.LOADING ? (
-                        <Box>
-                            <LinearProgress />
-                        </Box>
-                    ) : (
-                        <Paper
-                            elevation={0}
-                            sx={{
-                                height: '100%',
-                                borderRadius: 5,
-                                background:
-                                    theme.palette.mode === 'dark'
-                                        ? darkGlassBkg
-                                        : slate[50],
-                                padding: 1,
-                            }}
-                        >
-                            <Typography
-                                variant="h6"
-                                align="center"
-                                sx={{ mb: 2 }}
-                            >
-                                <FormattedMessage
-                                    id={getEmptyTableHeader(
-                                        tableState.status,
-                                        intlConfig
-                                    )}
-                                />
-                            </Typography>
-
-                            <Typography component="div" align="center">
-                                {getEmptyTableMessage(
+                    <Paper
+                        elevation={0}
+                        sx={{
+                            height: 300,
+                            borderRadius: 5,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            background:
+                                theme.palette.mode === 'dark'
+                                    ? darkGlassBkgColor
+                                    : slate[50],
+                            padding: 1,
+                        }}
+                    >
+                        <Typography variant="h6" align="center" sx={{ mb: 2 }}>
+                            <FormattedMessage
+                                id={getEmptyTableHeader(
                                     tableState.status,
                                     intlConfig
                                 )}
-                            </Typography>
-                        </Paper>
-                    )}
+                            />
+                        </Typography>
+
+                        <Typography component="div" align="center">
+                            {getEmptyTableMessage(
+                                tableState.status,
+                                intlConfig
+                            )}
+                        </Typography>
+                    </Paper>
                 </Grid>
             )}
         </Grid>
     );
 }
 
-export default ConnectorTile;
+export default ConnectorTiles;
