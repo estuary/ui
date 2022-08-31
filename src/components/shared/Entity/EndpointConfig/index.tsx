cf4047c668d4ff7cf6f4e69dc3dd340b79c6122e
Travis Jenkins <travis@estuary.dev>
2022-08-31 12:44:40 -0400

    wip: move endpoint config to folder

--- src/components/shared/Entity/EndpointConfig/index.tsx
+++ src/components/shared/Entity/EndpointConfig/index.tsx
@@ -1,6 +1,6 @@
 import { EditorStoreState } from 'components/editor/Store';
-import EndpointConfigForm from 'components/shared/Entity/EndpointConfigForm';
-import EndpointConfigHeader from 'components/shared/Entity/EndpointConfigHeader';
+import EndpointConfigForm from 'components/shared/Entity/EndpointConfig/Form';
+import EndpointConfigHeader from 'components/shared/Entity/EndpointConfig/Header';
 import WrapperWithHeader from 'components/shared/Entity/WrapperWithHeader';
 import Error from 'components/shared/Error';
 import {
