73357fc72ebed3bbbd458cf05e08065379dc94ae
Travis Jenkins <travis@estuary.dev>
2022-09-08 10:36:47 -0400

    wip: preventing double table calls

--- src/components/tables/EntityTable.tsx
+++ src/components/tables/EntityTable.tsx
@@ -149,7 +149,9 @@ function EntityTable({
     }, [selectData, setRows, enableSelection]);
 
     useEffect(() => {
-        mutateSelectData().catch(() => {});
+        if (successfulTransformations > 0) {
+            mutateSelectData().catch(() => {});
+        }
     }, [mutateSelectData, successfulTransformations]);
 
     const resetSelection = () => {
