8018ba6d30cfebec4ebc5e35b902bf5d104a12d1
Travis Jenkins <travis@estuary.dev>
2022-11-15 23:27:12 -0500

    Adding a stats hydrator

---
+++ src/components/tables/Captures/StatsHydrator.tsx
@@ -0,0 +1,44 @@
+import {
+    SelectableTableStore,
+    selectableTableStoreSelectors,
+} from 'components/tables/Store';
+import { useZustandStore } from 'context/Zustand/provider';
+import { useEffect } from 'react';
+import { SelectTableStoreNames } from 'stores/names';
+import { BaseComponentProps } from 'types';
+
+// Hydrator
+interface StatsHydratorProps extends BaseComponentProps {
+    selectableTableStoreName: SelectTableStoreNames;
+}
+
+const StatsHydrator = ({
+    children,
+    selectableTableStoreName,
+}: StatsHydratorProps) => {
+    const queryLoading = useZustandStore<
+        SelectableTableStore,
+        SelectableTableStore['query']['loading']
+    >(selectableTableStoreName, selectableTableStoreSelectors.query.loading);
+
+    const queryResponse = useZustandStore<
+        SelectableTableStore,
+        SelectableTableStore['query']['response']
+    >(selectableTableStoreName, selectableTableStoreSelectors.query.response);
+
+    const setStats = useZustandStore<
+        SelectableTableStore,
+        SelectableTableStore['setStats']
+    >(selectableTableStoreName, selectableTableStoreSelectors.stats.set);
+
+    useEffect(() => {
+        if (!queryLoading && queryResponse) {
+            setStats();
+        }
+    }, [queryLoading, queryResponse, setStats]);
+
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    return <>{children}</>;
+};
+
+export default StatsHydrator;
