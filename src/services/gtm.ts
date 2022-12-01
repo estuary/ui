ed3e72cc8d36b8711dc3d6d0a1a15d54ea68f9fe
Travis Jenkins <travis@estuary.dev>
2022-12-01 13:09:21 -0500

    Changing the dep we use for Google

--- src/services/gtm.ts
+++ src/services/gtm.ts
@@ -1,16 +1,14 @@
-import ReactGA from 'react-ga';
-import { getGoogleTageManagerSettings, isProduction } from 'utils/env-utils';
+import TagManager from 'react-gtm-module';
+import { getGoogleTageManagerSettings } from 'utils/env-utils';
 
 const googleTagManagerSettings = getGoogleTageManagerSettings();
 
 export const initGoogleTagManager = () => {
     if (
-        isProduction &&
+        // isProduction &&
         googleTagManagerSettings.enabled &&
         googleTagManagerSettings.id
     ) {
-        ReactGA.initialize(googleTagManagerSettings.id, {
-            debug: true,
-        });
+        TagManager.initialize({ gtmId: googleTagManagerSettings.id });
     }
 };
