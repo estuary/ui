5c775b532b73164f4cec6f3c58af537ba443364d
Travis Jenkins <travis@estuary.dev>
2022-12-02 20:39:33 -0500

    Updating form to add default option to allow ajv to set the default value

--- src/components/collection/ResourceConfigForm.tsx
+++ src/components/collection/ResourceConfigForm.tsx
@@ -2,7 +2,7 @@ import { materialCells } from '@jsonforms/material-renderers';
 import { JsonForms } from '@jsonforms/react';
 import { StyledEngineProvider } from '@mui/material';
 import { cloneDeep } from 'lodash';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { setDefaultsValidator } from 'services/ajv';
 import { custom_generateDefaultUISchema } from 'services/jsonforms';
 import defaultRenderers from 'services/jsonforms/defaultRenderers';
@@ -16,13 +16,13 @@ import {
     useResourceConfig_resourceSchema,
     useResourceConfig_setResourceConfig,
 } from 'stores/ResourceConfig/hooks';
+import { Annotations } from 'types/jsonforms';
+import { stripPathing } from 'utils/misc-utils';
 
 type Props = {
     collectionName: string;
     readOnly?: boolean;
 };
-
-const COLLECTION_NAME_ANNOTATION = 'x-collection-name';
 
 function ResourceConfigForm({ collectionName, readOnly = false }: Props) {
     const name = useRef(collectionName);
@@ -30,14 +30,11 @@ function ResourceConfigForm({ collectionName, readOnly = false }: Props) {
     // Resource Config Store
     const resourceConfig = useResourceConfig_resourceConfig();
     const setConfig = useResourceConfig_setResourceConfig();
-
-    let formData = resourceConfig[collectionName].data;
-
     const resourceSchema = useResourceConfig_resourceSchema();
+    const formData = resourceConfig[collectionName].data;
 
     // Form State Store
     const displayValidation = useFormStateStore_displayValidation();
-
     const isActive = useFormStateStore_isActive();
 
     useEffect(() => {
@@ -53,25 +50,40 @@ function ResourceConfigForm({ collectionName, readOnly = false }: Props) {
         },
     };
 
-    // Find field with x-collection-name annotation and fill it with the
-    // collection name
-    if (resourceSchema.properties) {
-        const collectionNameField = Object.entries(resourceSchema.properties)
-          .find(([_, value]) => value && COLLECTION_NAME_ANNOTATION in value) ?? [];
-        const collectionNameFieldKey = collectionNameField[0];
+    // Find field with x-collection-name annotation
+    const collectionNameFieldKey = useMemo(() => {
+        if (resourceSchema.properties) {
+            // Find the field with the collection name annotation
+            const collectionNameField =
+                Object.entries(resourceSchema.properties).find(
+                    ([_, value]) =>
+                        value && Annotations.defaultResourceConfigName in value
+                ) ?? [];
 
-        if (collectionNameFieldKey) {
-            const collectionNameSplit = collectionName.split('/');
-            formData = cloneDeep(formData);
-            formData[collectionNameFieldKey] =
-                collectionNameSplit[collectionNameSplit.length - 1];
+            // Try to fetch the key
+            return collectionNameField[0];
         }
-    }
+
+        return null;
+    }, [resourceSchema.properties]);
+
+    // Check if we need to add a default value
+    const preparedResourceSchema = useMemo(() => {
+        if (collectionNameFieldKey) {
+            // Add a default property set to the stripped collection name
+            const response = cloneDeep(resourceSchema);
+            response.properties[collectionNameFieldKey].default =
+                stripPathing(collectionName);
+            return response;
+        }
+
+        return resourceSchema.properties;
+    }, [collectionName, collectionNameFieldKey, resourceSchema]);
 
     return (
         <StyledEngineProvider injectFirst>
             <JsonForms
-                schema={resourceSchema}
+                schema={preparedResourceSchema}
                 uischema={uiSchema}
                 data={formData}
                 renderers={defaultRenderers}
