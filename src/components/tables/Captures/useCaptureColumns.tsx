3bd6a7daa4d73fdef44d36fbb982a76454e8ee67
Travis Jenkins <travis@estuary.dev>
2022-12-21 10:31:21 -0500

    Adding new files for fetcher refactoring and moving columns to hooks

---
+++ src/components/tables/Captures/useCaptureColumns.tsx
@@ -0,0 +1,64 @@
+import { useTenantDetails } from 'context/fetcher/Tenant';
+import { useMemo } from 'react';
+import { QUERY_PARAM_CONNECTOR_TITLE } from 'services/supabase';
+import { hasLength } from 'utils/misc-utils';
+import StatsHeader from '../cells/stats/Header';
+import { ColumnProps } from '../EntityTable';
+
+const defaultColumns: ColumnProps[] = [
+    {
+        field: null,
+        headerIntlKey: '',
+    },
+    {
+        field: 'catalog_name',
+        headerIntlKey: 'entityTable.data.entity',
+    },
+    {
+        field: QUERY_PARAM_CONNECTOR_TITLE,
+        headerIntlKey: 'entityTable.data.connectorType',
+    },
+    {
+        field: 'writes_to',
+        headerIntlKey: 'entityTable.data.writesTo',
+    },
+    {
+        field: 'updated_at',
+        headerIntlKey: 'entityTable.data.lastPublished',
+    },
+    {
+        field: null,
+        headerIntlKey: null,
+    },
+];
+
+const statsHeader: ColumnProps = {
+    field: null,
+    renderHeader: (index, selectableTableStoreName) => {
+        return (
+            <StatsHeader
+                key={`captures-statsHeader-${index}`}
+                header="entityTable.stats.written"
+                selectableTableStoreName={selectableTableStoreName}
+            />
+        );
+    },
+};
+
+const useCaptureColumns = (): ColumnProps[] => {
+    const tenantDetails = useTenantDetails();
+
+    const hasDetails = useMemo(() => hasLength(tenantDetails), [tenantDetails]);
+
+    return useMemo(() => {
+        if (hasDetails) {
+            const response = [...defaultColumns];
+            response.splice(3, 0, statsHeader);
+            return response;
+        } else {
+            return defaultColumns;
+        }
+    }, [hasDetails]);
+};
+
+export default useCaptureColumns;
---
+++ src/context/fetcher/GrantDetails.tsx
@@ -0,0 +1,50 @@
+import FullPageSpinner from 'components/fullPage/Spinner';
+import { useQuery, useSelect } from 'hooks/supabase-swr';
+import { createContext, useContext } from 'react';
+import { TABLES } from 'services/supabase';
+import { BaseComponentProps } from 'types';
+
+// TODO: Determine an approach that results in a single combined grants query of in the Authenticated app component.
+export interface CombinedGrantsExtQuery {
+    id: string;
+    object_role: string;
+}
+
+const GrantDetailsContext = createContext<CombinedGrantsExtQuery[] | null>(
+    null
+);
+
+const GrantDetailsContextProvider = ({ children }: BaseComponentProps) => {
+    const combinedGrantsQuery = useQuery<CombinedGrantsExtQuery>(
+        TABLES.COMBINED_GRANTS_EXT,
+        { columns: `id, object_role` },
+        []
+    );
+    const { data: grants, isValidating } = useSelect(combinedGrantsQuery);
+
+    const value = grants?.data ? grants.data : null;
+
+    if (isValidating || value === null) {
+        return <FullPageSpinner />;
+    }
+
+    return (
+        <GrantDetailsContext.Provider value={value}>
+            {children}
+        </GrantDetailsContext.Provider>
+    );
+};
+
+const useGrantDetails = () => {
+    const context = useContext(GrantDetailsContext);
+
+    if (context === null) {
+        throw new Error(
+            'useGrantDetails must be used within a GrantDetailsContextProvider'
+        );
+    }
+
+    return context;
+};
+
+export { GrantDetailsContextProvider, useGrantDetails };
