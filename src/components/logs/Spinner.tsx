1de7709ffca306d45ee58138b382a6cf00d38bba
Travis Jenkins <travis@estuary.dev>
2023-01-18 15:54:55 -0500

    Cleaning up commented code. Also, using custom keys if provided

--- src/components/logs/Spinner.tsx
+++ src/components/logs/Spinner.tsx
@@ -1,63 +1,36 @@
-import { CircularProgress } from '@mui/material';
+import { AlertColor } from '@mui/material';
+import { useMemo } from 'react';
 // import { MutableRefObject, useRef, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { useLogsContext } from './Context';
 // import { useInterval } from 'react-use';
-import LogLine, { lineNumberColor } from './Line';
+import LogLine from './Line';
+import SpinnerIcon from './SpinnerIcon';
+import { SpinnerMessageKeys } from './types';
 
-// TODO (Spinner) Leaving the custom animation stuff in for right now. Still want to use this
-//     spinner but not worth the peformance cost compared to a simple css animated spinner
+interface Props {
+    severity?: AlertColor;
+    runningKey?: SpinnerMessageKeys['runningKey'];
+    stoppedKey?: SpinnerMessageKeys['stoppedKey'];
+}
 
-// Spinner from https://github.com/sindresorhus/cli-spinners/blob/main/spinners.json
-// const ANIMATION_PROPERTIES = {
-//  interval: 150,
-//  frames: ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'],
-//  done: '⠿',
-// };
-
-// const getFrame = (frame: number) => {
-//  return ANIMATION_PROPERTIES.frames[frame];
-// };
-
-function Spinner() {
+function Spinner({ severity, runningKey, stoppedKey }: Props) {
     const intl = useIntl();
     const { stopped } = useLogsContext();
 
-    // const buildLogLine = (newFrame: MutableRefObject<number>) => {
-    //  return `${getFrame(newFrame.current)}`;
-    // };
-
-    // const frame = useRef(0);
-    // const [animationFrame, setAnimationFrame] = useState(buildLogLine(frame));
-
-    // useInterval(
-    //  () => {
-    //      const newFrame = frame.current + 1;
-    //      frame.current =
-    //          newFrame === ANIMATION_PROPERTIES.frames.length ? 0 : newFrame;
-    //      setAnimationFrame(buildLogLine(frame));
-    //      console.log('lineText', animationFrame);
-    //  },
-    //  stop ? null : ANIMATION_PROPERTIES.interval
-    // );
+    const lineContent = useMemo(() => {
+        return intl.formatMessage({
+            id: stopped
+                ? stoppedKey ?? 'logs.paused'
+                : runningKey ?? 'logs.default',
+        });
+    }, [intl, runningKey, stopped, stoppedKey]);
 
     return (
         <LogLine
             disableSelect
-            line={intl.formatMessage({
-                id: stopped ? 'logs.paused' : 'logs.default',
-            })}
-            lineNumber={
-                <CircularProgress
-                    variant={stopped ? 'determinate' : undefined}
-                    value={stopped ? 100 : undefined}
-                    size={18}
-                    sx={{
-                        alignSelf: 'end',
-                        color: lineNumberColor,
-                    }}
-                />
-            }
+            line={lineContent}
+            lineNumber={<SpinnerIcon severity={severity} stopped={stopped} />}
         />
     );
 }
