c8f8345f532b98b7f1084616277944577927312b
Travis Jenkins <travis@estuary.dev>
2023-01-30 18:12:55 -0500

    Moving stuff around so Details matches how Edit and Create are organized

---
+++ src/components/shared/Entity/Details/StatusIndicatorAndLabel.tsx
@@ -0,0 +1,63 @@
+import { TableCell, Typography, useTheme } from '@mui/material';
+import { Shard } from 'data-plane-gateway/types/shard_client';
+import { FormattedMessage } from 'react-intl';
+import {
+    useShardDetail_evaluateShardProcessingState,
+    useShardDetail_getShardStatusColor,
+    useShardDetail_getShardStatusMessageId,
+} from 'stores/ShardDetail/hooks';
+import { ShardStatusColor } from 'stores/ShardDetail/types';
+
+interface Props {
+    shard: Shard;
+}
+
+function StatusIndicatorAndLabel({ shard }: Props) {
+    const { id } = shard.spec;
+    const shardId = id ?? '';
+
+    const theme = useTheme();
+
+    const getShardStatusColor = useShardDetail_getShardStatusColor();
+    const getShardStatusMessageId = useShardDetail_getShardStatusMessageId();
+
+    const evaluateShardProcessingState =
+        useShardDetail_evaluateShardProcessingState();
+
+    const defaultStatusColor: ShardStatusColor =
+        theme.palette.mode === 'dark' ? '#EEF8FF' : '#04192A';
+
+    const statusMessageId = getShardStatusMessageId(shardId);
+
+    const taskDisabled: boolean = evaluateShardProcessingState(shardId);
+
+    return (
+        <TableCell width={250}>
+            <span
+                style={{
+                    height: 16,
+                    width: 16,
+                    marginRight: 12,
+                    border: taskDisabled
+                        ? `solid 2px ${getShardStatusColor(
+                              shardId,
+                              defaultStatusColor
+                          )}`
+                        : 0,
+                    backgroundColor: taskDisabled
+                        ? ''
+                        : getShardStatusColor(shardId, defaultStatusColor),
+                    borderRadius: 50,
+                    display: 'inline-block',
+                    verticalAlign: 'middle',
+                }}
+            />
+
+            <Typography component="span" sx={{ verticalAlign: 'middle' }}>
+                <FormattedMessage id={statusMessageId} />
+            </Typography>
+        </TableCell>
+    );
+}
+
+export default StatusIndicatorAndLabel;
---
+++ src/components/shared/Entity/Details/index.tsx
@@ -0,0 +1,58 @@
+import { Box, Divider, Grid, Typography } from '@mui/material';
+import { DataPreview } from 'components/collection/DataPreview';
+import { createEditorStore } from 'components/editor/Store/create';
+import { useEntityType } from 'context/EntityContext';
+import { LocalZustandProvider } from 'context/LocalZustand';
+import useGlobalSearchParams, {
+    GlobalSearchParams,
+} from 'hooks/searchParams/useGlobalSearchParams';
+import useBrowserTitle from 'hooks/useBrowserTitle';
+import { FormattedMessage } from 'react-intl';
+import { EditorStoreNames } from 'stores/names';
+import EditorAndLogs from './EditorAndLogs';
+import ShardInformation from './ShardInformation';
+
+function EntityDetails() {
+    useBrowserTitle('browserTitle.details');
+    const entityType = useEntityType();
+    const catalogName = useGlobalSearchParams(GlobalSearchParams.CATALOG_NAME);
+    const lastPubId = useGlobalSearchParams(GlobalSearchParams.LAST_PUB_ID);
+
+    const isCollection = entityType === 'collection';
+
+    return (
+        <Box>
+            {catalogName}
+            <LocalZustandProvider
+                createStore={createEditorStore(EditorStoreNames.GENERAL)}
+            >
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <ShardInformation entityType={entityType} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="subtitle1">
+                            <FormattedMessage id="detailsPanel.specification.header" />
+                        </Typography>
+                        <EditorAndLogs
+                            collectionNames={[catalogName]}
+                            lastPubId={lastPubId}
+                            disableLogs={true}
+                            localZustandScope={true}
+                        />
+                    </Grid>
+
+                    <Divider />
+
+                    {catalogName && isCollection ? (
+                        <Grid item xs={12}>
+                            <DataPreview collectionName={catalogName} />
+                        </Grid>
+                    ) : null}
+                </Grid>
+            </LocalZustandProvider>
+        </Box>
+    );
+}
+
+export default EntityDetails;
