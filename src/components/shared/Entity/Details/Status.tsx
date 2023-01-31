b61d9bd70ca0ede70eca05ef620209f5749a306b
Travis Jenkins <travis@estuary.dev>
2023-01-31 16:14:30 -0500

    Adding in temp text for shard to have an explanation blurb at top

--- src/components/shared/Entity/Details/Status.tsx
+++ src/components/shared/Entity/Details/Status.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import { useEntityType } from 'context/EntityContext';
 import ShardInformation from './ShardInformation';
 
@@ -7,6 +7,12 @@ function Status() {
 
     return (
         <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Typography>
+                    Below you can find a list of all shards running this{' '}
+                    {entityType} and the corresponding status.
+                </Typography>
+            </Grid>
             <Grid item xs={12}>
                 <ShardInformation entityType={entityType} />
             </Grid>
