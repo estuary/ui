e88bb623373bfd20d8fd4d9b82d4f85c8a70b616
Travis Jenkins <travis@estuary.dev>
2023-02-03 21:49:31 -0500

    Adding a super quick "timeline" for history

--- src/components/shared/Entity/Details/History.tsx
+++ src/components/shared/Entity/Details/History.tsx
@@ -1,26 +1,61 @@
-import { Box, Grid } from '@mui/material';
+import { Box, CircularProgress, Grid, List, ListItem } from '@mui/material';
+import KeyValueList from 'components/shared/KeyValueList';
+import Tile from 'components/shared/Tile';
+import { format } from 'date-fns';
 import useGlobalSearchParams, {
     GlobalSearchParams,
 } from 'hooks/searchParams/useGlobalSearchParams';
 import usePublicationSpecsExt_History from 'hooks/usePublicationSpecsExt';
+import { useIntl } from 'react-intl';
+
+const DATE_FORMAT = 'EEEE, MMM do, yyyy';
 
 function History() {
+    const intl = useIntl();
     const catalogName = useGlobalSearchParams(GlobalSearchParams.CATALOG_NAME);
 
     const { publications, isValidating } =
         usePublicationSpecsExt_History(catalogName);
 
-    if (isValidating) {
-        return <>Loading...</>;
+    if (isValidating || !publications) {
+        return <CircularProgress />;
     }
-
-    console.log('publications', publications);
 
     return (
         <Box>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                    This is the history page
+                    <List>
+                        {publications.map((publication) => (
+                            <ListItem
+                                key={`history-timeline-${publication.pub_id}`}
+                            >
+                                <Tile>
+                                    <KeyValueList
+                                        data={[
+                                            {
+                                                title: intl.formatMessage({
+                                                    id: 'data.email',
+                                                }),
+                                                val: publication.user_email,
+                                            },
+                                            {
+                                                title: intl.formatMessage({
+                                                    id: 'data.published_at',
+                                                }),
+                                                val: format(
+                                                    new Date(
+                                                        publication.published_at
+                                                    ),
+                                                    DATE_FORMAT
+                                                ),
+                                            },
+                                        ]}
+                                    />
+                                </Tile>
+                            </ListItem>
+                        ))}
+                    </List>
                 </Grid>
             </Grid>
         </Box>
