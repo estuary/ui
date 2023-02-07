9dcf55ef2e816f489336cc575bd918a7f1cd6f00
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-02-07 16:27:16 -0500

    Use current color as default stroke color for custom icon

--- src/components/icons/CheckSquare.tsx
+++ src/components/icons/CheckSquare.tsx
@@ -25,7 +25,7 @@ function SvgCheckSquare(
         >
             <path
                 d="M21 3.6V20.4C21 20.5591 20.9368 20.7117 20.8243 20.8243C20.7117 20.9368 20.5591 21 20.4 21H3.6C3.44087 21 3.28826 20.9368 3.17574 20.8243C3.06321 20.7117 3 20.5591 3 20.4V3.6C3 3.44087 3.06321 3.28826 3.17574 3.17574C3.28826 3.06321 3.44087 3 3.6 3H20.4C20.5591 3 20.7117 3.06321 20.8243 3.17574C20.9368 3.28826 21 3.44087 21 3.6Z"
-                stroke="black"
+                stroke="currentColor"
                 strokeWidth="1.5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -33,7 +33,7 @@ function SvgCheckSquare(
 
             <path
                 d="M8 12.4286L10.2857 14.7143L16 9"
-                stroke="black"
+                stroke="currentColor"
                 strokeWidth="1.5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
