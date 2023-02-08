ca73888259b5b5f0ce9680ba468946837f099607
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-02-08 17:42:03 -0500

    Add box shadow to top bar component

--- src/components/navigation/TopBar.tsx
+++ src/components/navigation/TopBar.tsx
@@ -19,6 +19,8 @@ const Topbar = ({ pageTitleProps }: Props) => {
             sx={{
                 position: 'fixed',
                 zIndex: theme.zIndex.drawer + zIndexIncrement,
+                boxShadow:
+                    'rgb(50 50 93 / 2%) 0px 2px 5px -1px, rgb(0 0 0 / 5%) 0px 1px 3px -1px',
             }}
         >
             <Toolbar
