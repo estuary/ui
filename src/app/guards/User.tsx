656905f1c53a93d2259c34bb9a7d165a5f87a2bf
Travis Jenkins <travis@estuary.dev>
2023-02-13 21:35:06 -0500

    Cleaning up old files that are no longer needed

--- src/app/guards/User.tsx
+++ src/app/guards/User.tsx
@@ -3,23 +3,25 @@ import FullPageSpinner from 'components/fullPage/Spinner';
 import * as React from 'react';
 import { useEffect } from 'react';
 import 'react-reflex/styles.css';
+import { Navigate } from 'react-router';
 import { identifyUser } from 'services/logrocket';
 import { BaseComponentProps } from 'types';
-
-const UnauthenticatedApp = React.lazy(() => import('../Unauthenticated'));
 
 function UserGuard({ children }: BaseComponentProps) {
     const { user } = Auth.useUser();
 
     useEffect(() => {
+        console.log('UserGuard- user', user);
         if (user) {
             identifyUser(user);
         }
     }, [user]);
 
+    console.log('UserGuard');
+
     return (
         <React.Suspense fallback={<FullPageSpinner />}>
-            {user ? children : <UnauthenticatedApp />}
+            {user ? children : <Navigate to="/login" replace />}
         </React.Suspense>
     );
 }
