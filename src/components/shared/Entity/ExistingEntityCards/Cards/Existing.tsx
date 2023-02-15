bbb4e3da16b09e55008d0cfd9e02d14c08df8694
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-02-15 17:59:30 -0500

    Remove unused existing entity card details panel logic

--- src/components/shared/Entity/ExistingEntityCards/Cards/Existing.tsx
+++ src/components/shared/Entity/ExistingEntityCards/Cards/Existing.tsx
@@ -1,5 +1,4 @@
-import Editor from '@monaco-editor/react';
-import { Box, Collapse, Paper, Typography, useTheme } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import {
     CaptureQueryWithSpec,
     MaterializationQueryWithSpec,
@@ -8,28 +7,20 @@ import { authenticatedRoutes } from 'app/routes';
 import ConnectorLogo from 'components/connectors/card/Logo';
 import CustomWidthTooltip from 'components/shared/CustomWidthTooltip';
 import EntityCardWrapper from 'components/shared/Entity/ExistingEntityCards/Cards/Wrapper';
-import {
-    alternateConnectorImageBackgroundSx,
-    monacoEditorComponentBackground,
-    semiTransparentBackground,
-} from 'context/Theme';
+import { alternateConnectorImageBackgroundSx } from 'context/Theme';
 import useGlobalSearchParams, {
     GlobalSearchParams,
 } from 'hooks/searchParams/useGlobalSearchParams';
 import { isEmpty } from 'lodash';
 import LogRocket from 'logrocket';
-import { useState } from 'react';
 import { useIntl } from 'react-intl';
 import { useNavigate } from 'react-router';
 import { CustomEvents } from 'services/logrocket';
-import { stringifyJSON } from 'services/stringify';
 import { getPathWithParams } from 'utils/misc-utils';
 
 interface Props {
     queryData: CaptureQueryWithSpec | MaterializationQueryWithSpec | null;
 }
-
-const EDITOR_HEIGHT = 396;
 
 const trackEvent = (
     data: CaptureQueryWithSpec | MaterializationQueryWithSpec
@@ -52,121 +43,84 @@ function ExistingEntityCard({ queryData }: Props) {
     );
     const navigate = useNavigate();
 
-    const theme = useTheme();
     const intl = useIntl();
 
-    const [detailsExpanded, setDetailsExpanded] = useState<boolean>(false);
+    const editTask = () => {
+        if (!isEmpty(queryData)) {
+            trackEvent(queryData);
 
-    const handlers = {
-        editTask: () => {
-            if (!isEmpty(queryData)) {
-                trackEvent(queryData);
+            const baseURL =
+                queryData.spec_type === 'capture'
+                    ? authenticatedRoutes.captures.edit.fullPath
+                    : authenticatedRoutes.materializations.edit.fullPath;
 
-                const baseURL =
-                    queryData.spec_type === 'capture'
-                        ? authenticatedRoutes.captures.edit.fullPath
-                        : authenticatedRoutes.materializations.edit.fullPath;
+            const baseParams = {
+                [GlobalSearchParams.CONNECTOR_ID]: queryData.connector_id,
+                [GlobalSearchParams.LIVE_SPEC_ID]: queryData.id,
+                [GlobalSearchParams.LAST_PUB_ID]: queryData.last_pub_id,
+            };
 
-                const baseParams = {
-                    [GlobalSearchParams.CONNECTOR_ID]: queryData.connector_id,
-                    [GlobalSearchParams.LIVE_SPEC_ID]: queryData.id,
-                    [GlobalSearchParams.LAST_PUB_ID]: queryData.last_pub_id,
-                };
-
-                // TODO (routes): Allow the user to return to the existing entity check page on browser back.
-                navigate(
-                    getPathWithParams(
-                        baseURL,
-                        prefillPubIds.length > 0
-                            ? {
-                                  ...baseParams,
-                                  [GlobalSearchParams.PREFILL_PUB_ID]:
-                                      prefillPubIds,
-                              }
-                            : baseParams
-                    )
-                );
-            }
-        },
-        toggleDetailsPanel: () => setDetailsExpanded(!detailsExpanded),
+            // TODO (routes): Allow the user to return to the existing entity check page on browser back.
+            navigate(
+                getPathWithParams(
+                    baseURL,
+                    prefillPubIds.length > 0
+                        ? {
+                              ...baseParams,
+                              [GlobalSearchParams.PREFILL_PUB_ID]:
+                                  prefillPubIds,
+                          }
+                        : baseParams
+                )
+            );
+        }
     };
 
     if (!queryData) {
         return null;
     } else {
         return (
-            <>
-                <EntityCardWrapper clickHandler={handlers.editTask}>
-                    <Box sx={alternateConnectorImageBackgroundSx}>
-                        <ConnectorLogo
-                            imageSrc={queryData.image}
-                            maxHeight={35}
-                            padding="0 0.5rem"
-                        />
-                    </Box>
+            <EntityCardWrapper clickHandler={editTask}>
+                <Box sx={alternateConnectorImageBackgroundSx}>
+                    <ConnectorLogo
+                        imageSrc={queryData.image}
+                        maxHeight={35}
+                        padding="0 0.5rem"
+                    />
+                </Box>
 
-                    <Box
-                        sx={{
-                            minWidth: 0,
-                            ml: 2,
-                            textAlign: 'left',
-                        }}
+                <Box
+                    sx={{
+                        minWidth: 0,
+                        ml: 2,
+                        textAlign: 'left',
+                    }}
+                >
+                    <CustomWidthTooltip
+                        title={queryData.catalog_name}
+                        placement="bottom-start"
                     >
-                        <CustomWidthTooltip
-                            title={queryData.catalog_name}
-                            placement="bottom-start"
-                        >
-                            <Typography noWrap sx={{ mb: 0.5 }}>
-                                {queryData.catalog_name}
-                            </Typography>
-                        </CustomWidthTooltip>
-
-                        <Typography variant="caption">
-                            {intl.formatMessage(
-                                {
-                                    id: 'existingEntityCheck.existingCard.label.lastPublished',
-                                },
-                                {
-                                    date: intl.formatDate(
-                                        queryData.updated_at,
-                                        {
-                                            day: 'numeric',
-                                            month: 'long',
-                                            year: 'numeric',
-                                        }
-                                    ),
-                                }
-                            )}
+                        <Typography noWrap sx={{ mb: 0.5 }}>
+                            {queryData.catalog_name}
                         </Typography>
-                    </Box>
-                </EntityCardWrapper>
+                    </CustomWidthTooltip>
 
-                <Collapse in={detailsExpanded}>
-                    <Paper
-                        elevation={0}
-                        sx={{
-                            p: 2,
-                            mx: 1,
-                            background:
-                                semiTransparentBackground[theme.palette.mode],
-                        }}
-                    >
-                        <Editor
-                            height={EDITOR_HEIGHT}
-                            value={stringifyJSON(queryData.spec.endpoint)}
-                            defaultLanguage="json"
-                            theme={
-                                monacoEditorComponentBackground[
-                                    theme.palette.mode
-                                ]
+                    <Typography variant="caption">
+                        {intl.formatMessage(
+                            {
+                                id: 'existingEntityCheck.existingCard.label.lastPublished',
+                            },
+                            {
+                                date: intl.formatDate(queryData.updated_at, {
+                                    day: 'numeric',
+                                    month: 'long',
+                                    year: 'numeric',
+                                }),
                             }
-                            saveViewState={false}
-                            path={queryData.catalog_name}
-                            options={{ readOnly: true }}
-                        />
-                    </Paper>
-                </Collapse>
-            </>
+                        )}
+                    </Typography>
+                </Box>
+            </EntityCardWrapper>
         );
     }
 }
