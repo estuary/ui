cafbc17826594397157275f83d465498a7b64a7d
Travis Jenkins <travis@estuary.dev>
2023-02-21 14:55:16 -0500

    Make the selection picker more usable to make it clear to users what is selected

--- src/components/collection/Picker.tsx
+++ src/components/collection/Picker.tsx
@@ -3,12 +3,15 @@ import {
     AutocompleteChangeReason,
     Box,
     TextField,
+    Typography,
 } from '@mui/material';
 import { useEditorStore_persistedDraftId } from 'components/editor/Store/hooks';
 import { useEntityType } from 'context/EntityContext';
+import { truncateTextSx } from 'context/Theme';
 import { useEntityWorkflow } from 'context/Workflow';
 import useDraftSpecs from 'hooks/useDraftSpecs';
 import useLiveSpecs from 'hooks/useLiveSpecs';
+import { Check } from 'iconoir-react';
 import { isEqual } from 'lodash';
 import { useEffect, useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
@@ -232,7 +235,6 @@ function CollectionPicker({ readOnly = false }: Props) {
                 value={collectionValues}
                 inputValue={inputValue}
                 size="small"
-                filterSelectedOptions
                 fullWidth
                 onChange={handlers.updateCollections}
                 onInputChange={(_event, newInputValue, reason) => {
@@ -257,6 +259,34 @@ function CollectionPicker({ readOnly = false }: Props) {
                         onBlur={handlers.validateSelection}
                     />
                 )}
+                renderOption={(props, option, { selected }) => {
+                    return (
+                        <li {...props}>
+                            <Box
+                                sx={{
+                                    ml: -2,
+                                    mr: 0.5,
+                                }}
+                            >
+                                <Check
+                                    aria-checked={selected}
+                                    style={{
+                                        visibility: selected
+                                            ? 'visible'
+                                            : 'hidden',
+                                    }}
+                                />
+                            </Box>
+                            <Typography
+                                sx={{
+                                    ...truncateTextSx,
+                                }}
+                            >
+                                {option.name}
+                            </Typography>
+                        </li>
+                    );
+                }}
             />
         </Box>
     ) : null;
