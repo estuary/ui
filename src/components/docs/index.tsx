a9e9ea4e093bf3d65aa85283e0882b0166bb8375
Travis Jenkins <travis@estuary.dev>
2023-02-27 22:15:35 -0500

    Adding message to iframe component so we can use a ref to communicate directly to it

--- src/components/docs/index.tsx
+++ src/components/docs/index.tsx
@@ -1,12 +1,27 @@
 import { Toolbar } from '@mui/material';
 import { useDocs } from 'context/Docs';
+import { useColorMode } from 'context/Theme';
+import { useEffect, useRef } from 'react';
 import { useIntl } from 'react-intl';
+import { getDocsSettings } from 'utils/env-utils';
 
 const sandbox = ['allow-scripts', 'allow-same-origin'].join(' ');
+const { origin } = getDocsSettings();
 
 function SidePanelConnectorDocs() {
     const intl = useIntl();
+    const iframeRef = useRef<HTMLIFrameElement>(null);
     const { docsURL } = useDocs();
+    const colorMode = useColorMode();
+
+    useEffect(() => {
+        if (iframeRef.current?.contentWindow) {
+            iframeRef.current.contentWindow.postMessage(
+                { type: 'estuary.colorMode', mode: colorMode.colorMode },
+                origin
+            );
+        }
+    }, [colorMode]);
 
     if (!docsURL) return null;
 
@@ -14,6 +29,7 @@ function SidePanelConnectorDocs() {
         <>
             <Toolbar />
             <iframe
+                ref={iframeRef}
                 style={{ border: 'none', height: '100%' }}
                 src={docsURL}
                 sandbox={sandbox}
