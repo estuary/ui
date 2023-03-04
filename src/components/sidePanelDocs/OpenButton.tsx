c8d2daffd9b3edeb170aa074bd1a7b3cf9cc33f9
Travis Jenkins <travis@estuary.dev>
2023-03-03 19:15:14 -0500

    Adding button to open docs on header

---
+++ src/components/sidePanelDocs/OpenButton.tsx
@@ -0,0 +1,56 @@
+import {
+    Box,
+    Button,
+    Collapse,
+    Divider,
+    useMediaQuery,
+    useTheme,
+} from '@mui/material';
+import { SidebarCollapse } from 'iconoir-react';
+import { FormattedMessage } from 'react-intl';
+import {
+    useSidePanelDocsStore_setShow,
+    useSidePanelDocsStore_show,
+    useSidePanelDocsStore_url,
+} from 'stores/SidePanelDocs/hooks';
+import { hasLength } from 'utils/misc-utils';
+
+function SidePanelDocsOpenButton() {
+    const theme = useTheme();
+    const belowMd = useMediaQuery(theme.breakpoints.down('md'));
+
+    const docsURL = useSidePanelDocsStore_url();
+    const showDocs = useSidePanelDocsStore_show();
+    const setShowDocs = useSidePanelDocsStore_setShow();
+    const showButton = !showDocs && !belowMd && hasLength(docsURL);
+
+    return (
+        <Collapse in={showButton} orientation="horizontal" collapsedSize={0}>
+            <Box sx={{ display: 'flex' }}>
+                <Divider
+                    orientation="vertical"
+                    flexItem
+                    sx={{ ml: 1, mr: 2 }}
+                />
+                <Button
+                    size="small"
+                    variant="outlined"
+                    onClick={() => {
+                        setShowDocs(true);
+                    }}
+                    endIcon={
+                        <SidebarCollapse
+                            style={{
+                                fontSize: 13,
+                            }}
+                        />
+                    }
+                >
+                    <FormattedMessage id="terms.documentation" />
+                </Button>
+            </Box>
+        </Collapse>
+    );
+}
+
+export default SidePanelDocsOpenButton;
