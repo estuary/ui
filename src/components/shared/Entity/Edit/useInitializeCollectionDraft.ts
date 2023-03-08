da5aaf70249064033ebc0c056bba7e813858d3f0
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-03-08 10:53:59 -0500

    Cleanup collection init hook logic

--- src/components/shared/Entity/Edit/useInitializeCollectionDraft.ts
+++ src/components/shared/Entity/Edit/useInitializeCollectionDraft.ts
@@ -1,8 +1,4 @@
-import {
-    createDraftSpec,
-    getDraftSpecsByCatalogName,
-    updateDraftSpec,
-} from 'api/draftSpecs';
+import { createDraftSpec, getDraftSpecsByCatalogName } from 'api/draftSpecs';
 import {
     getLiveSpecsByCatalogName,
     LiveSpecsExtQuery_ByCatalogName,
@@ -18,33 +14,20 @@ import { useCallback } from 'react';
 import { useResourceConfig_currentCollection } from 'stores/ResourceConfig/hooks';
 import { Annotations } from 'types/jsonforms';
 
-type SupabaseResponse<T> =
-    | { data: T; error?: undefined }
-    | { data: null; error: any };
-
 const specType = 'collection';
 
 const getCollection = async (
     collectionName: string
-): Promise<SupabaseResponse<LiveSpecsExtQuery_ByCatalogName>> => {
+): Promise<LiveSpecsExtQuery_ByCatalogName | null> => {
     const liveSpecResponse = await getLiveSpecsByCatalogName(
         collectionName,
         specType
     );
 
-    if (liveSpecResponse.error) {
-        console.log(
-            'getPublishedCollection | live specs',
-            liveSpecResponse.error
-        );
-
-        return { data: null, error: liveSpecResponse.error };
-    } else if (liveSpecResponse.data && liveSpecResponse.data.length > 0) {
-        return { data: liveSpecResponse.data[0] };
+    if (liveSpecResponse.data && liveSpecResponse.data.length > 0) {
+        return liveSpecResponse.data[0];
     } else {
-        console.log('getPublishedCollection | live specs | fall through');
-
-        return { data: null, error: liveSpecResponse.error };
+        return null;
     }
 };
 
@@ -87,7 +70,7 @@ function useInitializeCollectionDraft() {
         async (
             collectionName: string,
             lastPubId?: string,
-            spec?: any
+            liveSpec?: any
         ): Promise<void> => {
             if (draftId) {
                 const draftSpecResponse = await getDraftSpecsByCatalogName(
@@ -103,48 +86,57 @@ function useInitializeCollectionDraft() {
                     const expectedPubId =
                         draftSpecResponse.data[0].expect_pub_id;
 
+                    updateBindingsEditorState({
+                        spec: draftSpecResponse.data[0].spec,
+                        belongsToDraft: true,
+                    });
+
                     if (lastPubId && expectedPubId !== lastPubId) {
-                        console.log('Update existing draft spec entry');
-
-                        const updatedDraftSpecResponse = await updateDraftSpec(
-                            draftId,
-                            collectionName,
-                            draftSpecResponse.data[0].spec
-                        );
-
-                        if (
-                            updatedDraftSpecResponse.data &&
-                            updatedDraftSpecResponse.data.length > 0
-                        ) {
-                            updateBindingsEditorState({
-                                spec: updatedDraftSpecResponse.data[0].spec,
-                                belongsToDraft: true,
-                            });
-                        } else {
-                            updateBindingsEditorState({
-                                spec: draftSpecResponse.data[0].spec,
-                                belongsToDraft: true,
-                            });
-
-                            setCollectionInitializationError({
-                                severity: 'warning',
-                                messageId:
-                                    'workflows.collectionSelector.error.message.invalidPubId',
-                            });
-                        }
-                    } else {
-                        updateBindingsEditorState({
-                            spec: draftSpecResponse.data[0].spec,
-                            belongsToDraft: true,
+                        setCollectionInitializationError({
+                            severity: 'warning',
+                            messageId:
+                                'workflows.collectionSelector.error.message.invalidPubId',
                         });
                     }
-                } else if (spec) {
-                    console.log('Create new draft spec entry');
 
+                    // TODO (optimization): When a diff editor with merge capabilities is available in the UI, present
+                    //   the user with an option to review the "live" collection specification, merge in the desired
+                    //   modifications, and update the expected pub ID of the draft to reflect the latest publication.
+
+                    // if (!lastPubId || expectedPubId === lastPubId) {
+                    //     updateBindingsEditorState({
+                    //         spec: draftSpecResponse.data[0].spec,
+                    //         belongsToDraft: true,
+                    //     });
+                    // } else {
+                    //     const updatedDraftSpecResponse = await updateDraftSpec(
+                    //         draftId,
+                    //         collectionName,
+                    //         draftSpecResponse.data[0].spec
+                    //     );
+
+                    //     if (
+                    //         updatedDraftSpecResponse.data &&
+                    //         updatedDraftSpecResponse.data.length > 0
+                    //     ) {
+                    //         updateBindingsEditorState({
+                    //             spec: updatedDraftSpecResponse.data[0].spec,
+                    //             belongsToDraft: true,
+                    //         });
+
+                    //         setCollectionInitializationError({
+                    //             severity: 'warning',
+                    //             messageId:
+                    //                 'workflows.collectionSelector.error.message.invalidPubId',
+                    //         });
+                    //     }
+                    // }
+                } else if (liveSpec) {
+                    // The draft of a collection that has been published could not be found.
                     const newDraftSpecResponse = await createDraftSpec(
                         draftId,
                         collectionName,
-                        spec,
+                        liveSpec,
                         specType,
                         lastPubId
                     );
@@ -159,7 +151,7 @@ function useInitializeCollectionDraft() {
                         });
                     } else {
                         updateBindingsEditorState({
-                            spec,
+                            spec: liveSpec,
                             belongsToDraft: false,
                         });
 
@@ -171,6 +163,7 @@ function useInitializeCollectionDraft() {
                     }
                 } else {
                     // The draft of a collection that has never been published could not be found.
+                    updateBindingsEditorState(undefined);
                 }
             }
         },
@@ -181,24 +174,18 @@ function useInitializeCollectionDraft() {
         setCollectionInitializationError(null);
 
         if (currentCollection) {
-            const { data: publishedCollection, error: liveSpecError } =
-                await getCollection(currentCollection);
+            const publishedCollection = await getCollection(currentCollection);
 
-            if (liveSpecError) {
-                return liveSpecError;
-            } else {
-                const draftSpecError = await getCollectionDraftSpecs(
-                    currentCollection,
-                    publishedCollection?.last_pub_id,
-                    publishedCollection?.spec
-                );
-
-                return draftSpecError;
-            }
+            await getCollectionDraftSpecs(
+                currentCollection,
+                publishedCollection?.last_pub_id,
+                publishedCollection?.spec
+            );
         }
     }, [
         getCollectionDraftSpecs,
         setCollectionInitializationError,
+        updateBindingsEditorState,
         currentCollection,
     ]);
 }
