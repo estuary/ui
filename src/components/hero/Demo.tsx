daecd4062ae696ee23f72d2be50e54e0e394d9e4
Travis Jenkins <travis@estuary.dev>
2023-03-09 16:24:45 -0500

    Refactor/clean up and updating names to match most recent ones in prod

--- src/components/hero/Demo.tsx
+++ src/components/hero/Demo.tsx
@@ -1,9 +1,6 @@
 import { Grid } from '@mui/material';
-import { authenticatedRoutes } from 'app/routes';
 import ExternalLink from 'components/shared/ExternalLink';
 import { FormattedMessage } from 'react-intl';
-import { useNavigate } from 'react-router';
-import { getPathWithParams } from 'utils/misc-utils';
 import DemoButton from './DemoButton';
 import DemoStep from './Steps/Demo';
 
@@ -11,8 +8,6 @@ const DEMO_URL =
     'https://docs.google.com/spreadsheets/d/1Cd_afDejaVXKeGxSTCupKaTtrb3a7ZHBghTemDNKE5I/edit#gid=0';
 
 function HeroDemo() {
-    const navigate = useNavigate();
-
     return (
         <>
             <Grid container>
@@ -21,48 +16,9 @@ function HeroDemo() {
                 <DemoStep step={3} />
             </Grid>
             <Grid container>
-                <DemoButton
-                    step={1}
-                    onClick={() => {
-                        navigate(
-                            getPathWithParams(
-                                authenticatedRoutes.captures.fullPath,
-                                {
-                                    'cap-sq': 'estuary/wikipedia',
-                                }
-                            )
-                        );
-                    }}
-                />
-
-                <DemoButton
-                    step={2}
-                    onClick={() => {
-                        navigate(
-                            getPathWithParams(
-                                authenticatedRoutes.collections.fullPath,
-                                {
-                                    'col-sq':
-                                        'estuary/wikipedia/derivation-data-change',
-                                }
-                            )
-                        );
-                    }}
-                />
-
-                <DemoButton
-                    step={3}
-                    onClick={() => {
-                        navigate(
-                            getPathWithParams(
-                                authenticatedRoutes.materializations.fullPath,
-                                {
-                                    'mat-sq': 'estuary/dave-wiki-lines',
-                                }
-                            )
-                        );
-                    }}
-                />
+                <DemoButton step={1} type="captures" />
+                <DemoButton step={2} type="collections" />
+                <DemoButton step={3} type="materializations" />
             </Grid>
             <Grid
                 item
--- src/components/hero/DemoButton.tsx
+++ src/components/hero/DemoButton.tsx
@@ -1,13 +1,39 @@
 import { Box, Grid, Link } from '@mui/material';
+import { authenticatedRoutes } from 'app/routes';
 import { semiTransparentBackgroundIntensified } from 'context/Theme';
+import { useCallback } from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useNavigate } from 'react-router-dom';
+import { getPathWithParams } from 'utils/misc-utils';
+
+const FILTER_TABLE_PROPS = {
+    captures: {
+        'cap-sq': 'demo/wikipedia/recent-changes',
+    },
+    collections: {
+        'col-sq': 'demo/wikipedia/lines-and-bots',
+    },
+    materializations: {
+        'mat-sq': 'demo/wikipedia/lines-by-usertype',
+    },
+};
 
 interface Props {
     step: number;
-    onClick: () => void;
+    type: keyof typeof FILTER_TABLE_PROPS;
 }
 
-function DemoButton({ step, onClick }: Props) {
+function DemoButton({ step, type }: Props) {
+    const navigate = useNavigate();
+    const goToFilteredTable = useCallback(() => {
+        navigate(
+            getPathWithParams(
+                authenticatedRoutes[type].fullPath,
+                FILTER_TABLE_PROPS[type]
+            )
+        );
+    }, [navigate, type]);
+
     return (
         <Grid
             item
@@ -29,7 +55,7 @@ function DemoButton({ step, onClick }: Props) {
                     mt: 2,
                 }}
             >
-                <Link onClick={onClick} sx={{ cursor: 'pointer' }}>
+                <Link onClick={goToFilteredTable} sx={{ cursor: 'pointer' }}>
                     <FormattedMessage id={`home.hero.${step}.button`} />
                 </Link>
             </Box>
