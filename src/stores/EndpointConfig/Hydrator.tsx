be5da62c295d774ead2d5bad11899ef7feaa6ed8
Travis Jenkins <travis@estuary.dev>
2023-03-14 20:40:13 -0400

    Trying out setting server update... will replace with new flag soon

--- src/stores/EndpointConfig/Hydrator.tsx
+++ src/stores/EndpointConfig/Hydrator.tsx
@@ -1,18 +1,25 @@
 import { useEntityType } from 'context/EntityContext';
 import { useEntityWorkflow } from 'context/Workflow';
+import { isEmpty } from 'lodash';
 import LogRocket from 'logrocket';
+import { useEffect } from 'react';
 import { useEffectOnce } from 'react-use';
 import {
+    useEndpointConfigStore_endpointSchema,
     useEndpointConfig_hydrated,
     useEndpointConfig_hydrateState,
     useEndpointConfig_setHydrated,
     useEndpointConfig_setHydrationErrorsExist,
+    useEndpointConfig_setServerUpdateRequired,
 } from 'stores/EndpointConfig/hooks';
 import { BaseComponentProps } from 'types';
 
 export const EndpointConfigHydrator = ({ children }: BaseComponentProps) => {
     const entityType = useEntityType();
     const workflow = useEntityWorkflow();
+    const endpointSchema = useEndpointConfigStore_endpointSchema();
+
+    console.log('endpointSchema', { endpointSchema });
 
     const hydrated = useEndpointConfig_hydrated();
     const setHydrated = useEndpointConfig_setHydrated();
@@ -20,6 +27,8 @@ export const EndpointConfigHydrator = ({ children }: BaseComponentProps) => {
     const setHydrationErrorsExist = useEndpointConfig_setHydrationErrorsExist();
 
     const hydrateState = useEndpointConfig_hydrateState();
+
+    const setServerUpdateRequired = useEndpointConfig_setServerUpdateRequired();
 
     useEffectOnce(() => {
         if (
@@ -39,6 +48,15 @@ export const EndpointConfigHydrator = ({ children }: BaseComponentProps) => {
             );
         }
     });
+
+    useEffect(() => {
+        const hasFields = Boolean(
+            endpointSchema.properties && !isEmpty(endpointSchema.properties)
+        );
+
+        console.log('setServerUpdateRequired effect', { hasFields });
+        setServerUpdateRequired(hasFields);
+    }, [endpointSchema.properties, setServerUpdateRequired]);
 
     // eslint-disable-next-line react/jsx-no-useless-fragment
     return <>{children}</>;
