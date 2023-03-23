81c17682d7463be7a2c9ca8f662ff0c41c9de40c
Travis Jenkins <travis@estuary.dev>
2023-03-23 11:29:58 -0400

    Moving the modal to a component

---
+++ src/components/derivation/Create/index.tsx
@@ -0,0 +1,37 @@
+import { Dialog, DialogContent, DialogTitle } from '@mui/material';
+import { authenticatedRoutes } from 'app/routes';
+import NewCollection from 'components/collection/NewCollection';
+import { useState } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { useNavigate } from 'react-router-dom';
+
+function DerivationCreate() {
+    const navigate = useNavigate();
+
+    // There is _probably_ a better way to do this, but the idea is
+    // to reset the state of the NewCollection component every time
+    // it's closed, so you don't reopen it and have your previous
+    // selections still selected, which would be unexpected.
+    const [newCollectionKey, setNewCollectionKey] = useState(0);
+
+    return (
+        <Dialog
+            open
+            fullWidth
+            maxWidth="md"
+            onClose={() => {
+                navigate(authenticatedRoutes.collections.fullPath);
+                setNewCollectionKey((k) => k + 1);
+            }}
+        >
+            <DialogTitle>
+                <FormattedMessage id="newCollection.modal.title" />
+            </DialogTitle>
+            <DialogContent>
+                <NewCollection key={newCollectionKey} />
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default DerivationCreate;
