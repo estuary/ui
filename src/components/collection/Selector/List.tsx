ff6a4b5ddfb07319dac9fb0592fe8cb9a08a4407
Travis Jenkins <travis@estuary.dev>
2023-03-24 19:56:44 -0400

    Starting to make the list shared

---
+++ src/components/collection/Selector/List.tsx
@@ -0,0 +1,209 @@
+import {
+    Box,
+    Button,
+    Divider,
+    Stack,
+    Typography,
+    useTheme,
+} from '@mui/material';
+import {
+    DataGrid,
+    GridColDef,
+    GridColumnHeaderParams,
+    GridRenderCellParams,
+    GridValueGetterParams,
+} from '@mui/x-data-grid';
+import SelectorEmpty from 'components/editor/Bindings/SelectorEmpty';
+import { alternativeDataGridHeader, defaultOutline } from 'context/Theme';
+import { ReactNode, useRef } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { useUnmount } from 'react-use';
+import useConstant from 'use-constant';
+import CollectionSelectorRow from './Row';
+
+interface Props {
+    collections: Set<string>;
+
+    removeCollection: (collectionName: string) => void;
+    removeAllCollections: () => void;
+
+    currentCollection?: any;
+    setCurrentCollection?: (collection: any) => void;
+
+    readOnly?: boolean;
+    RediscoverButton?: ReactNode;
+
+    height?: number;
+}
+
+const initialState = {
+    columns: {
+        columnVisibilityModel: {
+            spec_type: false,
+        },
+    },
+};
+
+function CollectionSelectorList({
+    collections,
+    removeCollection,
+    removeAllCollections,
+    readOnly,
+    RediscoverButton,
+    currentCollection,
+    setCurrentCollection,
+    height,
+}: Props) {
+    const onSelectTimeOut = useRef<number | null>(null);
+    const theme = useTheme();
+    const intl = useIntl();
+    const collectionsLabel = useConstant(() =>
+        intl.formatMessage({
+            id: 'workflows.collectionSelector.label.listHeader',
+        })
+    );
+
+    const selectionEnabled = currentCollection && setCurrentCollection;
+
+    const collectionsArray = Array.from(collections);
+
+    const rows = collectionsArray.map((collection) => ({
+        id: collection,
+        name: collection,
+    }));
+
+    const columns: GridColDef[] = [
+        {
+            field: 'name',
+            flex: 1,
+            headerName: collectionsLabel,
+            sortable: false,
+            renderHeader: (params: GridColumnHeaderParams) => (
+                <Typography>{params.colDef.headerName}</Typography>
+            ),
+            renderCell: (params: GridRenderCellParams) => {
+                return (
+                    <CollectionSelectorRow
+                        collection={params.value}
+                        disabled={false}
+                        removeCollection={removeCollection}
+                    />
+                );
+            },
+            valueGetter: (params: GridValueGetterParams) => {
+                return params.row.name;
+            },
+        },
+    ];
+
+    useUnmount(() => {
+        if (onSelectTimeOut.current) clearTimeout(onSelectTimeOut.current);
+    });
+
+    return (
+        <>
+            <Box
+                sx={{
+                    ml: 'auto',
+                    borderTop: defaultOutline[theme.palette.mode],
+                    borderLeft: defaultOutline[theme.palette.mode],
+                }}
+            >
+                <Stack
+                    direction="row"
+                    spacing={1}
+                    divider={
+                        RediscoverButton ? (
+                            <Divider
+                                orientation="vertical"
+                                variant="middle"
+                                flexItem
+                            />
+                        ) : null
+                    }
+                    sx={{
+                        justifyContent: 'right',
+                    }}
+                >
+                    {RediscoverButton ? RediscoverButton : null}
+
+                    <Button
+                        variant="text"
+                        disabled={readOnly ?? rows.length === 0}
+                        onClick={removeAllCollections}
+                        sx={{ borderRadius: 0 }}
+                    >
+                        <FormattedMessage id="workflows.collectionSelector.cta.delete" />
+                    </Button>
+                </Stack>
+            </Box>
+
+            <Box sx={{ height: height ?? 480 }}>
+                <DataGrid
+                    components={{
+                        NoRowsOverlay: SelectorEmpty,
+                    }}
+                    rows={rows}
+                    columns={columns}
+                    headerHeight={40}
+                    rowCount={rows.length}
+                    hideFooter
+                    disableColumnSelector
+                    disableSelectionOnClick={!selectionEnabled}
+                    onRowClick={
+                        selectionEnabled
+                            ? (params: any) => {
+                                  // This is hacky but it works. It clears out the
+                                  //  current collection before switching.
+                                  //  If a user is typing quickly in a form and then selects a
+                                  //  different binding VERY quickly it could cause the updates
+                                  //  to go into the wrong form.
+                                  setCurrentCollection(null);
+                                  onSelectTimeOut.current = window.setTimeout(
+                                      () => {
+                                          setCurrentCollection(params.row.name);
+                                      }
+                                  );
+                              }
+                            : undefined
+                    }
+                    initialState={initialState}
+                    sx={{
+                        'borderBottom': 'none',
+                        '& .MuiDataGrid-row ': {
+                            cursor: 'pointer',
+                        },
+                        '& .MuiDataGrid-cell': {
+                            borderBottom: defaultOutline[theme.palette.mode],
+                        },
+                        '& .MuiDataGrid-columnSeparator': {
+                            display: 'none',
+                        },
+                        '& .MuiDataGrid-columnHeaders': {
+                            borderTop: defaultOutline[theme.palette.mode],
+                            borderBottom: defaultOutline[theme.palette.mode],
+                            bgcolor:
+                                alternativeDataGridHeader[theme.palette.mode],
+                        },
+                        '& .MuiDataGrid-columnHeader:hover': {
+                            '& .MuiDataGrid-columnHeaderTitleContainerContent':
+                                {
+                                    mr: 0.5,
+                                },
+                            '& .MuiDataGrid-menuIcon': {
+                                width: '2rem',
+                            },
+                        },
+                        '& .MuiDataGrid-columnHeaderTitleContainerContent': {
+                            width: '100%',
+                            justifyContent: 'space-between',
+                            mr: 4.5,
+                        },
+                    }}
+                />
+            </Box>
+        </>
+    );
+}
+
+export default CollectionSelectorList;
--- src/components/collection/Selector/index.tsx
+++ src/components/collection/Selector/index.tsx
@@ -1,27 +1,8 @@
-import {
-    Box,
-    Button,
-    Divider,
-    Stack,
-    Typography,
-    useTheme,
-} from '@mui/material';
-import {
-    DataGrid,
-    GridColDef,
-    GridColumnHeaderParams,
-    GridRenderCellParams,
-    GridValueGetterParams,
-} from '@mui/x-data-grid';
-import SelectorEmpty from 'components/editor/Bindings/SelectorEmpty';
-import { alternativeDataGridHeader, defaultOutline } from 'context/Theme';
+import { Box } from '@mui/material';
 import useLiveSpecs from 'hooks/useLiveSpecs';
 import { difference } from 'lodash';
-import { ReactNode, useRef } from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
-import { useUnmount } from 'react-use';
-import useConstant from 'use-constant';
-import CollectionSelectorRow from './Row';
+import { ReactNode } from 'react';
+import CollectionSelectorList from './List';
 import CollectionSelectorSearch from './Search';
 
 interface BindingSelectorProps {
@@ -42,14 +23,6 @@ interface BindingSelectorProps {
     height?: number;
 }
 
-const initialState = {
-    columns: {
-        columnVisibilityModel: {
-            spec_type: false,
-        },
-    },
-};
-
 function CollectionSelector({
     loading,
     skeleton,
@@ -66,62 +39,10 @@ function CollectionSelector({
 
     height,
 }: BindingSelectorProps) {
-    const theme = useTheme();
-
     const { liveSpecs } = useLiveSpecs('collection');
     const catalogNames = liveSpecs.map((liveSpec) => liveSpec.catalog_name);
 
-    const selectionEnabled = currentCollection && setCurrentCollection;
-    const onSelectTimeOut = useRef<number | null>(null);
-
-    const intl = useIntl();
-    const collectionsLabel = useConstant(() =>
-        intl.formatMessage({
-            id: 'workflows.collectionSelector.label.listHeader',
-        })
-    );
-
     const collectionsArray = Array.from(collections);
-
-    const handlers = {
-        removeAllCollections: (event: React.MouseEvent<HTMLElement>) => {
-            event.stopPropagation();
-            removeAllCollections();
-        },
-    };
-
-    const columns: GridColDef[] = [
-        {
-            field: 'name',
-            flex: 1,
-            headerName: collectionsLabel,
-            sortable: false,
-            renderHeader: (params: GridColumnHeaderParams) => (
-                <Typography>{params.colDef.headerName}</Typography>
-            ),
-            renderCell: (params: GridRenderCellParams) => {
-                return (
-                    <CollectionSelectorRow
-                        collection={params.value}
-                        disabled={false}
-                        removeCollection={removeCollection}
-                    />
-                );
-            },
-            valueGetter: (params: GridValueGetterParams) => {
-                return params.row.name;
-            },
-        },
-    ];
-
-    const rows = collectionsArray.map((collection) => ({
-        id: collection,
-        name: collection,
-    }));
-
-    useUnmount(() => {
-        if (onSelectTimeOut.current) clearTimeout(onSelectTimeOut.current);
-    });
 
     return loading ? (
         <Box>{skeleton}</Box>
@@ -142,106 +63,15 @@ function CollectionSelector({
                 }}
             />
 
-            <Box
-                sx={{
-                    ml: 'auto',
-                    borderTop: defaultOutline[theme.palette.mode],
-                    borderLeft: defaultOutline[theme.palette.mode],
-                }}
-            >
-                <Stack
-                    direction="row"
-                    spacing={1}
-                    divider={
-                        RediscoverButton ? (
-                            <Divider
-                                orientation="vertical"
-                                variant="middle"
-                                flexItem
-                            />
-                        ) : null
-                    }
-                    sx={{
-                        justifyContent: 'right',
-                    }}
-                >
-                    {RediscoverButton ? RediscoverButton : null}
-
-                    <Button
-                        variant="text"
-                        disabled={readOnly ?? rows.length === 0}
-                        onClick={handlers.removeAllCollections}
-                        sx={{ borderRadius: 0 }}
-                    >
-                        <FormattedMessage id="workflows.collectionSelector.cta.delete" />
-                    </Button>
-                </Stack>
-            </Box>
-
-            <Box sx={{ height: height ?? 480 }}>
-                <DataGrid
-                    components={{
-                        NoRowsOverlay: SelectorEmpty,
-                    }}
-                    rows={rows}
-                    columns={columns}
-                    headerHeight={40}
-                    rowCount={rows.length}
-                    hideFooter
-                    disableColumnSelector
-                    disableSelectionOnClick={!selectionEnabled}
-                    onRowClick={
-                        selectionEnabled
-                            ? (params: any) => {
-                                  // This is hacky but it works. It clears out the
-                                  //  current collection before switching.
-                                  //  If a user is typing quickly in a form and then selects a
-                                  //  different binding VERY quickly it could cause the updates
-                                  //  to go into the wrong form.
-                                  setCurrentCollection(null);
-                                  onSelectTimeOut.current = window.setTimeout(
-                                      () => {
-                                          setCurrentCollection(params.row.name);
-                                      }
-                                  );
-                              }
-                            : undefined
-                    }
-                    initialState={initialState}
-                    sx={{
-                        'borderBottom': 'none',
-                        '& .MuiDataGrid-row ': {
-                            cursor: 'pointer',
-                        },
-                        '& .MuiDataGrid-cell': {
-                            borderBottom: defaultOutline[theme.palette.mode],
-                        },
-                        '& .MuiDataGrid-columnSeparator': {
-                            display: 'none',
-                        },
-                        '& .MuiDataGrid-columnHeaders': {
-                            borderTop: defaultOutline[theme.palette.mode],
-                            borderBottom: defaultOutline[theme.palette.mode],
-                            bgcolor:
-                                alternativeDataGridHeader[theme.palette.mode],
-                        },
-                        '& .MuiDataGrid-columnHeader:hover': {
-                            '& .MuiDataGrid-columnHeaderTitleContainerContent':
-                                {
-                                    mr: 0.5,
-                                },
-                            '& .MuiDataGrid-menuIcon': {
-                                width: '2rem',
-                            },
-                        },
-                        '& .MuiDataGrid-columnHeaderTitleContainerContent': {
-                            width: '100%',
-                            justifyContent: 'space-between',
-                            mr: 4.5,
-                        },
-                    }}
-                />
-            </Box>
+            <CollectionSelectorList
+                height={height}
+                collections={collections}
+                removeCollection={removeCollection}
+                removeAllCollections={removeAllCollections}
+                RediscoverButton={RediscoverButton}
+                currentCollection={currentCollection}
+                setCurrentCollection={setCurrentCollection}
+            />
         </>
     );
 }
