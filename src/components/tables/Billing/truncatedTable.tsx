d0f970b1243e58c425537578130a5b8d4f317598
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-03-28 10:20:55 -0400

    Add loading state to projected costs table

--- src/components/tables/Billing/truncatedTable.tsx
+++ src/components/tables/Billing/truncatedTable.tsx
@@ -8,12 +8,16 @@ import {
     TableRow,
 } from '@mui/material';
 import Rows from 'components/tables/Billing/Rows';
+import TableLoadingRows from 'components/tables/Loading';
 import { FormattedMessage } from 'react-intl';
+import { useBilling_billingDetails } from 'stores/Tables/Billing/hooks';
+import { TableColumns } from 'types';
+import { hasLength } from 'utils/misc-utils';
 
 // TODO: Determine if the details table column is necessary and, if so,
 //   what data should be displayed in that column. My proposition is that
 //   the tier evaluation for that month should be identified in that column.
-const columns = [
+const columns: TableColumns[] = [
     {
         field: 'month',
         headerIntlKey: 'admin.billing.projectedCostTable.label.month',
@@ -37,21 +41,31 @@ const columns = [
 ];
 
 function TruncatedBillingTable() {
+    const billingDetails = useBilling_billingDetails();
+
     return (
         <TableContainer component={Box}>
             <Table size="small">
                 <TableHead>
                     <TableRow>
-                        {columns.map((column, index) => (
-                            <TableCell key={`${column.field}-${index}`}>
-                                <FormattedMessage id={column.headerIntlKey} />
-                            </TableCell>
-                        ))}
+                        {columns.map((column, index) =>
+                            column.headerIntlKey ? (
+                                <TableCell key={`${column.field}-${index}`}>
+                                    <FormattedMessage
+                                        id={column.headerIntlKey}
+                                    />
+                                </TableCell>
+                            ) : null
+                        )}
                     </TableRow>
                 </TableHead>
 
                 <TableBody>
-                    <Rows />
+                    {hasLength(billingDetails) ? (
+                        <Rows />
+                    ) : (
+                        <TableLoadingRows columns={columns} />
+                    )}
                 </TableBody>
             </Table>
         </TableContainer>
