c3a825c792f911ddbff8b0b374a50a34299aad6b
Travis Jenkins <travis@estuary.dev>
2023-03-30 10:25:31 -0400

    missing content

--- src/lang/en-US.ts
+++ src/lang/en-US.ts
@@ -665,6 +665,7 @@ const DetailsPanel: ResolvedIntlConfig['messages'] = {
     'detailsPanel.logs.title': `Logs`,
     'detailsPanel.logs.notFound': `Logs for this build cannot be found. This is likely a permissions issue. You don't have permissions to view other users' logs by default.`,
     'detailsPanel.shardDetails.fetchError': `Unable to fetch shard status`,
+    'detailsPanel.shardDetails.docLink': `Docs`,
     'detailsPanel.shardDetails.title': `Shard Information`,
     'detailsPanel.shardDetails.status.label': `Status`,
     'detailsPanel.shardDetails.id.label': `ID`,
