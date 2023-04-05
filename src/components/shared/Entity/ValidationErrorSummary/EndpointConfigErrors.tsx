04e51fa4d9fd3ad558f2a4a7c166d5432a790638
Travis Jenkins <travis@estuary.dev>
2023-04-05 11:25:52 -0400

    We only need to display the custom errors if there are changes in the config

--- src/components/shared/Entity/ValidationErrorSummary/EndpointConfigErrors.tsx
+++ src/components/shared/Entity/ValidationErrorSummary/EndpointConfigErrors.tsx
@@ -1,24 +1,25 @@
 import SectionError from 'components/shared/Entity/ValidationErrorSummary/SectionError';
 import {
+    useEndpointConfigStore_changed,
     useEndpointConfigStore_endpointConfigErrors,
     useEndpointConfigStore_endpointCustomErrors,
     useEndpointConfigStore_endpointSchema,
 } from 'stores/EndpointConfig/hooks';
 
 function EndpointConfigErrors() {
+    const endpointConfigChanged = useEndpointConfigStore_changed();
+
     const endpointCustomErrors = useEndpointConfigStore_endpointCustomErrors();
     const endpointErrors = useEndpointConfigStore_endpointConfigErrors();
     const endpointSchema = useEndpointConfigStore_endpointSchema();
 
-    console.log('endpoint config error', {
-        endpointErrors,
-        endpointSchema,
-        endpointCustomErrors,
-    });
+    const allErrors = endpointConfigChanged()
+        ? endpointCustomErrors.concat(endpointErrors)
+        : endpointErrors;
 
     return (
         <SectionError
-            errors={endpointCustomErrors.concat(endpointErrors)}
+            errors={allErrors}
             config={endpointSchema}
             configEmptyMessage="workflows.error.endpointConfig.empty"
             title="entityCreate.endpointConfig.endpointConfigHaveErrors"
