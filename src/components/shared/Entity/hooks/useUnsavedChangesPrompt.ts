c4f4c86aea6522e5ca396287650dda41a9ff1202
Travis Jenkins <travis@estuary.dev>
2023-04-06 22:05:42 -0400

    Need to pass in the message key and not the translated one

--- src/components/shared/Entity/hooks/useUnsavedChangesPrompt.ts
+++ src/components/shared/Entity/hooks/useUnsavedChangesPrompt.ts
@@ -1,14 +1,10 @@
 import { usePrompt } from 'hooks/useBlocker';
-import { useIntl } from 'react-intl';
 import { useUnmount } from 'react-use';
 
 export default function useUnsavedChangesPrompt(
     when: boolean,
     callback: Function
 ) {
-    const intl = useIntl();
-    const message = intl.formatMessage({ id: 'confirm.loseData' });
-
-    usePrompt(message, when);
+    usePrompt('confirm.loseData', when);
     useUnmount(() => callback());
 }
