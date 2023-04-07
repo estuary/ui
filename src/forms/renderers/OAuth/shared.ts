59f5d0f6d362895e1f25cfd1f386cff0ae11b8eb
Travis Jenkins <travis@estuary.dev>
2023-04-06 22:05:05 -0400

    Renaming and storing off values that MUST match the server

--- src/forms/renderers/OAuth/shared.ts
+++ src/forms/renderers/OAuth/shared.ts
@@ -1,11 +1,14 @@
 export const NO_PROVIDER = 'noProviderFound';
 export const CLIENT_ID = 'client_id';
 export const CLIENT_SECRET = 'client_secret';
-export const INJECTED = '_injectedDuringEncryption_'; //MUST stay in sync with animate-carnival/supabase/functions/oauth/encrypt-config.ts
+
+//MUST stay in sync with animate-carnival/supabase/functions/oauth/encrypt-config.ts
+export const CREDENTIALS = 'credentials';
+export const INJECTED = '_injectedDuringEncryption_';
 
 // These are injected by the server/encryption call so just setting
 //      some value here to pass the validation
-export const FAKE_DEFAULTS = {
+export const INJECTED_VALUES = {
     [CLIENT_ID]: INJECTED,
     [CLIENT_SECRET]: INJECTED,
 };
