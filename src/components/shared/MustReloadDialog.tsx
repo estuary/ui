87031d765c071b89117de441eaccd626d5107f2f
Travis Jenkins <travis@estuary.dev>
2023-04-21 18:06:42 -0400

    Breaking up to make this is a bit nicer

---
+++ src/components/shared/MustReloadDialog.tsx
@@ -0,0 +1,63 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Typography,
+} from '@mui/material';
+import { FormattedMessage } from 'react-intl';
+import { useNavigate } from 'react-router-dom';
+import AlertBox from './AlertBox';
+
+const ARIA_DESC_ID = 'chunkNotFetched-dialog-description';
+const ARIA_LABEL_ID = 'chunkNotFetched-dialog-title';
+
+function MustReloadDialog() {
+    const navigate = useNavigate();
+
+    const reloadPage = () => {
+        navigate(0);
+    };
+
+    return (
+        <Dialog
+            open
+            aria-labelledby={ARIA_LABEL_ID}
+            aria-describedby={ARIA_DESC_ID}
+        >
+            <DialogTitle id={ARIA_LABEL_ID}>
+                <FormattedMessage id="errorBoundry.chunkNotFetched.dialog.title" />
+            </DialogTitle>
+            <DialogContent>
+                <AlertBox
+                    short
+                    severity="error"
+                    title={
+                        <FormattedMessage id="errorBoundry.chunkNotFetched.error.title" />
+                    }
+                >
+                    <DialogContentText id={ARIA_DESC_ID}>
+                        <Typography>
+                            <FormattedMessage id="errorBoundry.chunkNotFetched.error.message1" />
+                        </Typography>
+                        <Typography>
+                            <FormattedMessage id="errorBoundry.chunkNotFetched.error.message2" />
+                        </Typography>
+                        <Typography>
+                            <FormattedMessage id="errorBoundry.chunkNotFetched.error.instructions" />
+                        </Typography>
+                    </DialogContentText>
+                </AlertBox>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={reloadPage}>
+                    <FormattedMessage id="cta.reload" />
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default MustReloadDialog;
