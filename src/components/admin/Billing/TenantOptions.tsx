5b2e4025b20cf5bf07aaea5c0a67f35cf70675b7
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-04-30 13:31:29 -0400

    Reset billing select table state when selected tenant changes

--- src/components/admin/Billing/TenantOptions.tsx
+++ src/components/admin/Billing/TenantOptions.tsx
@@ -1,16 +1,36 @@
 import { Skeleton } from '@mui/material';
 import AutocompletedField from 'components/shared/toolbar/AutocompletedField';
+import { useZustandStore } from 'context/Zustand/provider';
 import { useIntl } from 'react-intl';
 import {
     useBilling_setSelectedTenant,
     useBilling_tenants,
 } from 'stores/Billing/hooks';
+import { SelectTableStoreNames } from 'stores/names';
+import {
+    useBillingTable_setHydrated,
+    useBillingTable_setHydrationErrorsExist,
+} from 'stores/Tables/Billing/hooks';
+import {
+    SelectableTableStore,
+    selectableTableStoreSelectors,
+} from 'stores/Tables/Store';
 
 function TenantOptions() {
     const intl = useIntl();
 
+    // Billing Store
     const tenants = useBilling_tenants();
     const setSelectedTenant = useBilling_setSelectedTenant();
+
+    // Billing Select Table Store
+    const setHydrated = useBillingTable_setHydrated();
+    const setHydrationErrorsExist = useBillingTable_setHydrationErrorsExist();
+
+    const resetBillingSelectTableState = useZustandStore<
+        SelectableTableStore,
+        SelectableTableStore['resetState']
+    >(SelectTableStoreNames.BILLING, selectableTableStoreSelectors.state.reset);
 
     return tenants.length > 0 ? (
         <AutocompletedField
@@ -19,7 +39,13 @@ function TenantOptions() {
             })}
             options={tenants}
             defaultValue={tenants[0]}
-            changeHandler={(_event, value) => setSelectedTenant(value)}
+            changeHandler={(_event, value) => {
+                setSelectedTenant(value);
+
+                resetBillingSelectTableState();
+                setHydrated(false);
+                setHydrationErrorsExist(false);
+            }}
             autocompleteSx={{ flexGrow: 1 }}
         />
     ) : (
