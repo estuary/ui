a755be6b51b15b86de3e38686a23d9174062a351
Travis Jenkins <travis@estuary.dev>
2023-05-18 14:47:05 -0400

    Allow title to be whatever you want it to be.

--- src/components/tables/Title.tsx
+++ src/components/tables/Title.tsx
@@ -1,25 +1,35 @@
 import { Typography } from '@mui/material';
+import { ReactNode } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 interface Props {
-    header: string;
+    header: string | ReactNode;
     marginBottom?: number;
 }
 
+// Allowing header to be a node _kinda_ is doing what the
+//  toolbar property for the table already did. However,
+//  the toolbar props felt like it was meant for _toolbars_
+//  and now a title that is styled different.
 function Title({ header, marginBottom }: Props) {
-    return (
-        <Typography
-            component="span"
-            sx={{
-                mb: marginBottom,
-                alignItems: 'center',
-                fontSize: 18,
-                fontWeight: '400',
-            }}
-        >
-            <FormattedMessage id={header} />
-        </Typography>
-    );
+    if (typeof header === 'string') {
+        return (
+            <Typography
+                component="span"
+                sx={{
+                    mb: marginBottom,
+                    alignItems: 'center',
+                    fontSize: 18,
+                    fontWeight: '400',
+                }}
+            >
+                <FormattedMessage id={header} />
+            </Typography>
+        );
+    }
+
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    return <>{header}</>;
 }
 
 export default Title;
