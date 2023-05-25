9adb9fde3223f4adf7fc32a36167c80e9e7ab992
Travis Jenkins <travis@estuary.dev>
2023-05-25 17:50:39 -0400

    Adding log rocket reporting to this due to the severity of this being displayed

--- src/components/fullPage/Error.tsx
+++ src/components/fullPage/Error.tsx
@@ -5,6 +5,8 @@ import { ErrorDetails } from 'components/shared/Error/types';
 import FullPageWrapper from 'directives/FullPageWrapper';
 import { ReactElement } from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useMount } from 'react-use';
+import { CustomEvents, logRocketEvent } from 'services/logrocket';
 
 interface Props {
     error: ErrorDetails;
@@ -12,6 +14,10 @@ interface Props {
     title?: ReactElement | string;
 }
 function FullPageError({ error, message, title }: Props) {
+    useMount(() => {
+        logRocketEvent(CustomEvents.FULL_PAGE_ERROR_DISPLAYED);
+    });
+
     return (
         <FullPageWrapper>
             <Stack spacing={2}>
