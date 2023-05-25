389486344853e0fec0cd8b8beb5b07325e6cd45d
Travis Jenkins <travis@estuary.dev>
2023-05-25 12:19:14 -0400

    Wiring up full page error for guards

--- src/app/guards/LegalGuard.tsx
+++ src/app/guards/LegalGuard.tsx
@@ -1,18 +1,41 @@
+import FullPageError from 'components/fullPage/Error';
 import FullPageSpinner from 'components/fullPage/Spinner';
 import ClickToAccept from 'directives/ClickToAccept';
 import FullPageWrapper from 'directives/FullPageWrapper';
+import { FormattedMessage } from 'react-intl';
 import { BaseComponentProps } from 'types';
 import useDirectiveGuard from './hooks';
 
 const SELECTED_DIRECTIVE = 'clickToAccept';
 
 function LegalGuard({ children }: BaseComponentProps) {
-    const { directive, loading, status, mutate } =
+    const { directive, error, loading, status, mutate } =
         useDirectiveGuard(SELECTED_DIRECTIVE);
 
     if (loading || status === null) {
         return <FullPageSpinner />;
-    } else if (status !== 'fulfilled') {
+    }
+
+    if (error) {
+        return (
+            <FullPageError
+                error={error}
+                message={
+                    <FormattedMessage
+                        id="legal.error.failedToFetch.message"
+                        values={{
+                            privacy: (
+                                <FormattedMessage id="legal.docs.privacy" />
+                            ),
+                            terms: <FormattedMessage id="legal.docs.terms" />,
+                        }}
+                    />
+                }
+            />
+        );
+    }
+
+    if (status !== 'fulfilled') {
         return (
             <FullPageWrapper>
                 <ClickToAccept
