16a7ce5b130fc27af1a9511b0ed38f3bc33e7653
Phil <phil@estuary.dev>
2023-06-05 19:33:58 -0400

    omit new_name when evolving collections
    Updates the collection evolutions handler to no longer specify a
    `new_name` when evolving collections. This is a temporary change, which
    will remain in place until we implement a more comprehensive dialog for
    showing the proposed changes. In the meantime, this was a convenient way
    to opt in to the new behavior of the evolutions handler, which will now
    skip re-creating collections that have the `x-infer-schema` attribute in
    favor of only updating the materialization bindings to use a new
    resource name.

--- src/utils/name-utils.ts
+++ src/utils/name-utils.ts
@@ -1,4 +1,9 @@
 const COLLECTION_VERSION_RE = new RegExp('.*[_-][vV](\\d+)$');
+
+// This function is currently unused, as we only pass the `old_name` to the evolutions handler.
+// In the future, we'd like to present a more comprehensive message, with a suggested action for each
+// incompatible collection, and at that point this will once again be needed. This function will also
+// be needed by another future feature allowing users to manually re-create collections.
 export const suggestedName = (oldName: string) => {
     const regExMatch = COLLECTION_VERSION_RE.exec(oldName);
 
@@ -17,7 +22,10 @@ export const incrementCollectionNames = (collections: string[]) => {
     return collections.map((collectionName) => {
         return {
             old_name: collectionName,
-            new_name: suggestedName(collectionName),
+            // Leave `new_name` unset for now, which will allow the evolutions
+            // handler to only re-create materialization bindings in cases where
+            // collections use the `x-infer-schema` attribute.
+            // new_name: suggestedName(collectionName),
         };
     });
 };
