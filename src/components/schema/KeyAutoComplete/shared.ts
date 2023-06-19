2626413f6e16ce731c920abc92e01a654f454c64
Travis Jenkins <travis@estuary.dev>
2023-06-19 18:55:27 -0400

    Fixing the key validation check

--- src/components/schema/KeyAutoComplete/shared.ts
+++ src/components/schema/KeyAutoComplete/shared.ts
@@ -1,3 +1,7 @@
 export const keyIsValidOption = (options: any[], tagValue: string) => {
-    return Boolean(options.find((option: any) => tagValue === option.pointer));
+    return Boolean(
+        options.find(
+            (option: any) => tagValue === option.pointer || tagValue === option
+        )
+    );
 };
