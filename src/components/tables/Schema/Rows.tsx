a7113938cc0aa84481760bf31495c273c77fc613
Travis Jenkins <travis@estuary.dev>
2023-06-21 16:48:50 -0400

    Getting sorting working for putting special characters at the end

--- src/components/tables/Schema/Rows.tsx
+++ src/components/tables/Schema/Rows.tsx
@@ -1,5 +1,4 @@
 import { TableCell, TableRow, Typography } from '@mui/material';
-import { orderBy } from 'lodash';
 import { Schema, SortDirection } from 'types';
 import ChipList from '../cells/ChipList';
 
@@ -40,11 +39,33 @@ function Rows({ data, sortDirection, columnToSort }: RowsProps) {
 
     return (
         <>
-            {orderBy(data, [columnToSort], [sortDirection]).map(
-                (record: any, index: number) => (
+            {data
+                .sort((first: any, second: any) => {
+                    // Fetch the fields we want to sort
+                    const a = first[columnToSort];
+                    const b = second[columnToSort];
+
+                    // See if the values start with alphanumeric
+                    const aIsAlphabetical = a.localeCompare('a') >= 0;
+                    const bIsAlphabetical = b.localeCompare('a') >= 0;
+
+                    if (!aIsAlphabetical && bIsAlphabetical) {
+                        return 1;
+                    }
+
+                    if (aIsAlphabetical && !bIsAlphabetical) {
+                        return -1;
+                    }
+
+                    if (sortDirection === 'asc') {
+                        return a.localeCompare(b);
+                    }
+
+                    return b.localeCompare(a);
+                })
+                .map((record: any, index: number) => (
                     <Row row={record} key={`schema-table-rows-${index}`} />
-                )
-            )}
+                ))}
         </>
     );
 }
