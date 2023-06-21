3f09c4c5873a41bf473e1ace6c36d26fcc94095f
Travis Jenkins <travis@estuary.dev>
2023-06-21 10:37:01 -0400

    Taking out unused part of the Zustand Provider

--- src/context/Zustand/provider.tsx
+++ src/context/Zustand/provider.tsx
@@ -1,28 +1,14 @@
 import { createContext as createReactContext, useContext } from 'react';
 import { StoreName } from 'stores/names';
-import useConstant from 'use-constant';
 import { StoreApi, useStore } from 'zustand';
 import invariableStores from './invariableStores';
 import { ZustandProviderProps } from './types';
 
 export const ZustandContext = createReactContext<any | null>(null);
 
-export const ZustandProvider = ({
-    children,
-    storeSlice,
-}: ZustandProviderProps) => {
-    const storeOptions = useConstant(() => {
-        if (storeSlice) {
-            const { storeName, createStore } = storeSlice;
-
-            return { [storeName]: createStore };
-        } else {
-            return invariableStores;
-        }
-    });
-
+export const ZustandProvider = ({ children }: ZustandProviderProps) => {
     return (
-        <ZustandContext.Provider value={storeOptions}>
+        <ZustandContext.Provider value={invariableStores}>
             {children}
         </ZustandContext.Provider>
     );
