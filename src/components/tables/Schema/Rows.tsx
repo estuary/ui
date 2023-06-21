404fc1c3f25f27612b6db8e5462d9b0a6b37a23a
Travis Jenkins <travis@estuary.dev>
2023-06-21 16:51:00 -0400

    Making the name field special for sorting

--- src/components/tables/Schema/Rows.tsx
+++ src/components/tables/Schema/Rows.tsx
@@ -1,4 +1,5 @@
 import { TableCell, TableRow, Typography } from '@mui/material';
+import { orderBy } from 'lodash';
 import { Schema, SortDirection } from 'types';
 import ChipList from '../cells/ChipList';
 
@@ -37,35 +38,48 @@ function Rows({ data, sortDirection, columnToSort }: RowsProps) {
         return null;
     }
 
+    // We only do special sorting for name - otherwise we can use lodash
+    if (columnToSort === 'name') {
+        return (
+            <>
+                {data
+                    .sort((first: any, second: any) => {
+                        // Fetch the fields we want to sort
+                        const a = first[columnToSort];
+                        const b = second[columnToSort];
+
+                        // See if the values start with alphanumeric
+                        const aIsAlphabetical = a.localeCompare('a') >= 0;
+                        const bIsAlphabetical = b.localeCompare('a') >= 0;
+
+                        if (!aIsAlphabetical && bIsAlphabetical) {
+                            return 1;
+                        }
+
+                        if (aIsAlphabetical && !bIsAlphabetical) {
+                            return -1;
+                        }
+
+                        if (sortDirection === 'asc') {
+                            return a.localeCompare(b);
+                        }
+
+                        return b.localeCompare(a);
+                    })
+                    .map((record: any, index: number) => (
+                        <Row row={record} key={`schema-table-rows-${index}`} />
+                    ))}
+            </>
+        );
+    }
+
     return (
         <>
-            {data
-                .sort((first: any, second: any) => {
-                    // Fetch the fields we want to sort
-                    const a = first[columnToSort];
-                    const b = second[columnToSort];
-
-                    // See if the values start with alphanumeric
-                    const aIsAlphabetical = a.localeCompare('a') >= 0;
-                    const bIsAlphabetical = b.localeCompare('a') >= 0;
-
-                    if (!aIsAlphabetical && bIsAlphabetical) {
-                        return 1;
-                    }
-
-                    if (aIsAlphabetical && !bIsAlphabetical) {
-                        return -1;
-                    }
-
-                    if (sortDirection === 'asc') {
-                        return a.localeCompare(b);
-                    }
-
-                    return b.localeCompare(a);
-                })
-                .map((record: any, index: number) => (
+            {orderBy(data, [columnToSort], [sortDirection]).map(
+                (record: any, index: number) => (
                     <Row row={record} key={`schema-table-rows-${index}`} />
-                ))}
+                )
+            )}
         </>
     );
 }
