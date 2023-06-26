f47d30a8da56928134f7a9d400f4fb2497fbf191
Travis Jenkins <travis@estuary.dev>
2023-06-26 09:46:49 -0400

    Changing how we handle user details

--- src/components/menus/UserMenu.tsx
+++ src/components/menus/UserMenu.tsx
@@ -3,14 +3,11 @@ import { Stack, SxProps, Typography } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import MenuItem from '@mui/material/MenuItem';
-import { Auth } from '@supabase/ui';
 import UserAvatar from 'components/shared/UserAvatar';
 import { useClient } from 'hooks/supabase-swr';
+import useUserDetails from 'hooks/useUserDetails';
 import { LogOut, Mail, ProfileCircle } from 'iconoir-react';
-import { useMemo } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
-import { logRocketConsole } from 'services/logrocket';
-import { getUserDetails } from 'services/supabase';
 import IconMenu from './IconMenu';
 
 interface Props {
@@ -26,12 +23,7 @@ const nonInteractiveMenuStyling: SxProps = {
 const UserMenu = ({ iconColor }: Props) => {
     const intl = useIntl();
     const supabaseClient = useClient();
-
-    const { user } = Auth.useUser();
-    const { userName, email, emailVerified, avatar } = useMemo(() => {
-        logRocketConsole('fetching user details for menu', user);
-        return getUserDetails(user);
-    }, [user]);
+    const { userName, email, emailVerified, avatar } = useUserDetails();
 
     const handlers = {
         logout: async () => {
