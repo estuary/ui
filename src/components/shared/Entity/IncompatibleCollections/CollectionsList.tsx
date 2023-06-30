deecca08431e37f11241a2b2b52ab6e2781d5aa8
Travis Jenkins <travis@estuary.dev>
2023-06-30 14:12:38 -0400

    PR: Pulling out description into a component

--- src/components/shared/Entity/IncompatibleCollections/CollectionsList.tsx
+++ src/components/shared/Entity/IncompatibleCollections/CollectionsList.tsx
@@ -3,16 +3,16 @@ import { IncompatibleCollections } from 'api/evolutions';
 import { useBindingsEditorStore_incompatibleCollections } from 'components/editor/Bindings/Store/hooks';
 import KeyValueList from 'components/shared/KeyValueList';
 import { HelpCircle } from 'iconoir-react';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import { hasLength } from 'utils/misc-utils';
 import { suggestedName } from 'utils/name-utils';
+import Description from './Description';
 
 interface CollectionActionProps {
     incompatibleCollection: IncompatibleCollections;
 }
 
 function CollectionAction({ incompatibleCollection }: CollectionActionProps) {
-    const intl = useIntl();
     const recreateCause = hasLength(incompatibleCollection.requires_recreation)
         ? incompatibleCollection.requires_recreation[0]
         : null;
@@ -21,45 +21,9 @@ function CollectionAction({ incompatibleCollection }: CollectionActionProps) {
             ? suggestedName(incompatibleCollection.collection)
             : null;
 
-    let desc = null;
-    if (newName !== null) {
-        const reason = intl.formatMessage({
-            id: `entityEvolution.action.recreateCollection.reason.${recreateCause}`,
-        });
-        desc = (
-            <FormattedMessage
-                id="entityEvolution.action.recreateCollection.description"
-                values={{ newName, reason }}
-            />
-        );
-    } else if (incompatibleCollection.affected_materializations?.length === 1) {
-        // We _could_ also provide a reason as part of these. We're not doing so now because the `reason` that's attached to the fields on the AffectedConsumer
-        // are way too verbose. We'd need to update connectors to provide much more concise reasons before we present them here. Once/if we do that, then we
-        // should be able to format a message using the fields and reasons for each one.
-        desc = (
-            <FormattedMessage
-                id="entityEvolution.action.recreateOneBinding.description"
-                values={{
-                    materializationName:
-                        incompatibleCollection.affected_materializations[0]
-                            .name,
-                }}
-            />
-        );
-    } else {
-        desc = (
-            <FormattedMessage
-                id="entityEvolution.action.recreateBindings.description"
-                values={{
-                    materializationCount:
-                        incompatibleCollection.affected_materializations
-                            ?.length,
-                }}
-            />
-        );
-    }
     const helpMessage =
         newName !== null ? 'recreateCollection' : 'recreateBindings';
+
     return (
         <Box
             sx={{
@@ -70,7 +34,15 @@ function CollectionAction({ incompatibleCollection }: CollectionActionProps) {
                 alignItems: 'center',
             }}
         >
-            <Typography color="text.primary">{desc}</Typography>
+            <Typography color="text.primary">
+                <Description
+                    affectedMaterializations={
+                        incompatibleCollection.affected_materializations
+                    }
+                    newName={newName}
+                    recreateCause={recreateCause}
+                />
+            </Typography>
             <Tooltip
                 title={
                     <FormattedMessage
