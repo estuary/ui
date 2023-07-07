1d94ce4a45d405256833c5cb43894f211d83cb6e
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-07-07 09:44:07 -0400

    Repair the monaco editor in the details panel

--- src/components/editor/LiveSpec.tsx
+++ src/components/editor/LiveSpec.tsx
@@ -1,13 +1,26 @@
 import EditorWithFileSelector from 'components/editor/EditorWithFileSelector';
+import { useEditorStore_currentCatalog } from 'components/editor/Store/hooks';
+import { useMemo } from 'react';
+import { stringifyJSON } from 'services/stringify';
 
 interface Props {
     localZustandScope: boolean;
 }
 
 function LiveSpecEditor({ localZustandScope }: Props) {
+    const currentCatalog = useEditorStore_currentCatalog({
+        localScope: localZustandScope,
+    });
+
+    const specAsString = useMemo(
+        () => stringifyJSON(currentCatalog?.spec ?? null),
+        [currentCatalog?.spec]
+    );
+
     return (
         <EditorWithFileSelector
             localZustandScope={localZustandScope}
+            defaultValue={specAsString}
             disabled={true}
         />
     );
