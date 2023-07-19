57c3a842aab42c3b777682c499e32f80cab5231f
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-07-19 10:03:50 -0400

    Apply semantic highlighting to translated to constraint types

--- src/components/tables/cells/fieldSelection/ConstraintDetails.tsx
+++ src/components/tables/cells/fieldSelection/ConstraintDetails.tsx
@@ -9,31 +9,41 @@ interface Props {
     constraint: TranslatedConstraint;
 }
 
-const getConstraintMessageId = (constraintType: ConstraintTypes): string => {
+const getConstraintHeaderSettings = (
+    constraintType: ConstraintTypes
+): [string, 'success' | 'warning' | 'error'] => {
     switch (constraintType) {
         case ConstraintTypes.FIELD_REQUIRED:
-            return 'fieldSelection.table.label.fieldRequired';
+            return ['fieldSelection.table.label.fieldRequired', 'success'];
         case ConstraintTypes.LOCATION_REQUIRED:
-            return 'fieldSelection.table.label.locationRequired';
+            return ['fieldSelection.table.label.locationRequired', 'success'];
         case ConstraintTypes.LOCATION_RECOMMENDED:
-            return 'fieldSelection.table.label.locationRecommended';
+            return [
+                'fieldSelection.table.label.locationRecommended',
+                'warning',
+            ];
         case ConstraintTypes.FIELD_OPTIONAL:
-            return 'fieldSelection.table.label.fieldOptional';
+            return ['fieldSelection.table.label.fieldOptional', 'warning'];
         case ConstraintTypes.FIELD_FORBIDDEN:
-            return 'fieldSelection.table.label.fieldForbidden';
+            return ['fieldSelection.table.label.fieldForbidden', 'error'];
         case ConstraintTypes.UNSATISFIABLE:
-            return 'fieldSelection.table.label.unsatisfiable';
+            return ['fieldSelection.table.label.unsatisfiable', 'error'];
         default:
-            return 'fieldSelection.table.label.unknown';
+            return ['fieldSelection.table.label.unknown', 'error'];
     }
 };
 
 function ConstraintDetails({ constraint }: Props) {
-    const messageId = getConstraintMessageId(constraint.type);
+    const [messageId, textColor] = getConstraintHeaderSettings(constraint.type);
 
     return (
         <TableCell sx={{ minWidth: 275 }}>
-            <Typography sx={{ fontWeight: 500 }}>
+            <Typography
+                sx={{
+                    fontWeight: 500,
+                    color: (theme) => theme.palette[textColor].main,
+                }}
+            >
                 <FormattedMessage id={messageId} />
             </Typography>
 
