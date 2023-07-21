657081d37d088faed0ec96b5364c138f74d9650d
Travis Jenkins <travis@estuary.dev>
2023-07-21 17:25:51 -0400

    Adding import grouping/sorting to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,14 +9,58 @@ module.exports = {
         project: './tsconfig.json',
         tsconfigRootDir: __dirname,
     },
+    plugins: ['unused-imports', 'simple-import-sort', 'import'],
     rules: {
-        // Want to make sure imports are always formatted correctly
-        'sort-imports': [
+        // Want to make sure imports and exports are always formatted correctly
+        'unused-imports/no-unused-imports': 'error',
+        'simple-import-sort/imports': [
             'error',
             {
-                ignoreCase: true,
+                groups: [
+                    // Side effect imports.
+                    ['^\\u0000'],
+
+                    // React stuff
+                    ['^react'],
+
+                    // Single word imports... hope this covers most external packages that are not scoped
+                    ['^(\\w)+$', '^[a-zA-Z0-9-_]+$'],
+
+                    //  Scoped Packages
+                    ['^@(\\w)+'],
+
+                    // Group some internal stuff together
+                    ['^api/'],
+                    ['^app/'],
+                    ['^components/'],
+                    ['^context/'],
+                    ['^directives/'],
+                    ['^fonts/'],
+                    ['^forms/'],
+                    ['^hooks/'],
+                    ['^icons/'],
+                    ['^images/'],
+                    ['^lang/'],
+                    ['^pages/'],
+                    ['^polyfills/'],
+                    ['^services/'],
+                    ['^stores/'],
+                    ['^types/'],
+                    ['^utils/'],
+
+                    // Absolute imports and other imports such `@/foo`.
+                    // Anything not matched in another group.
+                    ['^'],
+
+                    // Relative imports.
+                    // Anything that starts with a dot.
+                    ['^\\.'],
+                ],
             },
         ],
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
 
         // Only turning off right now to see more actual issues
         '@typescript-eslint/no-explicit-any': 'off',
