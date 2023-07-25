c780810cf5e29ab7c0386004dfed45f6a9a4d4f0
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-07-25 17:12:30 -0400

    Hide field selection behind URL param

--- src/components/collection/ResourceConfig.tsx
+++ src/components/collection/ResourceConfig.tsx
@@ -2,6 +2,10 @@ import { Box, Typography } from '@mui/material';
 import ResourceConfigForm from 'components/collection/ResourceConfigForm';
 import FieldSelectionViewer from 'components/editor/Bindings/FieldSelection';
 import { useEntityType } from 'context/EntityContext';
+import useGlobalSearchParams, {
+    GlobalSearchParams,
+} from 'hooks/searchParams/useGlobalSearchParams';
+import { useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useResourceConfig_hydrated } from 'stores/ResourceConfig/hooks';
 import { BindingsEditorConfigSkeleton } from './CollectionSkeletons';
@@ -11,10 +15,22 @@ interface Props {
     readOnly?: boolean;
 }
 
+// TODO: (field selection): This is a way to "hide" the field selection view from a user
+//  but allow us to test the feature out in prod. Remove when the feature is launched.
+const hiddenSearchParam = 'please_show';
+
 function ResourceConfig({ collectionName, readOnly = false }: Props) {
     const entityType = useEntityType();
 
     const hydrated = useResourceConfig_hydrated();
+
+    const fieldSelectionFlag = useGlobalSearchParams(
+        GlobalSearchParams.HIDDEN_FIELD_SELECTION
+    );
+    const showFieldSelection = useMemo(
+        () => fieldSelectionFlag === hiddenSearchParam,
+        [fieldSelectionFlag]
+    );
 
     return (
         <>
@@ -33,7 +49,7 @@ function ResourceConfig({ collectionName, readOnly = false }: Props) {
                 )}
             </Box>
 
-            {entityType === 'materialization' ? (
+            {showFieldSelection && entityType === 'materialization' ? (
                 <FieldSelectionViewer collectionName={collectionName} />
             ) : null}
         </>
