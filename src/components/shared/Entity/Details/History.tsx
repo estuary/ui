f9b04881f75dbb5d336c250764d4abd231dab2e1
Travis Jenkins <travis@estuary.dev>
2023-08-01 15:44:25 -0400

    Some quick wiring up for change compare

--- src/components/shared/Entity/Details/History.tsx
+++ src/components/shared/Entity/Details/History.tsx
@@ -1,71 +1,60 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 import { DiffEditor } from '@monaco-editor/react';
 import {
     Box,
     CircularProgress,
     Grid,
     List,
-    ListItem,
+    ListItemButton,
+    ListItemText,
+    Stack,
     useTheme,
 } from '@mui/material';
-import { PublicationSpecsExt_PublicationHistory } from 'api/publicationSpecsExt';
-import KeyValueList from 'components/shared/KeyValueList';
-import Tile from 'components/shared/Tile';
 import { monacoEditorComponentBackground } from 'context/Theme';
-import { addDays, endOfMonth, format, startOfMonth, subDays } from 'date-fns';
+import { format } from 'date-fns';
 import useGlobalSearchParams, {
     GlobalSearchParams,
 } from 'hooks/searchParams/useGlobalSearchParams';
 import usePublicationSpecsExt_History from 'hooks/usePublicationSpecsExt';
 import { findIndex } from 'lodash';
-import { useMemo, useState } from 'react';
-import { useIntl } from 'react-intl';
+import { useEffect, useMemo, useState } from 'react';
 import { stringifyJSON } from 'services/stringify';
+import { hasLength } from 'utils/misc-utils';
 
 const CARD_DATE_FORMAT = `EEEE, MMM do, yyyy 'at' hh:mm:ss aa`;
-const CALENDAR_DATE_FORMAT = 'yyyy-MM-dd';
 
-const convertData = (
-    publications: PublicationSpecsExt_PublicationHistory[]
-) => {
-    const response: { [key: string]: number } = {}; // ScatterDataItemOption
-
-    publications.forEach((publication) => {
-        const formattedPublishDate = format(
-            new Date(publication.published_at),
-            CALENDAR_DATE_FORMAT
-        );
-
-        response[formattedPublishDate] = !response[formattedPublishDate]
-            ? 1
-            : response[formattedPublishDate] + 1;
-    });
-
-    return Object.entries(response);
+const formatDate = (date?: string) => {
+    return date ? format(new Date(date), CARD_DATE_FORMAT) : 'unknown';
 };
+
+// TODO (details history) this was used to format data into something a eCharts heatmap could use
+// const convertData = (
+//     publications: PublicationSpecsExt_PublicationHistory[]
+// ) => {
+//     const response: { [key: string]: number } = {}; // ScatterDataItemOption
+
+//     publications.forEach((publication) => {
+//         const formattedPublishDate = format(
+//             new Date(publication.published_at),
+//             CALENDAR_DATE_FORMAT
+//         );
+
+//         response[formattedPublishDate] = !response[formattedPublishDate]
+//             ? 1
+//             : response[formattedPublishDate] + 1;
+//     });
+
+//     return Object.entries(response);
+// };
 
 function History() {
     const theme = useTheme();
-    const intl = useIntl();
     const catalogName = useGlobalSearchParams(GlobalSearchParams.CATALOG_NAME);
 
     const { publications, isValidating } =
         usePublicationSpecsExt_History(catalogName);
 
     const [selectedPublication, setSelectedPublication] = useState<string>('');
-
-    const convertedData = useMemo(() => {
-        if (publications && publications.length > 0) {
-            const data = convertData(publications);
-
-            return {
-                data,
-                start: startOfMonth(subDays(new Date(data[0][0]), 1)),
-                end: endOfMonth(addDays(new Date(data[data.length - 1][0]), 1)),
-            };
-        }
-
-        return null;
-    }, [publications]);
 
     const [currentSpec, previousSpec] = useMemo(() => {
         if (publications) {
@@ -74,33 +63,39 @@ function History() {
             });
 
             if (currIndex > -1) {
-                const currValue = stringifyJSON(publications[currIndex].spec);
-                const prevValue = publications[currIndex + 1]
-                    ? stringifyJSON(publications[currIndex + 1].spec)
-                    : '';
+                const currValue = publications[currIndex];
+                const prevValue = publications[currIndex + 1];
                 return [currValue, prevValue];
             }
         }
 
-        return ['', ''];
+        return [null, null];
     }, [publications, selectedPublication]);
 
-    if (isValidating || !publications || !convertedData) {
+    useEffect(() => {
+        if (
+            selectedPublication === '' &&
+            publications &&
+            hasLength(publications)
+        ) {
+            const defaultCur = publications[0];
+            const defaultPubId = defaultCur.pub_id;
+
+            setSelectedPublication(defaultPubId);
+        }
+    }, [publications, selectedPublication]);
+
+    if (isValidating || !publications) {
         return <CircularProgress />;
     }
-
-    console.log('history', {
-        publications,
-        convertedData,
-    });
 
     return (
         <Box>
             <Grid container spacing={2}>
-                <Grid item xs={3}>
+                <Grid item xs={12} md={3}>
                     <List>
                         {publications.map((publication) => (
-                            <ListItem
+                            <ListItemButton
                                 key={`history-timeline-${publication.pub_id}`}
                                 onClick={() =>
                                     setSelectedPublication(publication.pub_id)
@@ -109,46 +104,49 @@ function History() {
                                     selectedPublication === publication.pub_id
                                 }
                             >
-                                <Tile>
-                                    <KeyValueList
-                                        data={[
-                                            {
-                                                title: intl.formatMessage({
-                                                    id: 'data.published_at',
-                                                }),
-                                                val: format(
-                                                    new Date(
-                                                        publication.published_at
-                                                    ),
-                                                    CARD_DATE_FORMAT
-                                                ),
-                                            },
-                                        ]}
-                                    />
-                                </Tile>
-                            </ListItem>
+                                <ListItemText>
+                                    <Stack>
+                                        <Box>
+                                            {formatDate(
+                                                publication.published_at
+                                            )}
+                                        </Box>
+                                        <Box>{publication.user_email}</Box>
+                                    </Stack>
+                                </ListItemText>
+                            </ListItemButton>
                         ))}
                     </List>
                 </Grid>
-                <Grid item xs={12}>
-                    <Grid container>
-                        <Grid item xs={6}>
-                            Old
+                {hasLength(publications) && currentSpec ? (
+                    <Grid item xs={12} md={9}>
+                        <Grid container>
+                            <Grid item xs={6}>
+                                {previousSpec
+                                    ? formatDate(previousSpec.published_at)
+                                    : ''}
+                            </Grid>
+                            <Grid item xs={6}>
+                                {formatDate(currentSpec.published_at)}
+                            </Grid>
                         </Grid>
-                        <Grid item xs={6}>
-                            New
-                        </Grid>
+                        <DiffEditor
+                            height="400px"
+                            original={
+                                previousSpec
+                                    ? stringifyJSON(previousSpec.spec)
+                                    : ''
+                            }
+                            modified={stringifyJSON(currentSpec.spec)}
+                            theme={
+                                monacoEditorComponentBackground[
+                                    theme.palette.mode
+                                ]
+                            }
+                            options={{ readOnly: true }}
+                        />
                     </Grid>
-                    <DiffEditor
-                        height="400px"
-                        original={previousSpec}
-                        modified={currentSpec}
-                        theme={
-                            monacoEditorComponentBackground[theme.palette.mode]
-                        }
-                        options={{ readOnly: true }}
-                    />
-                </Grid>
+                ) : null}
             </Grid>
         </Box>
     );
