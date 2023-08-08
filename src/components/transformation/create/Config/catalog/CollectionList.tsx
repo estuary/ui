450decaeab447398ab0d0ab4dd85746ebeec62d9
Travis Jenkins <travis@estuary.dev>
2023-08-08 10:30:38 -0400

    Getting the list to be virtualized

--- src/components/transformation/create/Config/catalog/CollectionList.tsx
+++ src/components/transformation/create/Config/catalog/CollectionList.tsx
@@ -1,6 +1,9 @@
 import { Box, ListItem, Typography } from '@mui/material';
 import CatalogListItem from 'components/transformation/create/Config/catalog/CatalogListItem';
+import { useCallback } from 'react';
 import { FormattedMessage } from 'react-intl';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
 import { DerivationAttribute } from 'stores/TransformationCreate/types';
 import { hasLength } from 'utils/misc-utils';
 import { CatalogListContent } from './CatalogList';
@@ -10,22 +13,51 @@ interface Props {
     fixedAttributeType: DerivationAttribute;
 }
 
+const lineHeight = 52;
+
 function CollectionList({ content, fixedAttributeType }: Props) {
+    const renderRow = useCallback(
+        (props: ListChildComponentProps) => {
+            const { index, style } = props;
+            const { attributeId, value, nestedValue, editorInvalid } =
+                content[index];
+
+            return (
+                <div style={style}>
+                    <CatalogListItem
+                        key={attributeId}
+                        attributeId={attributeId}
+                        fixedAttributeType={fixedAttributeType}
+                        itemLabel={value}
+                        editorInvalid={editorInvalid}
+                        nestedItemLabel={nestedValue}
+                    />
+                </div>
+            );
+        },
+        [content, fixedAttributeType]
+    );
+
     if (hasLength(content)) {
         return (
-            <Box sx={{ minWidth: 'max-content' }}>
-                {content.map(
-                    ({ attributeId, value, nestedValue, editorInvalid }) => (
-                        <CatalogListItem
-                            key={attributeId}
-                            attributeId={attributeId}
-                            fixedAttributeType={fixedAttributeType}
-                            itemLabel={value}
-                            editorInvalid={editorInvalid}
-                            nestedItemLabel={nestedValue}
-                        />
-                    )
-                )}
+            <Box sx={{ height: '100%' }}>
+                <AutoSizer style={{ width: '100%' }}>
+                    {({ width, height }: AutoSizer['state']) => {
+                        // TODO (typing) pretty sure this is the right type but need
+                        //  to figure that out before launching this
+                        return (
+                            <FixedSizeList
+                                height={height}
+                                width={width}
+                                itemSize={lineHeight}
+                                itemCount={content.length}
+                                overscanCount={10}
+                            >
+                                {renderRow}
+                            </FixedSizeList>
+                        );
+                    }}
+                </AutoSizer>
             </Box>
         );
     }
