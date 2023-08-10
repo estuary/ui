9ca7a840b34c22332971e399803f36b6b8269765
Travis Jenkins <travis@estuary.dev>
2023-08-10 16:55:53 -0400

    Starting to disable the edit button during actions

--- src/components/editor/Bindings/SchemaEdit/Toggle.tsx
+++ src/components/editor/Bindings/SchemaEdit/Toggle.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
+import { useFormStateStore_isActive } from 'stores/FormState/hooks';
 import {
     useBindingsEditorStore_editModeEnabled,
     useBindingsEditorStore_setEditModeEnabled,
@@ -8,13 +9,14 @@ import {
 function SchemaEditToggle() {
     const editModeEnabled = useBindingsEditorStore_editModeEnabled();
     const setEditModeEnabled = useBindingsEditorStore_setEditModeEnabled();
+    const formActive = useFormStateStore_isActive();
 
     const toggleEditMode = () => {
         setEditModeEnabled(!editModeEnabled);
     };
 
     return (
-        <Button onClick={toggleEditMode}>
+        <Button onClick={toggleEditMode} disabled={formActive}>
             <FormattedMessage id={editModeEnabled ? 'cta.close' : 'cta.edit'} />
         </Button>
     );
