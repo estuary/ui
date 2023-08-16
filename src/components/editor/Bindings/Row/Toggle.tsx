4e7bd511acaabec9e2dfe9181f8b27295ee70d31
Travis Jenkins <travis@estuary.dev>
2023-08-16 11:06:16 -0400

    Starting to get enable/disable working

--- src/components/editor/Bindings/Row/Toggle.tsx
+++ src/components/editor/Bindings/Row/Toggle.tsx
@@ -1,20 +1,34 @@
-import { Checkbox, Tooltip } from '@mui/material';
+import { ToggleButton, Tooltip } from '@mui/material';
+import { Square } from 'iconoir-react';
+import CheckSquare from 'icons/CheckSquare';
+import { useState } from 'react';
 
 interface Props {
     disabled: boolean;
 }
 
 function BindingsSelectorToggle({ disabled }: Props) {
+    const [enabled, setEnabled] = useState<boolean>(false);
+
     return (
         <Tooltip title="Enable/Disable Binding">
-            <Checkbox
+            <ToggleButton
                 disabled={disabled}
+                selected={enabled}
                 size="small"
+                sx={{
+                    border: 0,
+                    p: 0,
+                }}
+                value="check"
                 onChange={(event) => {
                     event.stopPropagation();
                     console.log('check box clicked on', { event });
+                    setEnabled(!enabled);
                 }}
-            />
+            >
+                {enabled ? <CheckSquare /> : <Square />}
+            </ToggleButton>
         </Tooltip>
     );
 }
