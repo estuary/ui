838a54ad28954f87651b8165935ae8feeab66783
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-08-17 19:56:04 -0400

    Display complete billing history for all tenants

--- src/components/tables/Billing/Rows.tsx
+++ src/components/tables/Billing/Rows.tsx
@@ -3,6 +3,7 @@ import { BillingRecord } from 'api/billing';
 import DataVolume from 'components/tables/cells/billing/DataVolume';
 import TimeStamp from 'components/tables/cells/billing/TimeStamp';
 import MonetaryValue from 'components/tables/cells/MonetaryValue';
+import { useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 interface RowProps {
@@ -36,14 +37,20 @@ function Row({ row }: RowProps) {
 
 // TODO (billing): Remove pagination placeholder when the new RPC is available.
 function Rows({ data }: RowsProps) {
+    // The table should only show the four, most recent months of billing history.
+    // If the user has accrued more than four months worth of billing data, truncate
+    // the data.
+    const processedData = useMemo(
+        () =>
+            data.length > 4 ? data.slice(data.length - 4, data.length) : data,
+        [data]
+    );
+
     return (
         <>
-            {data
-                .slice(data.length - 4, data.length)
-                .reverse()
-                .map((record, index) => (
-                    <Row row={record} key={index} />
-                ))}
+            {[...processedData].reverse().map((record, index) => (
+                <Row row={record} key={index} />
+            ))}
         </>
     );
 }
