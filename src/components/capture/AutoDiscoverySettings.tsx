77189310786b7dc85669d6bd78f0e76649f5e1f7
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-08-21 14:41:23 -0400

    PR Request: unverified changes

--- src/components/capture/AutoDiscoverySettings.tsx
+++ src/components/capture/AutoDiscoverySettings.tsx
@@ -67,25 +67,16 @@ function AutoDiscoverySettings({ readOnly }: Props) {
         if (settingsExist) {
             const autoDiscoverySettings = draftSpecs[0].spec.autoDiscover;
 
-            const bindingSetting = Boolean(
-                Object.hasOwn(autoDiscoverySettings, 'addNewBindings') &&
-                    autoDiscoverySettings.addNewBindings
-            );
-
-            const evolutionSetting = Boolean(
-                Object.hasOwn(
-                    autoDiscoverySettings,
-                    'evolveIncompatibleCollections'
-                ) && autoDiscoverySettings.evolveIncompatibleCollections
-            );
-
-            setAddNewBindings(bindingSetting, {
+            setAddNewBindings(Boolean(autoDiscoverySettings.addNewBindings), {
                 initOnly: true,
             });
 
-            setEvolveIncompatibleCollections(evolutionSetting, {
-                initOnly: true,
-            });
+            setEvolveIncompatibleCollections(
+                Boolean(autoDiscoverySettings.evolveIncompatibleCollections),
+                {
+                    initOnly: true,
+                }
+            );
         } else {
             setAddNewBindings(false, { initOnly: true });
             setEvolveIncompatibleCollections(false, { initOnly: true });
