642b58799e4f8ce7ca6fcc7667244820e0977ced
Travis Jenkins <travis@estuary.dev>
2023-08-22 16:37:12 -0400

    Quick handling of field selection when a collection is disabled

--- src/components/collection/ResourceConfig.tsx
+++ src/components/collection/ResourceConfig.tsx
@@ -3,7 +3,10 @@ import ResourceConfigForm from 'components/collection/ResourceConfigForm';
 import FieldSelectionViewer from 'components/editor/Bindings/FieldSelection';
 import { useEntityType } from 'context/EntityContext';
 import { FormattedMessage } from 'react-intl';
-import { useResourceConfig_hydrated } from 'stores/ResourceConfig/hooks';
+import {
+    useResourceConfig_hydrated,
+    useResourceConfig_resourceConfigOfCollectionProperty,
+} from 'stores/ResourceConfig/hooks';
 import { BindingsEditorConfigSkeleton } from './CollectionSkeletons';
 
 interface Props {
@@ -15,6 +18,12 @@ function ResourceConfig({ collectionName, readOnly = false }: Props) {
     const entityType = useEntityType();
 
     const hydrated = useResourceConfig_hydrated();
+
+    const collectionDisabled =
+        useResourceConfig_resourceConfigOfCollectionProperty(
+            collectionName,
+            'disable'
+        );
 
     return (
         <>
@@ -33,7 +42,7 @@ function ResourceConfig({ collectionName, readOnly = false }: Props) {
                 )}
             </Box>
 
-            {entityType === 'materialization' ? (
+            {entityType === 'materialization' && !collectionDisabled ? (
                 <FieldSelectionViewer collectionName={collectionName} />
             ) : null}
         </>
