53069fb0e249fc123d455b07bae52e5c3e7638e0
Travis Jenkins <travis@estuary.dev>
2023-08-25 12:51:22 -0400

    Sharing column related stuff: new file

---
+++ src/components/collection/Selector/Table/shared.ts
@@ -0,0 +1,16 @@
+export const catalogNameColumn = 'catalog_name';
+export const publishedColumn = 'updated_at';
+export const tableColumns = [
+    {
+        field: null,
+        headerIntlKey: '',
+    },
+    {
+        field: catalogNameColumn,
+        headerIntlKey: 'entityTable.data.userFullName',
+    },
+    {
+        field: publishedColumn,
+        headerIntlKey: 'entityTable.data.lastPublished',
+    },
+];
