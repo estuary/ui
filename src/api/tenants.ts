1543309fdde1714cef6f052f6d5dced76d70d5f6
Travis Jenkins <travis@estuary.dev>
2023-09-29 11:02:21 -0400

    Allowing to query for specific tenants

--- src/api/tenants.ts
+++ src/api/tenants.ts
@@ -1,14 +1,18 @@
 import { supabaseClient, TABLES } from 'services/supabase';
 import { Tenants } from 'types';
 
-const getTenantDetails = () => {
-    const queryBuilder = supabaseClient.from<Tenants>(TABLES.TENANTS).select(
+const getTenantDetails = (tenants?: string[]) => {
+    let queryBuilder = supabaseClient.from<Tenants>(TABLES.TENANTS).select(
         `    
             tasks_quota,
             collections_quota,
             tenant
         `
     );
+
+    if (tenants && tenants.length > 0) {
+        queryBuilder = queryBuilder.in('tenant', tenants);
+    }
 
     return queryBuilder;
 };
