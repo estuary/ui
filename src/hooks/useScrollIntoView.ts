edc074cfca6a084b3e2a310d1599ad972508de69
Travis Jenkins <travis@estuary.dev>
2023-11-01 16:44:39 -0400

    Allowing for options to be passed

--- src/hooks/useScrollIntoView.ts
+++ src/hooks/useScrollIntoView.ts
@@ -1,18 +1,19 @@
-import { MutableRefObject, useCallback } from 'react';
+import { RefObject, useCallback } from 'react';
 
-// Pass a ref and this will make sure it is scrolled into view
-//  The settings try to make it feel like the object is centered on the page
-//  eventually we should allow those settings to be passed but skipped for now
-//  as the main use are errors are the top of the create/edit pages (Q4 2023)
-function useScrollIntoView(target: MutableRefObject<any>) {
+function useScrollIntoView(target: RefObject<HTMLDivElement>) {
     return useCallback(
-        (scrollTarget?: MutableRefObject<any>) => {
+        (
+            scrollTarget?: RefObject<HTMLDivElement | null>,
+            options?: ScrollIntoViewOptions | boolean
+        ) => {
             const el = scrollTarget ?? target;
-            el.current?.scrollIntoView({
-                behavior: 'smooth',
-                block: 'center',
-                inline: 'center',
-            });
+            el.current?.scrollIntoView(
+                options ?? {
+                    behavior: 'smooth',
+                    block: 'center',
+                    inline: 'center',
+                }
+            );
         },
         [target]
     );
