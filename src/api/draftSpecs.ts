5d6749c3ab1251b4d08684a61203f9cc3938fb09
Travis Jenkins <travis@estuary.dev>
2023-11-09 15:16:57 -0500

    marking todo

--- src/api/draftSpecs.ts
+++ src/api/draftSpecs.ts
@@ -168,6 +168,7 @@ export const deleteDraftSpecsByCatalogName = async (
         const promises: Array<Promise<PostgrestResponse<any>>> = [];
         let index = 0;
 
+        // TODO (retry) need to figure how we want to handle this kind of action in retrying
         const deletePromiseGenerator = (idx: number) => {
             return supabaseClient
                 .from(TABLES.DRAFT_SPECS)
