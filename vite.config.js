587a89558e3f2c772ea05d9bafd1ccd9155948fe
Travis Jenkins <travis@estuary.dev>
2023-11-10 18:01:42 -0500

    Switching config from ts to js Fixing lint issue

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,15 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 import checker from 'vite-plugin-checker';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import VitePluginHtmlEnv from 'vite-plugin-html-env';
+import vitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         outDir: './build',
+    },
+    server: {
+        port: 3000,
     },
     plugins: [
         checker({
@@ -29,17 +32,11 @@ export default defineConfig({
         react(),
         svgr({ include: '**/*.svg?react' }),
         topLevelAwait(),
-        VitePluginHtmlEnv({
+        vitePluginHtmlEnv({
             compiler: true,
             // compiler: false // old
         }),
         viteTsconfigPaths(),
         wasm(),
     ],
-    optimizeDeps: {
-        include: ['@emotion/styled'],
-    },
-    server: {
-        port: 3000,
-    },
 });
