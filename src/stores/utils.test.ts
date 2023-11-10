8755aa999f453115d9a0e2c83488381e82c3ce42
Travis Jenkins <travis@estuary.dev>
2023-11-10 17:23:19 -0500

    Adding a quick test to make sure we have some test there

---
+++ src/stores/utils.test.ts
@@ -0,0 +1,13 @@
+// sum.test.js
+import { expect, test } from 'vitest';
+import { checkForErrors } from './utils';
+
+test('checkForErrors will handle different states for errors', () => {
+    expect(checkForErrors({})).toBe(undefined);
+    expect(checkForErrors({ errors: null })).toBe(null);
+
+    expect(checkForErrors({ errors: [] })).toBe(false);
+    expect(checkForErrors({ errors: [1] })).toBe(true);
+
+    expect(checkForErrors({ errors: 'fake error' })).toBe(true);
+});
