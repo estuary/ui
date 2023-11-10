296a6cb90d1ff416c56e6e07660c6217f9249991
Travis Jenkins <travis@estuary.dev>
2023-11-10 18:32:55 -0500

    removing react component svg for now

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgr from 'vite-plugin-svgr';
+// import svgr from 'vite-plugin-svgr';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 import checker from 'vite-plugin-checker';
@@ -21,7 +21,6 @@ export default defineConfig({
     plugins: [
         checker({
             eslint: {
-                // for example, lint .ts and .tsx
                 lintCommand: 'lint',
             },
             typescript: {
@@ -32,13 +31,12 @@ export default defineConfig({
             include: ['path', 'process', 'stream'],
         }),
         react(),
-        svgr({ include: '**/*.svg?react' }),
+        // svgr({ include: '**/*.svg?react' }),
         topLevelAwait(),
         viteCompression(),
         viteImageOptimizer({}),
         vitePluginHtmlEnv({
             compiler: true,
-            // compiler: false // old
         }),
         viteTsconfigPaths(),
         wasm(),
