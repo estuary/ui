9b3a4bee987104ae374e3359cffd4c24a3adb5fc
Travis Jenkins <travis@estuary.dev>
2023-11-10 19:51:37 -0500

    Removing react svg loading as we don't need this right now

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-// import svgr from 'vite-plugin-svgr';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 import checker from 'vite-plugin-checker';
@@ -33,7 +32,6 @@ export default defineConfig({
             include: ['path', 'process', 'stream'],
         }),
         react(),
-        // svgr({ include: '**/*.svg?react' }),
         topLevelAwait(),
         viteCompression(),
         viteImageOptimizer({}),
