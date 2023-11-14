a2988c7a7dc10c705b4d52edea7d5129cb7b837c
Travis Jenkins <travis@estuary.dev>
2023-11-14 11:54:53 -0500

    Cleaning up index files

--- src/index.tsx
+++ src/index.tsx
@@ -9,20 +9,6 @@ import { initGoogleTagManager } from 'services/gtm';
 import { initLogRocket } from 'services/logrocket';
 import AppProviders from './context';
 
-// export const profilerCallback = (
-//     id: any,
-//     phase: any,
-//     actual: number,
-//     base: number,
-//     start: any,
-//     commit: any,
-//     inter: any
-// ) => {
-//     console.log(`Render ${id} in ${actual}`);
-//     console.log('  Time > ', { actual, base, commit, start });
-//     console.log('  Deet > ', { inter, phase });
-// };
-
 initGoogleTagManager();
 initLogRocket();
 
