83956f967938cbb547d58d5774c370925eaf9cb9
Travis Jenkins <travis@estuary.dev>
2023-11-21 13:49:27 -0500

    Adding in a clearable input for the filter of collections (#828)

--- src/lang/en-US.ts
+++ src/lang/en-US.ts
@@ -1300,6 +1300,10 @@ const Notifications: ResolvedIntlConfig['messages'] = {
     'notifications.paymentMethods.missing.trialPast.instructions': `Please {cta} to continue using Estuary Flow.`,
 };
 
+const JsonForms: ResolvedIntlConfig['messages'] = {
+    'jsonForms.clearInput': `Clear input field`,
+};
+
 const enUSMessages: ResolvedIntlConfig['messages'] = {
     ...CommonMessages,
     ...CTAs,
@@ -1361,6 +1365,7 @@ const enUSMessages: ResolvedIntlConfig['messages'] = {
     ...NotBeforeNotAfter,
     ...Notifications,
     ...Fetchers,
+    ...JsonForms,
 };
 
 export default enUSMessages;
