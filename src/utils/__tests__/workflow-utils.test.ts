c4e7c5756e12c5dadfc4c0fc93f570897589ab64
Travis Jenkins <travis@estuary.dev>
2023-11-28 16:39:58 -0500

    Expanding empty test

--- src/utils/__tests__/workflow-utils.test.ts
+++ src/utils/__tests__/workflow-utils.test.ts
@@ -14,6 +14,7 @@ describe('getBindingIndex', () => {
         test('bindings are empty', () => {
             expect(getBindingIndex([], '')).toBe(defaultResponse);
             expect(getBindingIndex([null], '')).toBe(defaultResponse);
+            expect(getBindingIndex([undefined], '')).toBe(defaultResponse);
             expect(getBindingIndex([{}], '')).toBe(defaultResponse);
         });
 
