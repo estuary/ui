72e95694d96160b09e01de81230ec0a52f18608f
Travis Jenkins <travis@estuary.dev>
2023-12-01 12:27:09 -0500

    New background save hook

---
+++ src/components/shared/Entity/Actions/useSaveInBackground.ts
@@ -0,0 +1,37 @@
+import {
+    useEditorStore_id,
+    useEditorStore_queryResponse_draftSpecs,
+} from 'components/editor/Store/hooks';
+import { useEffect, useRef } from 'react';
+import { CustomEvents } from 'services/types';
+import useEntityWorkflowHelpers from '../hooks/useEntityWorkflowHelpers';
+import useSave from './useSave';
+
+function useSaveInBackground() {
+    const fireBackgroundTest = useRef(true);
+
+    const { callFailed } = useEntityWorkflowHelpers();
+
+    const draftId = useEditorStore_id();
+    const draftSpecs = useEditorStore_queryResponse_draftSpecs();
+
+    const saveCatalog = useSave(
+        CustomEvents.MATERIALIZATION_TEST_BACKGROUND,
+        callFailed,
+        true,
+        true
+    );
+
+    useEffect(() => {
+        if (
+            fireBackgroundTest.current &&
+            draftSpecs.length > 0 &&
+            !draftSpecs[0].built_spec
+        ) {
+            fireBackgroundTest.current = false;
+            void saveCatalog(draftId, true);
+        }
+    }, [draftId, draftSpecs, saveCatalog]);
+}
+
+export default useSaveInBackground;
