faeaab641b0bb5aeeb618714bace7619cef82b67
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-12-01 16:08:49 -0500

    Remove comment from user information by prefix hook

--- src/hooks/useUserInformationByPrefix.ts
+++ src/hooks/useUserInformationByPrefix.ts
@@ -2,10 +2,6 @@ import { getUserInformationByPrefix } from 'api/combinedGrantsExt';
 import { useSelectNew } from 'hooks/supabase-swr/hooks/useSelect';
 import { Capability, Grant_UserExt } from 'types';
 
-// TODO (optimization): The combined_grants_ext view limits the function of this hook. The original intent of the hook
-//   is to return information (email, full name, avatar URL, and user ID) for all administrators of a tenant, including users
-//   who have administrative access via a prefix privilege (e.g., user A is an admin of tenantB/ because tenantA/ has admin access
-//   to that prefix).
 function useUserInformationByPrefix(
     objectRoles: string[],
     capability: Capability
