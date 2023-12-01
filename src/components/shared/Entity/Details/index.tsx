9d87b63dbc998c92e7746873b881fe5b67d8eed2
Kiahna Tucker <tucker.kiahna@gmail.com>
2023-12-01 06:42:38 -0500

    Consolidate the EntityDetails and DetailContent components

--- src/components/shared/Entity/Details/index.tsx
+++ src/components/shared/Entity/Details/index.tsx
@@ -1,7 +1,12 @@
+import { Box, Divider, Stack, Typography } from '@mui/material';
 import LiveSpecsHydrator from 'components/editor/Store/LiveSpecsHydrator';
 import { createEditorStore } from 'components/editor/Store/create';
-import DetailContent from 'components/shared/Entity/Details/Content';
+import EditButton from 'components/shared/Entity/Details/EditButton';
+import MaterializeButton from 'components/shared/Entity/Details/MaterializeButton';
+import RenderTab from 'components/shared/Entity/Details/RenderTab';
+import DetailTabs from 'components/shared/Entity/Details/Tabs';
 import { LocalZustandProvider } from 'context/LocalZustand';
+import { truncateTextSx } from 'context/Theme';
 import useGlobalSearchParams, {
     GlobalSearchParams,
 } from 'hooks/searchParams/useGlobalSearchParams';
@@ -29,7 +34,39 @@ function EntityDetails() {
                 localZustandScope={true}
             >
                 <ShardHydrator catalogName={catalogName}>
-                    <DetailContent />
+                    <Stack spacing={2} sx={{ m: 1 }}>
+                        <Stack
+                            direction="row"
+                            sx={{
+                                justifyContent: 'space-between',
+                            }}
+                        >
+                            <Typography
+                                component="span"
+                                variant="h6"
+                                sx={{
+                                    ...truncateTextSx,
+                                    alignItems: 'center',
+                                }}
+                            >
+                                {catalogName}
+                            </Typography>
+
+                            <Stack direction="row">
+                                <EditButton buttonVariant="outlined" />
+
+                                <MaterializeButton />
+                            </Stack>
+                        </Stack>
+
+                        <Divider />
+
+                        <DetailTabs />
+                    </Stack>
+
+                    <Box sx={{ m: 1 }}>
+                        <RenderTab />
+                    </Box>
                 </ShardHydrator>
             </LiveSpecsHydrator>
         </LocalZustandProvider>
