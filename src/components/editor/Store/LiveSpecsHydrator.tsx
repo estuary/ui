aecf37ceb6efb648ef32481d3d8f4648130d5767
Travis Jenkins <travis@estuary.dev>
2023-12-05 21:45:24 -0500

    Having the hydrator pass the name to the entity not found page

--- src/components/editor/Store/LiveSpecsHydrator.tsx
+++ src/components/editor/Store/LiveSpecsHydrator.tsx
@@ -3,6 +3,9 @@ import {
     useEditorStore_setSpecs,
 } from 'components/editor/Store/hooks';
 import Error from 'components/shared/Error';
+import useGlobalSearchParams, {
+    GlobalSearchParams,
+} from 'hooks/searchParams/useGlobalSearchParams';
 import { useLiveSpecs_spec } from 'hooks/useLiveSpecs';
 import EntityNotFound from 'pages/error/EntityNotFound';
 import { useEffect } from 'react';
@@ -28,6 +31,7 @@ function LiveSpecsHydrator({
         collectionNames
     );
 
+    const catalogName = useGlobalSearchParams(GlobalSearchParams.CATALOG_NAME);
     const setSpecs = useEditorStore_setSpecs({ localScope: localZustandScope });
     const setId = useEditorStore_setId({ localScope: localZustandScope });
 
@@ -48,7 +52,7 @@ function LiveSpecsHydrator({
     // TODO (details) same as the error up above
     // Targetting to handle when a user does not have access
     if (!pubSpecsValidating && !hasLength(publicationSpecs)) {
-        return <EntityNotFound />;
+        return <EntityNotFound catalogName={catalogName} />;
     }
 
     // eslint-disable-next-line react/jsx-no-useless-fragment
