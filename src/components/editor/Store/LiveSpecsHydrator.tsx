aba97f2e4d1637e6b7aecda8ae6a1728c8f9bb12
Travis Jenkins <travis@estuary.dev>
2023-12-05 21:24:00 -0500

    Handling when the user cannot access an entity Returning validation checking

--- src/components/editor/Store/LiveSpecsHydrator.tsx
+++ src/components/editor/Store/LiveSpecsHydrator.tsx
@@ -4,6 +4,7 @@ import {
 } from 'components/editor/Store/hooks';
 import Error from 'components/shared/Error';
 import { useLiveSpecs_spec } from 'hooks/useLiveSpecs';
+import EntityNotFound from 'pages/error/EntityNotFound';
 import { useEffect } from 'react';
 import { BaseComponentProps } from 'types';
 import { hasLength } from 'utils/misc-utils';
@@ -18,11 +19,14 @@ function LiveSpecsHydrator({
     collectionNames,
     children,
 }: Props) {
-    const { liveSpecs: publicationSpecs, error: pubSpecsError } =
-        useLiveSpecs_spec(
-            `editorStore-${collectionNames?.join('-')}`,
-            collectionNames
-        );
+    const {
+        liveSpecs: publicationSpecs,
+        error: pubSpecsError,
+        isValidating: pubSpecsValidating,
+    } = useLiveSpecs_spec(
+        `editorStore-${collectionNames?.join('-')}`,
+        collectionNames
+    );
 
     const setSpecs = useEditorStore_setSpecs({ localScope: localZustandScope });
     const setId = useEditorStore_setId({ localScope: localZustandScope });
@@ -39,6 +43,12 @@ function LiveSpecsHydrator({
     // 2. Show the error but leave the proper header displaying
     if (pubSpecsError) {
         return <Error error={pubSpecsError} />;
+    }
+
+    // TODO (details) same as the error up above
+    // Targetting to handle when a user does not have access
+    if (!pubSpecsValidating && !hasLength(publicationSpecs)) {
+        return <EntityNotFound />;
     }
 
     // eslint-disable-next-line react/jsx-no-useless-fragment
