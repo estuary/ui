54af971e5c7d5d8a6b200676efc866ff0fa147be
Travis Jenkins <travis@estuary.dev>
2023-12-06 22:44:53 -0500

    Using new refresh hook in the button

--- src/components/editor/Bindings/FieldSelection/RefreshButton.tsx
+++ src/components/editor/Bindings/FieldSelection/RefreshButton.tsx
@@ -1,59 +1,29 @@
-import { Button } from '@mui/material';
-import useGenerateCatalog from 'components/materialization/useGenerateCatalog';
-import useSave from 'components/shared/Entity/Actions/useSave';
-import useEntityWorkflowHelpers from 'components/shared/Entity/hooks/useEntityWorkflowHelpers';
-import { CustomEvents } from 'services/types';
-import { useMutateDraftSpec } from 'components/shared/Entity/MutateDraftSpecContext';
-import { useState } from 'react';
+import { Box, Button } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
 import { Refresh } from 'iconoir-react';
+import useFieldSelectionRefresh from './useFieldSelectionRefresh';
 
 interface Props {
     buttonLabelId: string;
-    logEvent: CustomEvents.MATERIALIZATION_TEST;
     disabled?: boolean;
 }
 
-function RefreshButton({ disabled, logEvent, buttonLabelId }: Props) {
-    const { callFailed } = useEntityWorkflowHelpers();
-
-    const [updating, setUpdating] = useState(false);
-
-    const generateCatalog = useGenerateCatalog();
-    const mutateDraftSpec = useMutateDraftSpec();
-    const saveCatalog = useSave(logEvent, callFailed, true, true);
+function RefreshButton({ disabled, buttonLabelId }: Props) {
+    const { updating, refresh } = useFieldSelectionRefresh();
 
     return (
-        <Button
-            disabled={Boolean(updating || disabled)}
-            startIcon={<Refresh style={{ fontSize: 12 }} />}
-            variant="text"
-            onClick={async () => {
-                setUpdating(true);
-
-                let evaluatedDraftId;
-                try {
-                    evaluatedDraftId = await generateCatalog(mutateDraftSpec);
-                } catch (_error: unknown) {
-                    setUpdating(false);
-                }
-
-                // Make sure we have a draft id so we know the generate worked
-                //  if this is not returned then the function itself handled showing an error
-                if (evaluatedDraftId) {
-                    try {
-                        await saveCatalog(evaluatedDraftId, true);
-                    } catch (_error: unknown) {
-                        setUpdating(false);
-                    }
-                }
-
-                // I do not think this is truly needed but being safe so the user is not stuck with a disabled button
-                setUpdating(false);
-            }}
-        >
-            <FormattedMessage id={buttonLabelId} />
-        </Button>
+        <Box>
+            <Button
+                disabled={Boolean(updating || disabled)}
+                startIcon={<Refresh style={{ fontSize: 12 }} />}
+                variant="text"
+                onClick={async () => {
+                    await refresh();
+                }}
+            >
+                <FormattedMessage id={buttonLabelId} />
+            </Button>
+        </Box>
     );
 }
 export default RefreshButton;
