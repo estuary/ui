f590b976bb82b51bc3e6975fbaab1a074d43f40e
Travis Jenkins <travis@estuary.dev>
2023-12-14 19:03:42 -0500

    Adding a defined app version variable Writing the version to a json file during build Switching compression plugins

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,21 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 import checker from 'vite-plugin-checker';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import viteCompression from 'vite-plugin-compression';
+import { compression } from 'vite-plugin-compression2';
 import { ViteImageOptimizer as viteImageOptimizer } from 'vite-plugin-image-optimizer';
 import circleDependency from 'vite-plugin-circular-dependency';
 import { vitePluginVersionMark } from 'vite-plugin-version-mark';
 import { type Plugin } from 'vite';
 import path from 'path';
 
+const APP_VERSION = '__ESTUARY_UI_VERSION__';
+
 const writeVersionToFile: () => Plugin = () => ({
     name: 'write-version-to-file',
     async config(config) {
-        const version = config.define?.__ESTUARY_UI_VERSION__?.replaceAll(
-            `"`,
-            ''
-        );
+        const version = config.define?.[APP_VERSION]?.replaceAll(`"`, '');
         if (!version) {
-            console.error('__ESTUARY_UI_VERSION__ not found');
+            console.error(`${APP_VERSION} not found`);
             return;
         }
 
@@ -55,7 +54,12 @@ const writeVersionToFile: () => Plugin = () => ({
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
+        assetsDir: 'static',
         outDir: './build',
+    },
+
+    define: {
+        [APP_VERSION]: JSON.stringify(process.env.npm_package_version),
     },
 
     preview: { port: 3000, strictPort: true },
@@ -68,6 +72,12 @@ export default defineConfig({
     // https://github.com/vitejs/awesome-vite#plugins
     plugins: [
         viteTsconfigPaths(),
+        // vitePluginVersionMark({
+        //     ifGitSHA: false,
+        //     ifMeta: false,
+        //     ifGlobal: true,
+        //     ifLog: true,
+        // }),
 
         // Code injection
         nodePolyfills({
@@ -81,12 +91,10 @@ export default defineConfig({
 
         // Build/Deploy stuff
         viteImageOptimizer({}),
-        viteCompression(),
-        vitePluginVersionMark({
-            ifGitSHA: false,
-            ifMeta: false,
-            ifGlobal: true,
-            ifLog: true,
+        compression({
+            algorithm: 'gzip',
+            exclude: [/\.(br)$/, /\.(gz)$/],
+            deleteOriginalAssets: false,
         }),
 
         // Quality Control
