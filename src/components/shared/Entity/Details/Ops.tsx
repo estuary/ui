d50fb340776d3e29a8a935c955197da6d3478271
Travis Jenkins <travis@estuary.dev>
2023-12-15 14:55:34 -0500

    Getting the very basic stuff wired up

--- src/components/shared/Entity/Details/Ops.tsx
+++ src/components/shared/Entity/Details/Ops.tsx
@@ -1,14 +1,44 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
-import { Box } from '@mui/material';
+import { Editor } from '@monaco-editor/react';
+import { Box, Button, LinearProgress, useTheme } from '@mui/material';
 import UnderDev from 'components/shared/UnderDev';
+import { useEntityType } from 'context/EntityContext';
+import { monacoEditorComponentBackground } from 'context/Theme';
+import { useJournalData } from 'hooks/journals/useJournalData';
+import useGlobalSearchParams, {
+    GlobalSearchParams,
+} from 'hooks/searchParams/useGlobalSearchParams';
+import { stringifyJSON } from 'services/stringify';
 
 function Ops() {
-    // const catalogName = useGlobalSearchParams(GlobalSearchParams.CATALOG_NAME);
+    const theme = useTheme();
+
+    const entityType = useEntityType();
+    const catalogName = useGlobalSearchParams(GlobalSearchParams.CATALOG_NAME);
+    const collectionName = `ops.us-central1.v1/logs`;
+    const name = `${collectionName}/kind=${entityType}/name=${encodeURIComponent(
+        catalogName
+    )}/pivot=00`;
+
+    const journalData = useJournalData(name, 20, collectionName);
 
     return (
         <Box>
             <UnderDev />
-            <Box>Ops logs go here</Box>
+            {journalData.loading ? <LinearProgress /> : null}
+            <Box>
+                <Button onClick={journalData.refresh}>Refresh</Button>
+                <Editor
+                    height={400}
+                    defaultValue="loading..."
+                    value={stringifyJSON(journalData)}
+                    defaultLanguage="json"
+                    theme={monacoEditorComponentBackground[theme.palette.mode]}
+                    saveViewState={false}
+                    path={name}
+                    options={{ readOnly: true }}
+                />
+            </Box>
         </Box>
     );
 }
