c96f7f526d71fedc300a30d4c28a7b1529172abe
Travis Jenkins <travis@estuary.dev>
2023-12-18 12:19:51 -0500

    Travjenkins/fix binding search (#884)
    * Having captures use the stripped path name
    Always calculating the stripped path name
    Using the `id` to set the current collection
    
    * Adding function to fetch the correct selector
    
    * We not always have the proper name in the value so no need to manually configure this
    
    * updating string value

--- src/components/collection/Selector/List/shared.ts
+++ src/components/collection/Selector/List/shared.ts
@@ -1,1 +1,7 @@
 export const COLLECTION_SELECTOR_NAME_COL = 'id';
+export const COLLECTION_SELECTOR_STRIPPED_PATH_NAME = 'strippedPathName';
+
+export const getCollectionSelector = (isCapture: boolean) =>
+    isCapture
+        ? COLLECTION_SELECTOR_STRIPPED_PATH_NAME
+        : COLLECTION_SELECTOR_NAME_COL;
