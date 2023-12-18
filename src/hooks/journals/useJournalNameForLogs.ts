c205bf83f85c3fbc846c5f1f890fb4c4f96a819c
Travis Jenkins <travis@estuary.dev>
2023-12-18 11:38:24 -0500

    Fixing how we store regions

--- src/hooks/journals/useJournalNameForLogs.ts
+++ src/hooks/journals/useJournalNameForLogs.ts
@@ -1,13 +1,15 @@
 import { useEntityType } from 'context/EntityContext';
 import { useMemo } from 'react';
 
-const REGIONS = [`ops.us-central1.v1`];
+// This will expand eventually. Federated data planes will need to write logs to
+//      different regions. This is why this is in an array and takes some kind of "region selector".
+const REGIONS = [`us-central1`];
 
 function useJournalNameForLogs(entityName: string, region: number = 0) {
     const entityType = useEntityType();
 
     return useMemo(() => {
-        const collectionName = `${REGIONS[region]}/logs`;
+        const collectionName = `ops.${REGIONS[region]}.v1/logs`;
 
         return [
             `${collectionName}/kind=${entityType}/name=${encodeURIComponent(
