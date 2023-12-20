534ce9353b71b021cb79ea66037b65d0a98f6b6c
Travis Jenkins <travis@estuary.dev>
2023-12-20 16:11:48 -0500

    Allowing expand to be disabled

--- src/components/tables/cells/logs/ExpandRowButton.tsx
+++ src/components/tables/cells/logs/ExpandRowButton.tsx
@@ -1,32 +1,40 @@
-import { IconButton, Tooltip } from '@mui/material';
+import { Box, IconButton, Tooltip } from '@mui/material';
 import { NavArrowRight } from 'iconoir-react';
 import { useIntl } from 'react-intl';
 
 interface Props {
     expanded: boolean;
+    disable?: boolean;
 }
 
-function ExpandRowButton({ expanded }: Props) {
+function ExpandRowButton({ disable, expanded }: Props) {
     const intl = useIntl();
     return (
         <Tooltip
             title={intl.formatMessage({
-                id: expanded ? 'aria.closeExpand' : 'aria.openExpand',
+                id: disable
+                    ? 'ops.logsTable.expand.disabled'
+                    : expanded
+                    ? 'aria.closeExpand'
+                    : 'aria.openExpand',
             })}
             placement="top"
             enterDelay={500}
         >
-            <IconButton
-                aria-expanded={expanded}
-                sx={{
-                    padding: 0.5,
-                    marginRight: 0,
-                    transform: `rotate(${expanded ? '90' : '0'}deg)`,
-                    transition: 'all 250ms ease-in-out',
-                }}
-            >
-                <NavArrowRight />
-            </IconButton>
+            <Box>
+                <IconButton
+                    aria-expanded={disable ? undefined : expanded}
+                    disabled={disable}
+                    sx={{
+                        padding: 0.5,
+                        marginRight: 0,
+                        transform: `rotate(${expanded ? '90' : '0'}deg)`,
+                        transition: 'all 250ms ease-in-out',
+                    }}
+                >
+                    <NavArrowRight />
+                </IconButton>
+            </Box>
         </Tooltip>
     );
 }
