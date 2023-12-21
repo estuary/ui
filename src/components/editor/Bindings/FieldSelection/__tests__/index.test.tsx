7ef4b29d1f5217306dfa9c9beff4ec9c174e79e2
Travis Jenkins <travis@estuary.dev>
2023-12-21 14:32:23 -0500

    Moving around how we comment out this test

--- src/components/editor/Bindings/FieldSelection/__tests__/index.test.tsx
+++ src/components/editor/Bindings/FieldSelection/__tests__/index.test.tsx
@@ -1,14 +1,18 @@
-import { describe } from 'vitest';
+// import { describe, it, expect } from 'vitest';
 // import { render, screen } from '@testing-library/react';
 // import FieldSelectionViewer from '..';
 
 // const collectionName = 'testing/fake/name';
 
+// describe.skip('Field Selection Viewer', () => {
+//     it('should have a CTA to allow users to refresh the table', () => {
+//         render(<FieldSelectionViewer collectionName={collectionName} />);
+//         expect(screen.getByText(/See Fields/i)).toBeDefined();
+//     });
+// });
+
 // Testing a component will not work until there is a solution to this issue
 // https://github.com/vitest-dev/vitest/issues/2910
-describe.skip('Field Selection Viewer', () => {
-    //     it('should have a CTA to allow users to refresh the table', () => {
-    //         render(<FieldSelectionViewer collectionName={collectionName} />);
-    //         expect(screen.getByText(/See Fields/i)).toBeDefined();
-    //     });
-});
+import { describe } from 'vitest';
+
+describe.skip('Field Selection Viewer', () => {});
