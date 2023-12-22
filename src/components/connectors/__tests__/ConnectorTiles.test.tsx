90d191441fde47204bd9b0d9d59baea33f5d8dbe
Travis Jenkins <travis@estuary.dev>
2023-12-22 11:49:15 -0500

    Commenting out WIP

--- src/components/connectors/__tests__/ConnectorTiles.test.tsx
+++ src/components/connectors/__tests__/ConnectorTiles.test.tsx
@@ -1,14 +1,17 @@
-import { getByText, render, waitFor } from 'test/test-utils';
-import { describe, it } from 'vitest';
+import { render } from 'test/test-utils';
+import { describe, expect, it } from 'vitest';
 import ConnectorTiles from '../ConnectorTiles';
 
 describe('Connector Tiles', () => {
     it('should display the connector details', async () => {
         const rendered = render(<ConnectorTiles />);
 
-        await waitFor(() => {
-            expect(getByText('FooDB')).toBeInTheDocument();
-        });
+        // TODO (testing)
+        //      need to get the vitest `expect` wired up to handle matchers from other
+        //      libraries and can allow us to wait for stuff to load in
+        // await waitFor(() => {
+        //     expect(getByText('FooDB')).toBeInTheDocument();
+        // });
 
         expect(rendered.baseElement).toMatchSnapshot();
     });
