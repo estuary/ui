7184803ba91c901c2d9951726dcb619360db8002
Travis Jenkins <travis@estuary.dev>
2024-01-26 14:03:40 -0500

    Adding a quicker handler to prevent infinite loops

--- src/services/react.ts
+++ src/services/react.ts
@@ -3,15 +3,46 @@ import { lazy } from 'react';
 import { logRocketConsole, logRocketEvent } from './shared';
 import { CustomEvents } from './types';
 
+const LAZY_LOAD_FAILED_KEY = 'chunk_failed';
+
+// https://mitchgavan.com/code-splitting-react-safely/
+const setWithExpiry = (key: string, value: string, ttl: number) => {
+    const item = {
+        value,
+        expiry: new Date().getTime() + ttl,
+    };
+    localStorage.setItem(key, JSON.stringify(item));
+};
+
+const getWithExpiry = (key: string) => {
+    const itemString = window.localStorage.getItem(key);
+    const item = itemString ? JSON.parse(itemString) : null;
+
+    if (item === null) {
+        return null;
+    }
+
+    if (new Date().getTime() > item.expiry) {
+        localStorage.removeItem(key);
+        return null;
+    }
+
+    return item.value;
+};
+
 const handledLazy = (factory: () => Promise<{ default: any }>) => {
     return lazy(() =>
         factory().catch((error) => {
             logRocketEvent(CustomEvents.LAZY_LOADING, 'failed');
-            logRocketConsole('Component Failed Loading:', error);
+            logRocketConsole('Component Failed Loading 1:', error);
 
-            // Since we cannot fetch what we want go ahead and reload the apge
-            window.location.reload();
+            // Check if we're in an infinite loading loop before trying again
+            if (!getWithExpiry(LAZY_LOAD_FAILED_KEY)) {
+                setWithExpiry(LAZY_LOAD_FAILED_KEY, 'true', 10000);
+                window.location.reload();
+            }
 
+            // Still make sure to return something so the app does not blow up
             return { default: FullPageSpinner };
         })
     );
