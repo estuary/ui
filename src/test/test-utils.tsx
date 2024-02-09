fc71cb396b77e8d0b3cb7a73c19ca8df730b35a7
Travis Jenkins <travis@estuary.dev>
2024-02-09 11:18:54 -0500

    Fixing typing

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -17,10 +17,8 @@ const AllTheProviders = ({ children }: { children: ReactElement }) => {
     );
 };
 
-const customRender = (
-    ui: ReactElement,
-    options?: Omit<RenderOptions, 'wrapper'>
-) => render(ui, { wrapper: AllTheProviders, ...options });
+const customRender = (ui: ReactElement, options?: RenderOptions): any =>
+    render(ui, { wrapper: AllTheProviders, ...options });
 
 export * from '@testing-library/react';
 export { customRender as render };
