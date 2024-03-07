3fdf8f03efe80cad023477a4488363ceb3b537a8
Travis Jenkins <travis@estuary.dev>
2024-03-07 17:04:23 -0500

    Removing the flicker setting because it causes more flicker when changed the drop down

--- src/components/shared/Entity/DetailsForm/Form.tsx
+++ src/components/shared/Entity/DetailsForm/Form.tsx
@@ -63,6 +63,8 @@ function DetailsFormForm({ connectorTags, entityType, readOnly }: Props) {
     const formData = useDetailsForm_details();
     const { connectorImage: originalConnectorImage } = formData;
 
+    console.log('DetailsFormForm formData', formData);
+
     const connectorImagePath = useDetailsForm_connectorImage_imagePath();
     const connectorIdChanged = useDetailsForm_changed_connectorId();
 
@@ -203,8 +205,6 @@ function DetailsFormForm({ connectorTags, entityType, readOnly }: Props) {
             if (details.data.connectorImage.connectorId === connectorId) {
                 setDetails_connector(details.data.connectorImage);
             } else {
-                // Set the details before navigating to reduce "flicker"
-                setDetails(details);
                 setEntityNameChanged(details.data.entityName);
 
                 navigateToCreate(
