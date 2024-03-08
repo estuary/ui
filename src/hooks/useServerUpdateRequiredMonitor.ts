0a8205d7f614bbb369ee13508973d1c119e00fcd
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-03-08 13:24:45 -0500

    Set server update required flag when resource config order changes

--- src/hooks/useServerUpdateRequiredMonitor.ts
+++ src/hooks/useServerUpdateRequiredMonitor.ts
@@ -1,4 +1,3 @@
-import { useEntityType } from 'context/EntityContext';
 import { DraftSpecQuery } from 'hooks/useDraftSpecs';
 import { isEqual } from 'lodash';
 import { useEffect, useMemo } from 'react';
@@ -7,86 +6,66 @@ import {
     useBinding_resourceConfigs,
     useBinding_setServerUpdateRequired,
 } from 'stores/Binding/hooks';
-import { Bindings } from 'stores/Binding/types';
-import { hasLength } from 'utils/misc-utils';
-import {
-    getCollectionName,
-    getCollectionNameProp,
-    getDisableProps,
-} from 'utils/workflow-utils';
+import { getBindingIndex, getDisableProps } from 'utils/workflow-utils';
 
 const useServerUpdateRequiredMonitor = (draftSpecs: DraftSpecQuery[]) => {
-    const entityType = useEntityType();
-
     const bindings = useBinding_bindings();
     const resourceConfigs = useBinding_resourceConfigs();
     const setServerUpdateRequired = useBinding_setServerUpdateRequired();
 
-    const collectionNameProp = useMemo(
-        () => getCollectionNameProp(entityType),
-        [entityType]
-    );
-
     const resourceConfigUpdated = useMemo(() => {
-        if (hasLength(draftSpecs)) {
+        if (draftSpecs.length > 0) {
             if (
-                draftSpecs[0]?.spec.bindings.length ===
+                draftSpecs[0].spec.bindings.length ===
                 Object.keys(resourceConfigs).length
             ) {
-                const matchedBindings: Bindings = {};
-
                 // The lengths have not changed so we need to check each binding
-                return draftSpecs[0]?.spec.bindings.some((binding: any) => {
-                    // Pull out resource as that is moved into `data`
-                    const { resource, disable } = binding;
+                return Object.entries(bindings).some(
+                    ([collection, bindingUUIDs]) => {
+                        return bindingUUIDs.some((bindingUUID) => {
+                            const targetBindingIndex = Object.hasOwn(
+                                bindings,
+                                collection
+                            )
+                                ? bindings[collection].findIndex(
+                                      (uuid) => uuid === bindingUUID
+                                  )
+                                : -1;
 
-                    // Snag the name so we know which resource config to check
-                    const collectionName = getCollectionName(
-                        binding[collectionNameProp]
-                    );
-
-                    // If a binding with a given source collection does not exist, an update is needed.
-                    if (!Object.hasOwn(bindings, collectionName)) {
-                        return true;
-                    }
-
-                    const matchedBindingUUIDs: string[] = Object.hasOwn(
-                        matchedBindings,
-                        collectionName
-                    )
-                        ? matchedBindings[collectionName]
-                        : [];
-
-                    return bindings[collectionName]
-                        .filter(
-                            (bindingUUID) =>
-                                !matchedBindingUUIDs.includes(bindingUUID)
-                        )
-                        .every((bindingUUID) => {
-                            // Do a quick simple disabled check before comparing the entire object
-                            if (
-                                // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-                                resourceConfigs[bindingUUID]?.meta?.disable !==
-                                getDisableProps(disable).disable
-                            ) {
-                                return true;
-                            }
-
-                            // Since we checked disabled up above we can not just check if the data changed
-                            const resourceMatched = isEqual(
-                                // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-                                resourceConfigs[bindingUUID]?.data,
-                                resource
+                            const existingBindingIndex = getBindingIndex(
+                                draftSpecs[0]?.spec.bindings,
+                                collection,
+                                targetBindingIndex
                             );
 
-                            if (resourceMatched) {
-                                matchedBindings[collectionName] =
-                                    matchedBindingUUIDs.concat(bindingUUID);
+                            if (existingBindingIndex > -1) {
+                                const { resource, disable } =
+                                    draftSpecs[0].spec.bindings[
+                                        existingBindingIndex
+                                    ];
+
+                                // Do a quick simple disabled check before comparing the entire object
+                                if (
+                                    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+                                    resourceConfigs[bindingUUID]?.meta
+                                        .disable !==
+                                    getDisableProps(disable).disable
+                                ) {
+                                    return true;
+                                }
+
+                                // Since we checked disabled up above we can not just check if the data changed
+                                return !isEqual(
+                                    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+                                    resourceConfigs[bindingUUID]?.data,
+                                    resource
+                                );
                             }
 
-                            return !resourceMatched;
+                            return true;
                         });
-                });
+                    }
+                );
             } else {
                 // Lengths do not match so we know the update is needed
                 return true;
@@ -94,7 +73,7 @@ const useServerUpdateRequiredMonitor = (draftSpecs: DraftSpecQuery[]) => {
         }
 
         return false;
-    }, [bindings, collectionNameProp, draftSpecs, resourceConfigs]);
+    }, [bindings, draftSpecs, resourceConfigs]);
 
     useEffect(() => {
         setServerUpdateRequired(resourceConfigUpdated);
