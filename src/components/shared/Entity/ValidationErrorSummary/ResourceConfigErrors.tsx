fbaaaa6151bbf3ffaf0c0f8e44b9316429e8d6fa
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-03-13 15:31:24 -0400

    Set the config SectionError property in ResourceConfigErrors

--- src/components/shared/Entity/ValidationErrorSummary/ResourceConfigErrors.tsx
+++ src/components/shared/Entity/ValidationErrorSummary/ResourceConfigErrors.tsx
@@ -2,6 +2,7 @@ import SectionError from 'components/shared/Entity/ValidationErrorSummary/Sectio
 import { useMemo } from 'react';
 import { useIntl } from 'react-intl';
 import {
+    useBinding_collections,
     useBinding_fullSourceErrorsExist,
     useBinding_resourceConfigErrors,
 } from 'stores/Binding/hooks';
@@ -10,6 +11,7 @@ import { hasLength } from 'utils/misc-utils';
 function ResourceConfigErrors() {
     const intl = useIntl();
 
+    const collections = useBinding_collections();
     const filteredResourceConfigErrors = useBinding_resourceConfigErrors();
     const fullSourceErrorsExist = useBinding_fullSourceErrorsExist();
 
@@ -38,6 +40,7 @@ function ResourceConfigErrors() {
     return (
         <SectionError
             errors={errorMessages}
+            config={collections}
             configEmptyMessage="entityCreate.endpointConfig.collectionsMissing"
             title="entityCreate.endpointConfig.resourceConfigHaveErrors"
         />
