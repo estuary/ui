c255f5d45ca0e84023ed20766dd506919093d213
Travis Jenkins <travis@estuary.dev>
2024-03-29 10:10:40 -0400

    Make events always log so they are easy to find (#1061)

--- src/services/shared.ts
+++ src/services/shared.ts
@@ -2,21 +2,6 @@ import LogRocket from 'logrocket';
 import { isProduction } from 'utils/env-utils';
 import { CustomEvents } from './types';
 
-export const logRocketEvent = (
-    event: CustomEvents | string,
-    eventProperties?: any
-) => {
-    // Just want to be very very safe
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (LogRocket?.track) {
-        LogRocket.track(event, eventProperties);
-    }
-
-    if (process.env.NODE_ENV === 'development') {
-        console.log(event, eventProperties);
-    }
-};
-
 export const logRocketConsole = (message: string, ...props: any[]) => {
     // Just want to be very very safe
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
@@ -27,6 +12,19 @@ export const logRocketConsole = (message: string, ...props: any[]) => {
     if (!isProduction) {
         console.log(message, props);
     }
+};
+
+export const logRocketEvent = (
+    event: CustomEvents | string,
+    eventProperties?: any
+) => {
+    // Just want to be very very safe
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (LogRocket?.track) {
+        LogRocket.track(event, eventProperties);
+    }
+
+    logRocketConsole(`Event Logging : ${event}`, eventProperties);
 };
 
 export const FAILED_TO_FETCH = 'FAILED TO FETCH';
