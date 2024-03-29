ffc4e3fdc39a2d2bbdff1e5286dec145767524f3
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-03-29 13:34:15 -0400

    Create settings tab tenant selector

---
+++ src/components/admin/Settings/TenantOptions.tsx
@@ -0,0 +1,78 @@
+import { Skeleton } from '@mui/material';
+import AutocompletedField from 'components/shared/toolbar/AutocompletedField';
+import { useTenantDetails } from 'context/fetcher/Tenant';
+import useGlobalSearchParams, {
+    GlobalSearchParams,
+} from 'hooks/searchParams/useGlobalSearchParams';
+import { noop } from 'lodash';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { useIntl } from 'react-intl';
+import { hasLength } from 'utils/misc-utils';
+
+function TenantOptions() {
+    const intl = useIntl();
+
+    const tenants = useTenantDetails();
+
+    const handledDefault = useRef(false);
+    const defaultSelectedTenant = useGlobalSearchParams(
+        GlobalSearchParams.PREFIX
+    );
+    const [autoCompleteValue, setAutoCompleteValue] = useState<string | null>(
+        null
+    );
+    const [inputValue, setInputValue] = useState('');
+    const [defaultValue, setDefaultValue] = useState<string | null>(null);
+
+    const tenantNames = useMemo<string[] | null>(
+        () =>
+            tenants && tenants.length > 0
+                ? tenants.map(({ tenant }) => tenant)
+                : null,
+        [tenants]
+    );
+
+    useEffect(() => {
+        if (tenantNames) {
+            // Try using the value from the URL first so if the param gets updated the dropdown changes
+            const newVal =
+                hasLength(defaultSelectedTenant) &&
+                tenantNames.includes(defaultSelectedTenant)
+                    ? defaultSelectedTenant
+                    : tenantNames[0];
+
+            setAutoCompleteValue(newVal);
+
+            if (!handledDefault.current) {
+                setDefaultValue(newVal);
+                handledDefault.current = true;
+            }
+        }
+    }, [defaultSelectedTenant, tenantNames]);
+
+    return defaultValue && tenantNames ? (
+        <AutocompletedField
+            label={intl.formatMessage({
+                id: 'common.tenant',
+            })}
+            options={tenantNames}
+            defaultValue={defaultValue}
+            changeHandler={noop}
+            autocompleteSx={{ flexGrow: 1 }}
+            AutoCompleteOptions={{
+                inputValue,
+                onInputChange: (_event, newInputValue) => {
+                    setInputValue(newInputValue);
+                },
+                onChange: (_event, newAutoCompleteValue) => {
+                    setAutoCompleteValue(newAutoCompleteValue);
+                },
+                value: autoCompleteValue,
+            }}
+        />
+    ) : (
+        <Skeleton sx={{ flexGrow: 1 }} />
+    );
+}
+
+export default TenantOptions;
--- src/components/admin/Settings/index.tsx
+++ src/components/admin/Settings/index.tsx
@@ -1,6 +1,7 @@
-import { Divider, Stack } from '@mui/material';
+import { Divider, Grid, Stack } from '@mui/material';
 import { authenticatedRoutes } from 'app/routes';
 import PrefixAlerts from 'components/admin/Settings/PrefixAlerts';
+import TenantOptions from 'components/admin/Settings/TenantOptions';
 import AdminTabs from 'components/admin/Tabs';
 import usePageTitle from 'hooks/usePageTitle';
 import StorageMappings from './StorageMappings';
@@ -13,6 +14,21 @@ function Settings() {
     return (
         <>
             <AdminTabs />
+
+            <Grid
+                container
+                spacing={{ xs: 3, md: 2 }}
+                sx={{ p: 2, justifyContent: 'flex-end' }}
+            >
+                <Grid
+                    item
+                    xs={12}
+                    md={3}
+                    sx={{ mt: 2.5, display: 'flex', alignItems: 'end' }}
+                >
+                    <TenantOptions />
+                </Grid>
+            </Grid>
 
             <PrefixAlerts />
 
