5e39dfc5f342d9978616c3068c6805054b936d34
Travis Jenkins <travis@estuary.dev>
2024-04-02 12:21:16 -0400

    Sharing the status indicator and label between the table and tooltip version

--- src/components/shared/Entity/Shard/StatusIndicatorAndLabel.tsx
+++ src/components/shared/Entity/Shard/StatusIndicatorAndLabel.tsx
@@ -1,56 +1,53 @@
-import { Box, Stack, TableCell, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
 import { TaskShardDetails } from 'stores/ShardDetail/types';
 
 interface Props {
     shard: TaskShardDetails;
+    smallMargin?: boolean;
 }
 
 const INDICATOR_SIZE = 16;
-const INDICATOR_MARGIN = 12;
-const NOTES_INDENT = INDICATOR_SIZE + INDICATOR_MARGIN;
 
-function StatusIndicatorAndLabel({ shard }: Props) {
+function StatusIndicatorAndLabel({ shard, smallMargin }: Props) {
     const { id, color, disabled, messageId, messageNoteId } = shard;
 
+    const INDICATOR_MARGIN = smallMargin ? 4 : 12;
+    const NOTES_INDENT = INDICATOR_SIZE + INDICATOR_MARGIN;
+
     return (
-        <TableCell width={250} key={`status-indicator-for-shard__${id}`}>
-            <Stack>
-                <Box>
-                    <span
-                        style={{
-                            height: INDICATOR_SIZE,
-                            width: INDICATOR_SIZE,
-                            marginRight: INDICATOR_MARGIN,
-                            border: disabled ? `solid 2px ${color}` : 0,
-                            backgroundColor: disabled ? '' : color,
-                            borderRadius: 50,
-                            display: 'inline-block',
-                            verticalAlign: 'middle',
-                        }}
-                    />
+        <Stack key={`status-indicator-for-shard__${id}`}>
+            <Box>
+                <span
+                    style={{
+                        height: INDICATOR_SIZE,
+                        width: INDICATOR_SIZE,
+                        marginRight: INDICATOR_MARGIN,
+                        border: disabled ? `solid 2px ${color}` : 0,
+                        backgroundColor: disabled ? '' : color,
+                        borderRadius: 50,
+                        display: 'inline-block',
+                        verticalAlign: 'middle',
+                    }}
+                />
 
-                    <Typography
-                        component="span"
-                        sx={{ verticalAlign: 'middle' }}
-                    >
-                        <FormattedMessage id={messageId} />
-                    </Typography>
-                </Box>
+                <Typography component="span" sx={{ verticalAlign: 'middle' }}>
+                    <FormattedMessage id={messageId} />
+                </Typography>
+            </Box>
 
-                {messageNoteId ? (
-                    <Typography
-                        component="span"
-                        style={{
-                            marginLeft: NOTES_INDENT,
-                            verticalAlign: 'middle',
-                        }}
-                    >
-                        <FormattedMessage id={messageNoteId} />
-                    </Typography>
-                ) : null}
-            </Stack>
-        </TableCell>
+            {messageNoteId ? (
+                <Typography
+                    component="span"
+                    style={{
+                        marginLeft: NOTES_INDENT,
+                        verticalAlign: 'middle',
+                    }}
+                >
+                    <FormattedMessage id={messageNoteId} />
+                </Typography>
+            ) : null}
+        </Stack>
     );
 }
 
