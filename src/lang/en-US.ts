5cb2724700740db7a04e147ee812f95e6c732e2a
Travis Jenkins <travis@estuary.dev>
2024-04-09 11:15:06 -0400

    PR: removing unused content

--- src/lang/en-US.ts
+++ src/lang/en-US.ts
@@ -413,7 +413,6 @@ const EntityTable: ResolvedIntlConfig['messages'] = {
     'entityTable.stats.read.docs': `Docs Read`,
 
     'entityTable.stats.error': `Failed to fetch stats.`,
-    'entityTable.stats.filterMenu': `Stats for`,
 
     'entityTable.unmatchedFilter.header': `No results found.`,
     'entityTable.unmatchedFilter.message': `We couldn't find any data matching your search. Please try a different filter.`,
