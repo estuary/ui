b9b9cea3b3ac4f8c8d26671fad9c851e4efa041b
Travis Jenkins <travis@estuary.dev>
2024-05-01 14:42:32 -0400

    Starting to ensure we have all the data we need to render graphs

--- src/hooks/useDetailsStats.ts
+++ src/hooks/useDetailsStats.ts
@@ -4,6 +4,9 @@ import { useSelectNew } from 'hooks/supabase-swr/hooks/useSelect';
 import { hasLength } from 'utils/misc-utils';
 import { CatalogStats_Details } from 'types';
 import useDetailsUsageStore from 'components/shared/Entity/Details/Usage/useDetailsUsageStore';
+import { useMemo } from 'react';
+import { DateTime, Interval } from 'luxon';
+import { find } from 'lodash';
 
 function useDetailsStats(catalogName: string, grain: string) {
     const entityType = useEntityType();
@@ -20,11 +23,52 @@ function useDetailsStats(catalogName: string, grain: string) {
         }
     );
 
+    const stats = useMemo(() => {
+        if (data?.data && data.data.length > 0) {
+            const max = DateTime.utc().startOf('hour');
+
+            // Subtracting 1 here because the interval is inclusive of the minimum
+            const min = max.minus({ hours: range - 1 });
+
+            // Go through the entire interval and populate the response if we have data
+            //  otherwise default to an "empty" response
+            const timeRange: CatalogStats_Details[] = Interval.fromDateTimes(
+                min,
+                max.plus({ hours: 1 })
+            )
+                .splitBy({ hours: 1 })
+                .map((timeInterval) => {
+                    const ts = timeInterval.start?.toFormat(
+                        `yyyy-MM-dd'T'HH:mm:ssZZ`
+                    );
+                    const currentStat = find(data.data, { ts });
+
+                    if (currentStat) {
+                        return currentStat;
+                    }
+
+                    return {
+                        ts,
+                        catalog_name: '',
+                        grain: 'hourly',
+                        bytes_read: 0,
+                        docs_read: 0,
+                        bytes_written: 0,
+                        docs_written: 0,
+                    } as CatalogStats_Details;
+                });
+
+            return timeRange;
+        }
+
+        return undefined;
+    }, [data?.data, range]);
+
     // Not always returning an array so we know when the empty array is a response
     //  vs a default. That way we can keep showing old data while new data is geing fetched
     //  during isValidating
     return {
-        stats: data?.data,
+        stats,
         error,
         isValidating,
     };
