b83f1be4f98f1f1bc041e8648c1d0fa5b446981f
Kiahna Tucker <77648584+kiahna-tucker@users.noreply.github.com>
2024-05-13 15:22:30 -0400

    Enable provisioning write access to a prefix (#1106)
    * Enable provisioning write access to a prefix
    
    * Display custom error message for duplicate grant
    
    * Remove logging statement

--- src/lang/en-US.ts
+++ src/lang/en-US.ts
@@ -560,6 +560,7 @@ const AdminPage: ResolvedIntlConfig['messages'] = {
     'admin.prefix.issueGrant.notification.success.message': `{objectRole} has been shared with {subjectRole}.`,
     'admin.prefix.issueGrant.error.invalidPrefix': `The value entered in the Shared Prefix text field is not an extension of an existing prefix.`,
     'admin.prefix.confirmation.listItem': `{subjectRole} having {capability} over {objectRole}.`,
+    'admin.prefix.issueGrant.error.duplicatePrefix': `The requested access has already been provisioned.`,
 
     'admin.alerts.header': `Organization Notifications`,
     'admin.alerts.cta.addAlertMethod': `Configure Notifications`,
