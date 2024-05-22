5612f19c7ee9cafa48745e294e15f9cf32914568
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-05-22 11:30:59 -0400

    Remove getStoreName

--- src/stores/DetailsForm/hooks.ts
+++ src/stores/DetailsForm/hooks.ts
@@ -1,23 +1,7 @@
 import useGlobalSearchParams, {
     GlobalSearchParams,
 } from 'hooks/searchParams/useGlobalSearchParams';
-import { DetailsFormStoreNames } from 'stores/names';
-import { Entity } from 'types';
 import { useDetailsFormStore } from './Store';
-
-export const getStoreName = (entityType: Entity): DetailsFormStoreNames => {
-    switch (entityType) {
-        case 'capture':
-            return DetailsFormStoreNames.CAPTURE;
-        case 'materialization':
-            return DetailsFormStoreNames.MATERIALIZATION;
-        case 'collection':
-            return DetailsFormStoreNames.COLLECTION;
-        default: {
-            throw new Error('Invalid DetailsForm store name');
-        }
-    }
-};
 
 // Selector hooks
 export const useDetailsForm_changed_connectorId = () => {
