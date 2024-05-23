160e4f776d4d81c0c51b7c2ec2dbeed91c98bdbb
Travis Jenkins <travis@estuary.dev>
2024-05-23 10:18:35 -0400

    Adding new button to view details

---
+++ src/components/shared/Entity/Actions/ViewDetails.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@mui/material';
+
+import { buttonSx } from 'components/shared/Entity/Header';
+import { FormattedMessage } from 'react-intl';
+
+import useEntityWorkflowHelpers from '../hooks/useEntityWorkflowHelpers';
+
+function EntityViewDetails() {
+    const { closeLogs } = useEntityWorkflowHelpers();
+
+    return (
+        <Button
+            onClick={async (event) => {
+                event.preventDefault();
+                closeLogs();
+            }}
+            sx={buttonSx}
+        >
+            <FormattedMessage id="cta.goToDetails" />
+        </Button>
+    );
+}
+
+export default EntityViewDetails;
