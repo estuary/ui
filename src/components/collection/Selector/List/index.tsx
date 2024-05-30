2fa613f6b7a9af134a20142f32d33f00fb1e145b
Travis Jenkins <travis@estuary.dev>
2024-05-30 17:10:33 -0400

    Reverting change

--- src/components/collection/Selector/List/index.tsx
+++ src/components/collection/Selector/List/index.tsx
@@ -102,8 +102,9 @@ function CollectionSelectorList({
     const formStatus = useFormStateStore_status();
 
     const selectionEnabled =
-        // currentBindingUUID &&
-        setCurrentBinding && formStatus !== FormStatus.UPDATING;
+        currentBindingUUID &&
+        setCurrentBinding &&
+        formStatus !== FormStatus.UPDATING;
 
     const [filterModel, setFilterModel] =
         useState<GridFilterModel>(defaultFilterModel);
