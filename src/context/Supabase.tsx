ea1efface43b1173c17729515fe660ab7da33347
Travis Jenkins <travis@estuary.dev>
2024-06-10 16:51:14 -0400

    New supabase provider file

---
+++ src/context/Supabase.tsx
@@ -0,0 +1,40 @@
+import { createClient } from '@supabase/supabase-js';
+import FullPageSpinner from 'components/fullPage/Spinner';
+import { BaseComponentProps } from 'types';
+import { useUser } from './UserContext';
+
+if (
+    !import.meta.env.VITE_SUPABASE_URL ||
+    !import.meta.env.VITE_SUPABASE_ANON_KEY
+) {
+    throw new Error(
+        'You must set the Supabase url and anon key in the env settings.'
+    );
+}
+
+const supabaseSettings = {
+    url: import.meta.env.VITE_SUPABASE_URL,
+    anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
+};
+
+export const supabaseClient = createClient(
+    supabaseSettings.url,
+    supabaseSettings.anonKey
+);
+
+// This is not a real provider. This is here so that we know createClient is called early and also
+//  so it is called in a somewhat consistent order. This is also waiting until the client has been
+//  constructed before letting the application start rendering.
+function SupabaseProvider({ children }: BaseComponentProps) {
+    const { initialized } = useUser();
+
+    if (!initialized) {
+        return <FullPageSpinner />;
+    }
+
+    // Only returning the child and need the JSX Fragment
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    return <>{children}</>;
+}
+
+export default SupabaseProvider;
