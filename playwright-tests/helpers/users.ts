f6623e3d00f311b481fb553cd76e84e941acf31c
Travis Jenkins <travis@estuary.dev>
2024-06-26 12:46:26 -0400

    Adding more users

--- playwright-tests/helpers/users.ts
+++ playwright-tests/helpers/users.ts
@@ -1,4 +1,7 @@
 export const USERS = {
+    admin: 'ui-test__admin',
     captures: 'ui-test__captures',
+    collections: 'ui-test__collections',
     homepage: 'ui-test__homepage',
+    materializations: 'ui-test__materialization',
 };
