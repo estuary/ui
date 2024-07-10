a7ec0fc9d964226d35dfdbc35be6a4bfcefe1d19
Travis Jenkins <travis@estuary.dev>
2024-07-10 14:31:29 -0400

    expand cache a bit do not poll on the hiding preview data

--- src/hooks/gatewayAuthToken/cache.ts
+++ src/hooks/gatewayAuthToken/cache.ts
@@ -3,7 +3,7 @@ import { GatewayAuthTokenResponse } from 'types';
 
 // The limit of 10 is totally arbitrary. Felt like an okay number so all 3 tables could have
 //  something in cache and allow someone some flexibility to search
-const gatewayTokenCache = new LRUMap<string, GatewayAuthTokenResponse>(10);
+const gatewayTokenCache = new LRUMap<string, GatewayAuthTokenResponse>(100);
 
 export const storeGatewayAuthConfig = (
     key: string | null | undefined,
