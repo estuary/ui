a0accff6b73a2fb297c22af8cd41e6fbb149529a
Travis Jenkins <travis@estuary.dev>
2024-07-19 16:49:56 -0400

    Renaming file to match new context name

--- src/context/AuthenticatedHydrators.tsx
+++ src/context/AuthenticatedHydrators.tsx
@@ -1,6 +1,6 @@
 import { EntitiesHydrator } from 'stores/Entities/Hydrator';
 import { BaseComponentProps } from 'types';
-import { TenantBillingDetailsContextProvider } from './fetcher/Tenant';
+import { TenantBillingDetailsContextProvider } from './fetcher/TenantBillingDetails';
 
 function AuthenticatedHydrators({ children }: BaseComponentProps) {
     return (
