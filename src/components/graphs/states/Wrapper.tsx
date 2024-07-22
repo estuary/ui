33a4aaa41f8e9db18cbf45c3f6a3d10bf7cd5a7f
Travis Jenkins <travis@estuary.dev>
2024-07-22 15:34:54 -0400

    PR: checking active for the graph

--- src/components/graphs/states/Wrapper.tsx
+++ src/components/graphs/states/Wrapper.tsx
@@ -8,6 +8,7 @@ import { hasLength } from 'utils/misc-utils';
 import { eChartsTooltipSX } from '../tooltips';
 
 function GraphStateWrapper({ children }: BaseComponentProps) {
+    const billingStoreActive = useBillingStore((state) => state.active);
     const billingStoreHydrated = useBillingStore((state) => state.hydrated);
     const networkFailed = useBillingStore((state) => state.networkFailed);
     const billingHistory = useBillingStore((state) => state.invoices);
@@ -25,7 +26,7 @@ function GraphStateWrapper({ children }: BaseComponentProps) {
         );
     }
 
-    if (billingStoreHydrated) {
+    if (!billingStoreActive && billingStoreHydrated) {
         return hasLength(billingHistory) ? (
             <Box sx={eChartsTooltipSX}>{children}</Box>
         ) : (
