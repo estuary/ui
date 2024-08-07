5fe9b09531791b9d39b50b171b3095eb2be1d044
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-08-07 13:45:37 -0400

    Enable welcome section to be hidden

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 //TODO (UI / UX) - These icons are not final
-import { Toolbar, Typography } from '@mui/material';
+import { Collapse, IconButton, Toolbar, Typography } from '@mui/material';
 import { authenticatedRoutes } from 'app/routes';
 import Dashboard from 'components/dashboard';
 import HeroImageAndDescription from 'components/hero';
@@ -8,7 +8,8 @@ import useGlobalSearchParams, {
     GlobalSearchParams,
 } from 'hooks/searchParams/useGlobalSearchParams';
 import usePageTitle from 'hooks/usePageTitle';
-import { useMemo } from 'react';
+import { Plus, Xmark } from 'iconoir-react';
+import { useMemo, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 const Home = () => {
@@ -17,6 +18,8 @@ const Home = () => {
     const homePageError = useGlobalSearchParams(
         GlobalSearchParams.HOME_PAGE_ERROR
     );
+
+    const [welcomeShown, setWelcomeShown] = useState(false);
 
     // home page error related
     const notificationMessage = useMemo(() => {
@@ -35,13 +38,29 @@ const Home = () => {
                     notificationTitle="directives.grant.skipped.title"
                 />
             ) : null}
-            <Toolbar sx={{ justifyContent: 'center', mb: 3 }}>
-                <Typography variant="h4">
+
+            <Toolbar
+                disableGutters
+                sx={{
+                    justifyContent: welcomeShown ? 'center' : 'flex-start',
+                    mb: 3,
+                }}
+            >
+                <Typography variant={welcomeShown ? 'h4' : 'h6'}>
                     <FormattedMessage id="home.main.header" />
                 </Typography>
+
+                <IconButton
+                    onClick={() => setWelcomeShown(!welcomeShown)}
+                    style={{ position: 'absolute', top: 14, right: 0 }}
+                >
+                    {welcomeShown ? <Xmark /> : <Plus />}
+                </IconButton>
             </Toolbar>
 
-            <HeroImageAndDescription />
+            <Collapse in={welcomeShown}>
+                <HeroImageAndDescription />
+            </Collapse>
 
             <Dashboard />
         </>
