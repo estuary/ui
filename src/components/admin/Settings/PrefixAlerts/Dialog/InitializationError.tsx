a0ec8245ee6562f4d111af6d370632e50ea2e0e7
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-08-14 10:02:35 -0400

    Remove ability to retry failed dialog initialization

--- src/components/admin/Settings/PrefixAlerts/Dialog/InitializationError.tsx
+++ src/components/admin/Settings/PrefixAlerts/Dialog/InitializationError.tsx
@@ -1,9 +1,5 @@
-import { AlertTitle, Box, Stack, Typography } from '@mui/material';
-import SafeLoadingButton from 'components/SafeLoadingButton';
-import AlertBox from 'components/shared/AlertBox';
-import { useState } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { initializeNotificationSubscriptions } from '../useAlertSubscriptionDialog';
+import { Box } from '@mui/material';
+import Error from 'components/shared/Error';
 import useAlertSubscriptionsStore from '../useAlertSubscriptionsStore';
 
 export default function InitializationError() {
@@ -11,60 +7,13 @@ export default function InitializationError() {
         (state) => state.serverError
     );
 
-    const prefix = useAlertSubscriptionsStore((state) => state.prefix);
-
-    const initializeSubscriptionState = useAlertSubscriptionsStore(
-        (state) => state.initializeState
-    );
-
-    const [loading, setLoading] = useState(false);
-
-    const retryInitialization = async (
-        event: React.MouseEvent<HTMLElement>
-    ) => {
-        event.preventDefault();
-
-        setLoading(true);
-
-        const { data: existingSubscriptions, error } =
-            await initializeNotificationSubscriptions(prefix);
-
-        initializeSubscriptionState(prefix, existingSubscriptions, error);
-        setLoading(false);
-    };
-
     if (!serverError) {
         return null;
     }
 
     return (
         <Box style={{ marginBottom: 16 }}>
-            <AlertBox
-                severity="error"
-                short
-                title={
-                    <AlertTitle>
-                        <FormattedMessage id="error.title" />
-                    </AlertTitle>
-                }
-            >
-                <Stack spacing={1}>
-                    <Typography>
-                        <FormattedMessage id="admin.alerts.error.initializationFailed" />
-                    </Typography>
-
-                    <Stack direction="row" sx={{ justifyContent: 'flex-end' }}>
-                        <SafeLoadingButton
-                            loading={loading}
-                            onClick={retryInitialization}
-                            size="small"
-                            variant="contained"
-                        >
-                            <FormattedMessage id="cta.restart" />
-                        </SafeLoadingButton>
-                    </Stack>
-                </Stack>
-            </AlertBox>
+            <Error condensed error={serverError} severity="error" />
         </Box>
     );
 }
--- src/components/admin/Settings/PrefixAlerts/useAlertSubscriptionDialog.ts
+++ src/components/admin/Settings/PrefixAlerts/useAlertSubscriptionDialog.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { formatNotificationSubscriptionsByPrefix } from 'utils/notification-utils';
 import useAlertSubscriptionsStore from './useAlertSubscriptionsStore';
 
-export const initializeNotificationSubscriptions = async (prefix?: string) => {
+const initializeNotificationSubscriptions = async (prefix?: string) => {
     const { data, error } = await getNotificationSubscriptions(prefix);
 
     if (data) {
