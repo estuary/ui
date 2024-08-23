0e2db98e4e632bb0b630278dc4ce091b329bc9ad
Travis Jenkins <travis@estuary.dev>
2024-08-23 16:34:32 -0400

    using common value

--- src/components/shared/guards/ConnectorSelected.tsx
+++ src/components/shared/guards/ConnectorSelected.tsx
@@ -3,14 +3,13 @@ import useGlobalSearchParams, {
 } from 'hooks/searchParams/useGlobalSearchParams';
 import { Navigate } from 'react-router-dom';
 import { BaseComponentProps } from 'types';
+import { MAC_ADDR_RE } from 'validation';
 
 // This 'navigateToPath' is so stupid and so annoying. However, for whatever reason
 //  if you have the navigate to equal to '..' it threw you back up too many levels
 interface Props extends BaseComponentProps {
     navigateToPath: string;
 }
-
-const MAC_ADDR_RE = new RegExp(/^([0-9A-F]{2}:){7}([0-9A-F]{2})$/i);
 
 // This is quick - but really we should just make a general "SearchParamGuard" that
 //  can easily check we have everything we need.
