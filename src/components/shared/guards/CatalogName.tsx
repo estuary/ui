37b1fde16647e95aaee985c8eea95ef3c5491158
Travis Jenkins <travis@estuary.dev>
2024-08-23 16:32:20 -0400

    Putting eventing in common place

--- src/components/shared/guards/CatalogName.tsx
+++ src/components/shared/guards/CatalogName.tsx
@@ -2,8 +2,6 @@ import useGlobalSearchParams, {
     GlobalSearchParams,
 } from 'hooks/searchParams/useGlobalSearchParams';
 import EntityNotFound from 'pages/error/EntityNotFound';
-import { logRocketEvent } from 'services/shared';
-import { CustomEvents } from 'services/types';
 import { BaseComponentProps } from 'types';
 import { hasLength } from 'utils/misc-utils';
 import { validateCatalogName } from 'validation';
@@ -11,10 +9,7 @@ import { validateCatalogName } from 'validation';
 function CatalogNameGuard({ children }: BaseComponentProps) {
     const catalogName = useGlobalSearchParams(GlobalSearchParams.CATALOG_NAME);
 
-    if (hasLength(validateCatalogName(catalogName))) {
-        logRocketEvent(CustomEvents.ERROR_INVALID_CATALOG_NAME, {
-            catalogName,
-        });
+    if (hasLength(validateCatalogName(catalogName, false, true))) {
         return <EntityNotFound catalogName={catalogName} />;
     }
 
