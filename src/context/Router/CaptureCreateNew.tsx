bbbf31f8ea90d433f028b89b3db2ff83ab0c91d6
Travis Jenkins <travis@estuary.dev>
2024-08-23 15:24:16 -0400

    Adding a guard to ensure we have a connector selected

--- src/context/Router/CaptureCreateNew.tsx
+++ src/context/Router/CaptureCreateNew.tsx
@@ -1,5 +1,7 @@
+import { authenticatedRoutes } from 'app/routes';
 import CaptureCreate from 'components/capture/Create';
 import AdminCapabilityGuard from 'components/shared/guards/AdminCapability';
+import ConnectorSelectedGuard from 'components/shared/guards/ConnectorSelected';
 import { EntityContextProvider } from 'context/EntityContext';
 import { WorkflowContextProvider } from 'context/Workflow';
 
@@ -8,7 +10,13 @@ function CaptureCreateNewRoute() {
         <EntityContextProvider value="capture">
             <WorkflowContextProvider value="capture_create">
                 <AdminCapabilityGuard>
-                    <CaptureCreate />
+                    <ConnectorSelectedGuard
+                        navigateToPath={
+                            authenticatedRoutes.captures.create.fullPath
+                        }
+                    >
+                        <CaptureCreate />
+                    </ConnectorSelectedGuard>
                 </AdminCapabilityGuard>
             </WorkflowContextProvider>
         </EntityContextProvider>
