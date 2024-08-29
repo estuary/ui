be2dbd1b2afd1dffc6cd7988da2f1ea51988c44c
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-08-29 17:46:08 -0400

    Create a login error component for the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,43 +10,23 @@ import {
 import { authenticatedRoutes } from 'app/routes';
 import HeroImageAndDescription from 'components/hero';
 import Dashboard from 'components/home/dashboard';
-import LoginNotifications from 'components/login/Notifications';
+import LoginError from 'components/home/LoginError';
 import { useShowDashboardWelcome } from 'context/DashboardWelcome';
 import { defaultOutline, semiTransparentBackground } from 'context/Theme';
-import useGlobalSearchParams, {
-    GlobalSearchParams,
-} from 'hooks/searchParams/useGlobalSearchParams';
 import usePageTitle from 'hooks/usePageTitle';
 import { Plus, Xmark } from 'iconoir-react';
-import { useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 const Home = () => {
     const theme = useTheme();
 
     usePageTitle({ header: authenticatedRoutes.home.title });
-    const homePageError = useGlobalSearchParams(
-        GlobalSearchParams.HOME_PAGE_ERROR
-    );
 
     const { welcomeShown, setWelcomeShown } = useShowDashboardWelcome();
 
-    // home page error related
-    const notificationMessage = useMemo(() => {
-        if (homePageError) {
-            return 'directives.grant.skipped.message';
-        }
-        return null;
-    }, [homePageError]);
-
     return (
         <>
-            {notificationMessage ? (
-                <LoginNotifications
-                    notificationMessage={notificationMessage}
-                    notificationTitle="directives.grant.skipped.title"
-                />
-            ) : null}
+            <LoginError />
 
             <Box
                 sx={{
