ac59c34f07fc70691580101671a52f44d5995c5b
Travis Jenkins <travis@estuary.dev>
2024-09-05 16:39:20 -0400

    new files for chip list typing

---
+++ src/components/shared/ChipList/types.ts
@@ -0,0 +1,20 @@
+export interface ChipDisplay {
+    display: string;
+    link?: string;
+    title?: string;
+}
+
+export interface ChipWrapperProps {
+    onClick?: () => void;
+    disabled?: boolean;
+    stripPath?: boolean;
+    title?: string;
+    val: ChipDisplay;
+}
+
+export interface ChipListProps {
+    values: string[] | ChipDisplay[];
+    disabled?: boolean;
+    maxChips?: number;
+    stripPath?: boolean;
+}
