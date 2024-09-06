67df7f417706746cdcac81c2c6fe6f10a8207f73
Travis Jenkins <travis@estuary.dev>
2024-09-06 12:35:58 -0400

    fixing types

--- playwright-tests/helpers/types.ts
+++ playwright-tests/helpers/types.ts
@@ -8,6 +8,7 @@ export interface AuthProps extends AuthFile {
     name: string;
     filePath: string;
     saved: boolean;
+    tenant: string;
 }
 
 export type StartSessionWithUserResponse = AuthProps;
