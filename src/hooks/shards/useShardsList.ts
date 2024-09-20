940af975cda54f1aa38777277769b639c82771f3
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-09-20 10:36:18 -0400

    Depreciate use of gateway_auth_token rpc in useShardsList

--- src/hooks/shards/useShardsList.ts
+++ src/hooks/shards/useShardsList.ts
@@ -1,13 +1,36 @@
 import { useUserStore } from 'context/User/useUserContextStore';
 import { ShardClient, ShardSelector } from 'data-plane-gateway';
-import useGatewayAuthToken from 'hooks/gatewayAuthToken/useGatewayAuthToken';
+import { Shard } from 'data-plane-gateway/types/shard_client';
 import { useMemo } from 'react';
 import { logRocketConsole } from 'services/shared';
 import useSWR from 'swr';
-import {
-    dataPlaneFetcher_list,
-    shouldRefreshToken,
-} from 'utils/dataPlane-utils';
+import { dataPlaneFetcher_list } from 'utils/dataPlane-utils';
+
+interface TaskAuthorizationResponse {
+    brokerAddress: string;
+    brokerToken: string;
+    opsLogsJournal: string;
+    opsStatsJournal: string;
+    reactorAddress: string;
+    reactorToken: string;
+    retryMillis: number;
+    shardIdPrefix: string;
+}
+
+const authorizeTask = async (
+    accessToken: string | undefined,
+    catalogName: string
+): Promise<TaskAuthorizationResponse> =>
+    fetch(`http://localhost:8675/authorize/user/task`, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            task: catalogName,
+        }),
+    }).then((response) => response.json());
 
 // These status do not change often so checking every 30 seconds is probably enough
 const INTERVAL = 30000;
@@ -15,53 +38,57 @@ const INTERVAL = 30000;
 const useShardsList = (catalogNames: string[]) => {
     const session = useUserStore((state) => state.session);
 
-    const { data: gatewayConfig, refresh: refreshAccess } =
-        useGatewayAuthToken(catalogNames);
-
-    const shardClient = useMemo(() => {
-        if (gatewayConfig?.gateway_url && gatewayConfig.token) {
-            const authToken = gatewayConfig.token;
-            const baseUrl = new URL(gatewayConfig.gateway_url);
-
-            return new ShardClient(baseUrl, authToken);
-        } else {
-            return null;
-        }
-    }, [gatewayConfig]);
-
-    const taskSelector = new ShardSelector();
-    catalogNames.forEach((name) => taskSelector.task(name));
-
     const fetcher = async (_url: string) => {
         // We check this in the swrKey memo so this should never actually happen
-        if (!(shardClient && session)) {
+        if (!session) {
             return { shards: [] };
         }
 
-        const dataPlaneListResponse = await dataPlaneFetcher_list(
-            shardClient,
-            taskSelector,
-            'ShardsList'
-        );
+        const shardPromises = catalogNames.map(async (name) => {
+            const { reactorAddress, reactorToken } = await authorizeTask(
+                session.access_token,
+                name
+            );
+            const reactorURL = new URL(reactorAddress);
 
-        if (!Array.isArray(dataPlaneListResponse)) {
-            return Promise.reject(dataPlaneListResponse);
-        }
+            const shardClient = new ShardClient(reactorURL, reactorToken);
+            const taskSelector = new ShardSelector().task(name);
 
-        return {
-            shards:
-                dataPlaneListResponse.length > 0 ? dataPlaneListResponse : [],
-        };
+            const dataPlaneListResponse = await dataPlaneFetcher_list(
+                shardClient,
+                taskSelector,
+                'ShardsList'
+            );
+
+            if (!Array.isArray(dataPlaneListResponse)) {
+                return Promise.reject(dataPlaneListResponse);
+            }
+
+            return {
+                shards:
+                    dataPlaneListResponse.length > 0
+                        ? dataPlaneListResponse
+                        : [],
+            };
+        });
+
+        const shardResponses = await Promise.all(shardPromises);
+
+        const response: { shards: Shard[] } = { shards: [] };
+
+        shardResponses.forEach(({ shards }) => {
+            response.shards = response.shards.concat(shards);
+        });
+
+        return response;
     };
 
     const swrKey = useMemo(
         () =>
-            shardClient && session && catalogNames.length > 0
-                ? `shards-${
-                      gatewayConfig?.gateway_url ?? '__missing_gateway_url__'
-                  }-${catalogNames.join('-')}`
+            session && catalogNames.length > 0
+                ? `shards-${catalogNames.join('-')}`
                 : null,
-        [shardClient, session, catalogNames, gatewayConfig?.gateway_url]
+        [session, catalogNames]
     );
 
     return useSWR(swrKey, fetcher, {
@@ -71,15 +98,6 @@ const useShardsList = (catalogNames: string[]) => {
         revalidateOnFocus: false, // We're already refreshing and these status do not change often
         onError: async (error: string | Error) => {
             logRocketConsole('useShardsList on error', { error });
-
-            // Check if we need to refresh the access token before returning the error
-            if (
-                shouldRefreshToken(
-                    typeof error === 'object' ? error.message : error
-                )
-            ) {
-                await refreshAccess();
-            }
         },
     });
 };
