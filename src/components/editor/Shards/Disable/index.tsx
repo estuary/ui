6c95eab7cf46e0ddfccb20164257913b8b729f98
Travis Jenkins <travis@estuary.dev>
2024-09-24 13:50:05 -0400

    Fixing the warning to display correctly Moving the hook fetch into the component to reduce extra renders

--- src/components/editor/Shards/Disable/index.tsx
+++ src/components/editor/Shards/Disable/index.tsx
@@ -2,17 +2,11 @@ import { Stack, Typography } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
 import { useEntityType } from 'context/EntityContext';
 import { useEditorStore_persistedDraftId } from 'components/editor/Store/hooks';
-import useGlobalSearchParams, {
-    GlobalSearchParams,
-} from 'hooks/searchParams/useGlobalSearchParams';
+
 import ShardsDisableForm from './Form';
 import ShardsDisableWarning from './Warning';
 
 function ShardsDisable() {
-    const forcedToEnable = useGlobalSearchParams(
-        GlobalSearchParams.FORCED_SHARD_ENABLE
-    );
-
     const entityType = useEntityType();
 
     const draftId = useEditorStore_persistedDraftId();
@@ -23,7 +17,7 @@ function ShardsDisable() {
 
     return (
         <>
-            {forcedToEnable ? <ShardsDisableWarning /> : null}
+            <ShardsDisableWarning />
 
             <Stack spacing={1} sx={{ mb: 2 }}>
                 <Typography variant="formSectionHeader">
