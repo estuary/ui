3c4a4ddbccfb542b8acb023f3539dc906b23efb3
Travis Jenkins <travis@estuary.dev>
2024-09-24 13:41:48 -0400

    cleaning up hook that is not used

--- src/stores/Binding/hooks.ts
+++ src/stores/Binding/hooks.ts
@@ -122,9 +122,6 @@ export const useBinding_collections = () => {
     return useBindingStore(useShallow((state) => state.getCollections()));
 };
 
-export const useBinding_collections_count = () =>
-    useBindingStore(useShallow((state) => state.getCollections().length));
-
 export const useBinding_enabledCollections_count = () =>
     useBindingStore(
         useShallow((state) => state.getEnabledCollections().length)
