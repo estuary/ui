32ca9a8e75f49e1b016929343e2cd46c03a37fa7
Travis Jenkins <travis@estuary.dev>
2024-09-25 16:54:10 -0400

    Setting values (though not working just yet)

--- src/components/shared/Entity/prompts/steps/dataFlowReset/SelectMaterialization/ManualSelectionButton.tsx
+++ src/components/shared/Entity/prompts/steps/dataFlowReset/SelectMaterialization/ManualSelectionButton.tsx
@@ -1,14 +1,38 @@
 import { Button } from '@mui/material';
+import { AddCollectionDialogCTAProps } from 'components/shared/Entity/types';
+import invariableStores from 'context/Zustand/invariableStores';
 import { FormattedMessage } from 'react-intl';
+import { useStore } from 'zustand';
 import { usePreSavePromptStore } from '../../../store/usePreSavePromptStore';
 
-function ManualSelectionButton() {
-    const setBackfillTarget = usePreSavePromptStore((state) => {
-        return state.context.setBackfillTarget;
+function ManualSelectionButton({ toggle }: AddCollectionDialogCTAProps) {
+    const [selected] = useStore(
+        invariableStores['Collections-Selector-Table'],
+        (state) => {
+            return [state.selected];
+        }
+    );
+
+    const updateContext = usePreSavePromptStore((state) => {
+        return state.updateContext;
     });
 
-    const close = () => {
-        setBackfillTarget();
+    const close = async () => {
+        const selectedRow = Array.from(selected).map(([_key, row]) => row)[0];
+        const newCatalog = selectedRow ? selectedRow.catalog_name : null;
+
+        // Only fire updates if a change happened. Since single select table can allow the user
+        //   to deselect a row and then select it again
+        // if (newCatalog && sourceCapture !== newCatalog) {
+        // setBackfillTarget(newCatalog)
+        // }
+
+        updateContext({
+            backfillTarget: {
+                catalog_name: newCatalog,
+            },
+        });
+        toggle(false);
     };
 
     return (
