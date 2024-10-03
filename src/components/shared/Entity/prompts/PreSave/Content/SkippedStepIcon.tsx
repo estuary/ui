a1cd76eb2a4707982793684e26e6b96c4bc0751b
Travis Jenkins <travis@estuary.dev>
2024-10-03 11:26:09 -0400

    Adding a `skipped` icon Sharing icon stuff

---
+++ src/components/shared/Entity/prompts/PreSave/Content/SkippedStepIcon.tsx
@@ -0,0 +1,21 @@
+import { useTheme } from '@mui/material';
+import { MinusCircle } from 'iconoir-react';
+import { pixelSize } from './shared';
+
+// TODO (progress icons)
+// We have a few ways of doing this. We should get these all aligned.
+function SkippedStepIcon() {
+    const theme = useTheme();
+
+    return (
+        <MinusCircle
+            style={{
+                color: theme.palette.grey[500],
+                width: pixelSize,
+                height: pixelSize,
+            }}
+        />
+    );
+}
+
+export default SkippedStepIcon;
