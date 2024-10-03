9abd13f76a0ec409a68d3e7d32402fbe09777c29
Travis Jenkins <travis@estuary.dev>
2024-10-03 15:34:33 -0400

    Cleaning up types

--- src/components/shared/Entity/prompts/types.ts
+++ src/components/shared/Entity/prompts/types.ts
@@ -3,12 +3,6 @@ import { PublicationJobStatus } from 'api/publications';
 import { ErrorDetails } from 'components/shared/Error/types';
 import { ProgressStates } from 'components/tables/RowActions/Shared/types';
 
-export interface PromptStepMessageIds {
-    running: string;
-    skipped: string;
-    success: string;
-}
-
 export interface PromptStepState {
     error: ErrorDetails | null; // Both server and client side error
     progress: ProgressStates; // Stores what the step is currently doing
