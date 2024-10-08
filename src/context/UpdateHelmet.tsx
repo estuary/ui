564bd82d87b6d03de71b28fe19c51e5d005e0a93
Travis Jenkins <travis@estuary.dev>
2024-10-08 18:10:18 -0400

    Adding comment

--- src/context/UpdateHelmet.tsx
+++ src/context/UpdateHelmet.tsx
@@ -12,6 +12,10 @@ const UpdateHelmetContext = React.createContext<{
     updateMetaTitle: () => {},
 });
 
+// Instead of using the component directly we do this to make it easier to wire up
+//  the already created hook useBrowserTitle into Helmet. If we want to add more
+//  properties to contorl we should take the time to work out another way of routes
+//  controlling Helmet. Probably want to "build it into" React Router configuration.
 const UpdateHelmetProvider = ({ children }: BaseComponentProps) => {
     const [title, setTitle] = useState<string | null>(null);
     const [metaDescription, setMetaDescription] = useState<string | undefined>(
