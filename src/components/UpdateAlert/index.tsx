7418746a516d3fefa2091198c9bcd859801e961b
Travis Jenkins <travis@estuary.dev>
2024-10-28 14:40:24 -0400

    PR: releasing hidden first

--- src/components/UpdateAlert/index.tsx
+++ src/components/UpdateAlert/index.tsx
@@ -1,16 +1,11 @@
-import { Box, Divider } from '@mui/material';
 import { getLatestVersionDetails } from 'api/meta';
-import ButtonWithPopper from 'components/shared/buttons/ButtonWithPopper';
-import { NavArrowDown } from 'iconoir-react';
 import { useEffect, useState } from 'react';
-import { useIntl } from 'react-intl';
 import { logRocketEvent } from 'services/shared';
 import { CustomEvents } from 'services/types';
 import useSWR from 'swr';
-import Actions from './Actions';
 
 export function UpdateAlert() {
-    const intl = useIntl();
+    // const intl = useIntl();
 
     const [hasLatest, setHasLatest] = useState<boolean>(true);
 
@@ -40,26 +35,28 @@ export function UpdateAlert() {
         return null;
     }
 
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <ButtonWithPopper
-                popper={<Actions />}
-                buttonProps={{
-                    endIcon: <NavArrowDown style={{ fontSize: 13 }} />,
-                    size: 'small',
-                    sx: {
-                        alignItems: 'center',
-                        p: 1,
-                    },
-                    variant: 'text',
-                }}
-                popperProps={{
-                    placement: 'bottom-end',
-                }}
-            >
-                {intl.formatMessage({ id: 'updateAlert.cta' })}
-            </ButtonWithPopper>
-            <Divider orientation="vertical" flexItem sx={{ ml: 1, mr: 2 }} />
-        </Box>
-    );
+    // Pushing to production with this commented out first to be safe
+    return null;
+    // return (
+    //     <Box sx={{ display: 'flex' }}>
+    //         <ButtonWithPopper
+    //             popper={<Actions />}
+    //             buttonProps={{
+    //                 endIcon: <NavArrowDown style={{ fontSize: 13 }} />,
+    //                 size: 'small',
+    //                 sx: {
+    //                     alignItems: 'center',
+    //                     p: 1,
+    //                 },
+    //                 variant: 'text',
+    //             }}
+    //             popperProps={{
+    //                 placement: 'bottom-end',
+    //             }}
+    //         >
+    //             {intl.formatMessage({ id: 'updateAlert.cta' })}
+    //         </ButtonWithPopper>
+    //         <Divider orientation="vertical" flexItem sx={{ ml: 1, mr: 2 }} />
+    //     </Box>
+    // );
 }
