520dc8f84f3cde1c50bb69df099aae9d54649ac9
Travis Jenkins <travis@estuary.dev>
2024-12-02 17:46:51 -0500

    Adding link to deployment docs

--- src/components/admin/Settings/DataPlanes/index.tsx
+++ src/components/admin/Settings/DataPlanes/index.tsx
@@ -1,22 +1,32 @@
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
+import ExternalLink from 'components/shared/ExternalLink';
 import DataPlanesTable from 'components/tables/DataPlanes';
 import { useIntl } from 'react-intl';
+
+const docsUrl = 'https://docs.estuary.dev/getting-started/deployment-options/';
 
 function DataPlanes() {
     const intl = useIntl();
 
     return (
-        <>
-            <Typography
-                component="div"
-                variant="h6"
-                sx={{ m: 2, alignItems: 'center' }}
-            >
-                {intl.formatMessage({ id: 'admin.dataPlanes.private.header' })}
-            </Typography>
+        <Stack spacing={2}>
+            <Stack direction="row" spacing={1}>
+                <Typography
+                    component="div"
+                    variant="h6"
+                    sx={{ m: 2, alignItems: 'center' }}
+                >
+                    {intl.formatMessage({
+                        id: 'admin.dataPlanes.private.header',
+                    })}
+                </Typography>
+                <ExternalLink link={docsUrl}>
+                    {intl.formatMessage({ id: 'terms.documentation' })}
+                </ExternalLink>
+            </Stack>
 
             <DataPlanesTable />
-        </>
+        </Stack>
     );
 }
 
