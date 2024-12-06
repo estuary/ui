381389cebe2f090e361617f160cc2dd973ae306e
Travis Jenkins <travis@estuary.dev>
2024-12-06 15:10:32 -0500

    removing box as it is no longer needed

--- src/components/admin/Settings/DataPlanes/index.tsx
+++ src/components/admin/Settings/DataPlanes/index.tsx
@@ -1,14 +1,11 @@
-import { Box } from '@mui/material';
 import DataPlanesTable from 'components/tables/DataPlanes';
 import { DataPlaneScopeContextProvider } from 'context/DataPlaneScopeContext';
 
 function DataPlanes() {
     return (
-        <Box>
-            <DataPlaneScopeContextProvider>
-                <DataPlanesTable />
-            </DataPlaneScopeContextProvider>
-        </Box>
+        <DataPlaneScopeContextProvider>
+            <DataPlanesTable />
+        </DataPlaneScopeContextProvider>
     );
 }
 
