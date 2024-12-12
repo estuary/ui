5b203c3e62c4e369334a4a8ab06a3b487a7f0db6
Travis Jenkins <travis@estuary.dev>
2024-12-12 13:07:01 -0500

    removing testing loading removing reset state as this can mess things up here

--- src/components/editor/Bindings/CollectionSpecViewer.tsx
+++ src/components/editor/Bindings/CollectionSpecViewer.tsx
@@ -1,9 +1,6 @@
-import { LinearProgress } from '@mui/material';
 import {
     useBindingsEditorStore_collectionData,
-    useBindingsEditorStore_inferSchemaResponseDoneProcessing,
     useBindingsEditorStore_populateInferSchemaResponse,
-    useBindingsEditorStore_resetState,
 } from 'components/editor/Bindings/Store/hooks';
 import ReadOnly from 'components/schema/KeyAutoComplete/ReadOnly';
 import PropertiesViewer from 'components/schema/PropertiesViewer';
@@ -19,33 +16,18 @@ function CollectionSpecViewer() {
 
     const currentCollection = useBinding_currentCollection();
 
-    const resetState = useBindingsEditorStore_resetState();
-
     const populateInferSchemaResponse =
         useBindingsEditorStore_populateInferSchemaResponse();
-
-    const inferSchemaResponseDoneProcessing =
-        useBindingsEditorStore_inferSchemaResponseDoneProcessing();
 
     useEffect(() => {
         if (collectionData && currentCollection) {
             populateInferSchemaResponse(collectionData.spec, currentCollection);
         }
-
-        return () => {
-            resetState();
-        };
-    }, [
-        collectionData,
-        currentCollection,
-        populateInferSchemaResponse,
-        resetState,
-    ]);
+    }, [collectionData, currentCollection, populateInferSchemaResponse]);
 
     if (currentBindingUUID && collectionData) {
         return (
             <>
-                {inferSchemaResponseDoneProcessing ? null : <LinearProgress />}
                 <ReadOnly value={collectionData.spec.key} />
                 <PropertiesViewer disabled />
             </>
