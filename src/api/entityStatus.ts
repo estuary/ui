b40b47b64e2b348c1ee684840b36834b2d643dee
Kiahna Tucker <tucker.kiahna@gmail.com>
2024-12-19 09:08:09 -0500

    Update format of /status request URL

--- src/api/entityStatus.ts
+++ src/api/entityStatus.ts
@@ -4,8 +4,9 @@ import { getEntityStatusSettings } from 'utils/env-utils';
 
 const { entityStatusBaseEndpoint } = getEntityStatusSettings();
 
+// Local API documentation can be found here: http://localhost:8675/api/v1/docs
 export const getEntityStatus = async (
     accessToken: string,
     catalogName: string
 ): Promise<EntityStatusResponse> =>
-    client(`${entityStatusBaseEndpoint}${catalogName}`, {}, accessToken);
+    client(`${entityStatusBaseEndpoint}?name=${catalogName}`, {}, accessToken);
