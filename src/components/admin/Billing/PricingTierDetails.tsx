70e1e717263e2837da019f992f96a6263380f038
Travis Jenkins <travis@estuary.dev>
2025-01-22 10:33:26 -0500

    Moving stuff into a hook to make it a bit simpler and sharable

--- src/components/admin/Billing/PricingTierDetails.tsx
+++ src/components/admin/Billing/PricingTierDetails.tsx
@@ -1,30 +1,18 @@
 import { Skeleton, Typography } from '@mui/material';
-import { useTenantBillingDetails } from 'context/fetcher/TenantBillingDetails';
+import { useTenantUsesExternalPayment } from 'context/fetcher/TenantBillingDetails';
 import { useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useBillingStore } from 'stores/Billing/Store';
 import { useTenantStore } from 'stores/Tenant/Store';
 
 function PricingTierDetails() {
-    const { tenantBillingDetails } = useTenantBillingDetails();
-
     const selectedTenant = useTenantStore((state) => state.selectedTenant);
+    const externalPaymentMethod = useTenantUsesExternalPayment(selectedTenant);
 
     const billingStoreHydrated = useBillingStore((state) => state.hydrated);
     const paymentMethodExists = useBillingStore(
         (state) => state.paymentMethodExists
     );
-
-    const externalPaymentMethod = useMemo(() => {
-        return !tenantBillingDetails
-            ? false
-            : tenantBillingDetails.some((tenantBillingDetail) => {
-                  return (
-                      tenantBillingDetail.tenant === selectedTenant &&
-                      tenantBillingDetail.payment_provider === 'external'
-                  );
-              });
-    }, [selectedTenant, tenantBillingDetails]);
 
     const messageId = useMemo(
         () =>
