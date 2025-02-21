19d0b4c03778ccac013505bd54408377866e2539
Kiahna Tucker <tucker.kiahna@gmail.com>
2025-02-21 13:13:26 -0500

    Use warning as default alert severity

--- src/components/navigation/shared.ts
+++ src/components/navigation/shared.ts
@@ -2,10 +2,8 @@ import { PaletteMode } from '@mui/material';
 import {
     errorColoredOutline_hovered,
     infoColoredOutline_hovered,
-    primaryColoredOutline_hovered,
     semiTransparentBackground_error,
     semiTransparentBackground_info,
-    semiTransparentBackground_primary,
     semiTransparentBackground_success,
     semiTransparentBackground_warning,
     successColoredOutline_hovered,
@@ -21,14 +19,12 @@ export const getSemanticBackgroundColor = (
     switch (severity) {
         case 'success':
             return semiTransparentBackground_success[colorMode];
-        case 'warning':
-            return semiTransparentBackground_warning[colorMode];
         case 'error':
             return semiTransparentBackground_error[colorMode];
         case 'info':
             return semiTransparentBackground_info[colorMode];
         default:
-            return semiTransparentBackground_primary[colorMode];
+            return semiTransparentBackground_warning[colorMode];
     }
 };
 
@@ -39,14 +35,12 @@ export const getSemanticBorder = (
     switch (severity) {
         case 'success':
             return successColoredOutline_hovered[colorMode];
-        case 'warning':
-            return warningColoredOutline_hovered[colorMode];
         case 'error':
             return errorColoredOutline_hovered[colorMode];
         case 'info':
             return infoColoredOutline_hovered[colorMode];
         default:
-            return primaryColoredOutline_hovered[colorMode];
+            return warningColoredOutline_hovered[colorMode];
     }
 };
 
