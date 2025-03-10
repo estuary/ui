b48bee64f1e5d45bafa8f0624d41ff5fd0c18b75
Travis Jenkins <travis@estuary.dev>
2025-03-10 13:17:03 -0400

    PR: marking todo

--- src/components/shared/types.ts
+++ src/components/shared/types.ts
@@ -3,6 +3,8 @@ import { AlertColor, SxProps } from '@mui/material';
 import { ReactNode } from 'react';
 import { BaseComponentProps } from 'types';
 
+// TODO (AlertBox) we defaulted short to false at the start. That was a mistake
+//  so we need to get `short` to default to `true` soon.
 export interface AlertBoxProps extends BaseComponentProps {
     severity: AlertColor;
     short: boolean; // Forcing this on... but do not want to change all calls right now
