840632485dddd7615cde83e0eda59a2d672b8597
Travis Jenkins <travis@estuary.dev>
2025-03-20 12:17:45 -0400

    new files

---
+++ src/directives/Onboard/Survey/OriginOption.tsx
@@ -0,0 +1,36 @@
+import { Chip, FormControlLabel, Radio } from '@mui/material';
+import { chipOutlinedStyling } from 'context/Theme';
+import { useOnboardingStore_surveyResponse } from 'directives/Onboard/Store/hooks';
+import { OriginOptionProps } from './types';
+
+function OriginOption({ option }: OriginOptionProps) {
+    const surveyResponse = useOnboardingStore_surveyResponse();
+    const currentOption = surveyResponse.origin === option;
+
+    const labelId = `${option} label`;
+    const inputId = `${option} input`;
+
+    return (
+        <FormControlLabel
+            disableTypography
+            value={option}
+            control={<Radio id={inputId} size="small" />}
+            htmlFor={inputId}
+            label={
+                <Chip
+                    component="span"
+                    color={currentOption ? 'primary' : undefined}
+                    id={labelId}
+                    variant="outlined"
+                    label={option}
+                    sx={{
+                        ...chipOutlinedStyling,
+                        p: 0,
+                    }}
+                />
+            }
+        />
+    );
+}
+
+export default OriginOption;
