d7b68609a090118be741e82f0ace86f1bd0898ef
Kiahna Tucker <tucker.kiahna@gmail.com>
2025-03-26 09:43:08 -0400

    Prevent custom toggle button group prop from being forwarded to component

--- src/components/shared/OutlinedToggleButtonGroup.tsx
+++ src/components/shared/OutlinedToggleButtonGroup.tsx
@@ -4,26 +4,28 @@ import {
     ToggleButtonGroupProps,
 } from '@mui/material';
 
-const OutlinedToggleButtonGroup = styled(ToggleButtonGroup)<
-    ToggleButtonGroupProps & { buttonSelector?: string }
->(({ buttonSelector }) => ({
-    [`${buttonSelector ?? '& .MuiToggleButton-root'}`]: {
-        '&:not(:first-of-type), &:not(:last-of-type)': {
-            borderRadius: 0,
+const OutlinedToggleButtonGroup = styled(ToggleButtonGroup, {
+    shouldForwardProp: (props) => props !== 'buttonSelector',
+})<ToggleButtonGroupProps & { buttonSelector?: string }>(
+    ({ buttonSelector }) => ({
+        [`${buttonSelector ?? '& .MuiToggleButton-root'}`]: {
+            '&:not(:first-of-type), &:not(:last-of-type)': {
+                borderRadius: 0,
+            },
+            '&:first-of-type': {
+                borderTopLeftRadius: 4,
+                borderBottomLeftRadius: 4,
+                borderTopRightRadius: 0,
+                borderBottomRightRadius: 0,
+            },
+            '&:last-of-type': {
+                borderTopLeftRadius: 0,
+                borderBottomLeftRadius: 0,
+                borderTopRightRadius: 4,
+                borderBottomRightRadius: 4,
+            },
         },
-        '&:first-of-type': {
-            borderTopLeftRadius: 4,
-            borderBottomLeftRadius: 4,
-            borderTopRightRadius: 0,
-            borderBottomRightRadius: 0,
-        },
-        '&:last-of-type': {
-            borderTopLeftRadius: 0,
-            borderBottomLeftRadius: 0,
-            borderTopRightRadius: 4,
-            borderBottomRightRadius: 4,
-        },
-    },
-}));
+    })
+);
 
 export default OutlinedToggleButtonGroup;
