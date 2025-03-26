95316551516421976b297403b03428143962d7bb
Kiahna Tucker <tucker.kiahna@gmail.com>
2025-03-26 14:11:20 -0400

    Hide entity status section for non-support users

--- src/components/shared/Entity/Details/Logs/Status/index.tsx
+++ src/components/shared/Entity/Details/Logs/Status/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Stack, Typography } from '@mui/material';
 import UnderDev from 'components/shared/UnderDev';
+import { useUserInfoSummaryStore } from 'context/UserInfoSummary/useUserInfoSummaryStore';
 import useGlobalSearchParams, {
     GlobalSearchParams,
 } from 'hooks/searchParams/useGlobalSearchParams';
@@ -16,13 +17,13 @@ export default function Status() {
 
     const intl = useIntl();
 
-    // const hasSupportRole = useUserInfoSummaryStore(
-    //     (state) => state.hasSupportAccess
-    // );
+    const hasSupportRole = useUserInfoSummaryStore(
+        (state) => state.hasSupportAccess
+    );
 
-    // if (!hasSupportRole) {
-    //     return null;
-    // }
+    if (!hasSupportRole) {
+        return null;
+    }
 
     return (
         <EntityStatusHydrator catalogName={catalogName}>
