ccb7ca91639dfe34713364213d400786b26ee82c
Travis Jenkins <travis@estuary.dev>
2025-03-28 11:16:03 -0400

    PR: marking todos

--- src/api/shared.ts
+++ src/api/shared.ts
@@ -1,3 +1,6 @@
+// TODO (endpoints) - weed need to start moving these lists out of the `services/supabase`
+//  file and into here. Do not really like the name `shared` but it follows the standard.
+
 export const OAUTH_OPERATIONS = {
     AUTH_URL: 'auth-url',
     ACCESS_TOKEN: 'access-token',
