46d1abedcaa76f7a4ff59bf9db8700b5b38dcd69
Travis Jenkins <travis@estuary.dev>
2025-03-28 11:44:17 -0400

    PR: typo

--- src/api/shared.ts
+++ src/api/shared.ts
@@ -1,4 +1,4 @@
-// TODO (endpoints) - weed need to start moving these lists out of the `services/supabase`
+// TODO (endpoints) - we need to start moving these lists out of the `services/supabase`
 //  file and into here. Do not really like the name `shared` but it follows the standard.
 
 export const OAUTH_OPERATIONS = {
