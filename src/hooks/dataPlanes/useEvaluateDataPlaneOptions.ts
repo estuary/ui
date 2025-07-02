448e065d2531cc5917c58d9cd2db754d29a827f4
Kiahna Tucker <tucker.kiahna@gmail.com>
2025-07-02 09:16:58 -0400

    Ensure the existing data-plane is always fetched during hydration

--- src/hooks/dataPlanes/useEvaluateDataPlaneOptions.ts
+++ src/hooks/dataPlanes/useEvaluateDataPlaneOptions.ts
@@ -1,7 +1,6 @@
 import { useCallback } from 'react';
 
 import { getDataPlaneOptions } from 'src/api/dataPlanes';
-import { useEntityWorkflow_Editing } from 'src/context/Workflow';
 import { logRocketEvent } from 'src/services/shared';
 import { CustomEvents } from 'src/services/types';
 import { useDetailsFormStore } from 'src/stores/DetailsForm/Store';
@@ -13,13 +12,11 @@ import {
 } from 'src/utils/dataPlane-utils';
 
 export const useEvaluateDataPlaneOptions = () => {
-    const isEdit = useEntityWorkflow_Editing();
-
-    const storageMappings = useEntitiesStore((state) => state.storageMappings);
-
     const setDataPlaneOptions = useDetailsFormStore(
         (state) => state.setDataPlaneOptions
     );
+
+    const storageMappings = useEntitiesStore((state) => state.storageMappings);
 
     const setStorageMappingPrefix = useWorkflowStore(
         (state) => state.setStorageMappingPrefix
@@ -34,13 +31,23 @@ export const useEvaluateDataPlaneOptions = () => {
                 reactorAddress: string | null;
             }
         ) => {
+            // Get required data-plane information from the matched storage mapping.
             const { dataPlaneNames, storageMappingPrefix } = getDataPlaneInfo(
                 storageMappings,
                 catalogName
             );
 
-            const { data: dataPlanes, error } =
-                await getDataPlaneOptions(dataPlaneNames);
+            // Add the existing data-plane name to the array of data-plane names of the
+            // matched storage mapping in the event it is not there. This data-plane should
+            // be treated as the default in edit workflows so it must be the first element
+            // in the array of data-plane names.
+            const evaluatedDataPlaneNames = existingDataPlane?.name
+                ? [existingDataPlane.name].concat(dataPlaneNames)
+                : dataPlaneNames;
+
+            const { data: dataPlanes, error } = await getDataPlaneOptions(
+                evaluatedDataPlaneNames
+            );
 
             if (!dataPlanes || dataPlanes.length === 0 || error) {
                 logRocketEvent(CustomEvents.DATA_PLANE_SELECTOR, {
@@ -59,7 +66,7 @@ export const useEvaluateDataPlaneOptions = () => {
                               gcp_service_account_email: null,
                               aws_iam_user_arn: null,
                           },
-                          isEdit ? undefined : (existingDataPlane.name ?? '')
+                          existingDataPlane.name ?? ''
                       )
                     : null;
 
@@ -70,11 +77,40 @@ export const useEvaluateDataPlaneOptions = () => {
                 return fallbackOptions;
             }
 
+            // If the array of data-planes does not contain an element with the same name
+            // as the existing data-plane, stub the BaseDataPlaneQuery response corresponding
+            // to the existing data-plane so it can appear as a data-plane option. This is
+            // particularly important for edit workflows.
+            const evaluatedDataPlanes = dataPlanes;
+            const existingDataPlaneFetched = Boolean(
+                existingDataPlane?.name &&
+                    dataPlanes.some(
+                        ({ data_plane_name }) =>
+                            data_plane_name === existingDataPlane.name
+                    )
+            );
+
+            if (existingDataPlane && !existingDataPlaneFetched) {
+                logRocketEvent(CustomEvents.DATA_PLANE_SELECTOR, {
+                    noOptionsFound: false,
+                    fallbackExists: Boolean(existingDataPlane),
+                });
+
+                evaluatedDataPlanes.concat({
+                    data_plane_name: existingDataPlane.name ?? '',
+                    id: existingDataPlane.id,
+                    reactor_address: existingDataPlane.reactorAddress ?? '',
+                    cidr_blocks: null,
+                    gcp_service_account_email: null,
+                    aws_iam_user_arn: null,
+                });
+            }
+
             const options = dataPlanes
                 ? dataPlanes.map((dataPlane) =>
                       generateDataPlaneOption(
                           dataPlane,
-                          isEdit ? undefined : dataPlaneNames[0]
+                          evaluatedDataPlaneNames[0]
                       )
                   )
                 : [];
@@ -84,6 +120,6 @@ export const useEvaluateDataPlaneOptions = () => {
 
             return options;
         },
-        [storageMappings, setDataPlaneOptions, setStorageMappingPrefix, isEdit]
+        [storageMappings, setDataPlaneOptions, setStorageMappingPrefix]
     );
 };
