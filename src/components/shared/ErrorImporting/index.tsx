51ad71f64e31ed3f7fe0c61d993513b2d14c4a78
Travis Jenkins <travis@estuary.dev>
2025-07-14 15:26:47 -0400

    Moving code to a more shared location as we'll need it

--- src/components/shared/ErrorImporting/index.tsx
+++ src/components/shared/ErrorImporting/index.tsx
@@ -1,19 +1,21 @@
 import type { FallbackProps } from 'react-error-boundary';
+import type { LazyLoadFailureStates } from 'src/components/shared/ErrorImporting/types';
 
 import { useEffect, useState } from 'react';
 
 import FullPageSpinner from 'src/components/fullPage/Spinner';
 import Error from 'src/components/shared/Error';
 import {
-    getWithExpiry,
-    setWithExpiry,
-} from 'src/components/shared/ErrorImporting/shared';
-import {
     failedToLazyLoad,
     logRocketConsole,
     logRocketEvent,
 } from 'src/services/shared';
 import { CustomEvents } from 'src/services/types';
+import {
+    getWithExpiry,
+    LocalStorageKeys,
+    setWithExpiry,
+} from 'src/utils/localStorage-utils';
 
 export function ErrorImporting({ error }: FallbackProps) {
     const [stopTrying, setStopTrying] = useState(false);
@@ -29,16 +31,26 @@ export function ErrorImporting({ error }: FallbackProps) {
 
             // If we already tried and are hitting the error again
             //  just show the error and clear the local storage
-            if (getWithExpiry() === 'reloaded') {
+            if (
+                getWithExpiry<LazyLoadFailureStates>(
+                    LocalStorageKeys.LAZY_LOAD_FAILED_KEY
+                ) === 'reloaded'
+            ) {
                 logRocketEvent(CustomEvents.LAZY_LOADING, 'stopped');
                 setStopTrying(true);
-                setWithExpiry(null);
+                setWithExpiry<LazyLoadFailureStates>(
+                    LocalStorageKeys.LAZY_LOAD_FAILED_KEY,
+                    null
+                );
                 return;
             }
 
             // Update LR and the token
             logRocketEvent(CustomEvents.LAZY_LOADING, 'reloaded');
-            setWithExpiry('reloaded');
+            setWithExpiry<LazyLoadFailureStates>(
+                LocalStorageKeys.LAZY_LOAD_FAILED_KEY,
+                'reloaded'
+            );
 
             // Wait just a bit before reloading incase there was a network blip
             setTimeout(() => window.location.reload(), 250);
--- src/utils/localStorage-utils.ts
+++ src/utils/localStorage-utils.ts
@@ -3,9 +3,46 @@ export enum LocalStorageKeys {
     DATAPLANE_CHOICE = 'estuary.dataplane-choice',
     CIDR_BLOCK_CHOICE = 'estuary.cidr-block-choice',
     DASHBOARD_WELCOME = 'estuary.dashboard-welcome',
+    LAZY_LOAD_FAILED_KEY = 'chunk_failed',
     MARKETPLACE_VERIFY = 'estuary.marketplace-verify',
     NAVIGATION_SETTINGS = 'estuary.navigation-settings',
     SIDE_PANEL_DOCS = 'estuary.side-panel-docs',
     TABLE_SETTINGS = 'estuary.table-settings',
     // ENABLE_DATA_FLOW_RESET = 'estuary.enable-data-flow-reset', // Went Live
 }
+
+export const setWithExpiry = <T = unknown>(
+    key: LocalStorageKeys,
+    value: any | T
+) => {
+    if (value === null) {
+        localStorage.removeItem(key);
+    } else {
+        localStorage.setItem(
+            key,
+            JSON.stringify({
+                value,
+                expiry: new Date().getTime() + 5000,
+            })
+        );
+    }
+};
+
+export const getWithExpiry = <T = unknown>(key: LocalStorageKeys): null | T => {
+    const itemString = window.localStorage.getItem(key);
+    const item = itemString ? JSON.parse(itemString) : null;
+
+    // Either there is no setting or we couldn't parse it. Either way we should try reloading again
+    if (item === null) {
+        return null;
+    }
+
+    // We have waited long enough to allow trying again so clearing out the key
+    if (new Date().getTime() > item.expiry) {
+        setWithExpiry(key, null);
+        return null;
+    }
+
+    // Return value so we do not try reloading again
+    return item.value;
+};
