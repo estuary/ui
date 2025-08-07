44463de5942a8926a552e7d73c495369ca2fd88b
Travis Jenkins <travis@estuary.dev>
2025-08-07 08:25:43 -0400

    Removing unused arguments

--- src/services/gitpod.ts
+++ src/services/gitpod.ts
@@ -1,8 +1,6 @@
 import { Buffer } from 'buffer';
 import type { RefreshTokenData } from 'src/types';
 
-import { isArray } from 'lodash';
-
 const GIT_POD_URL = `https://gitpod.io/#`;
 const GIT_REPO = 'https://github.com/estuary/flow-gitpod-base';
 const GIT_REPO_PATH = `/${GIT_REPO}`; // Must start with a slash to separate it from the variables
@@ -13,29 +11,21 @@ const GIT_REPO_PATH = `/${GIT_REPO}`; // Must start with a slash to separate it 
 export const generateGitPodURL = (
     draftId: string,
     token: RefreshTokenData,
-    derivationLanguage: string,
-    sourceCollections: Set<string> | string[],
     computedEntityName: string
 ) => {
-    const sourceCollectionCount = isArray(sourceCollections)
-        ? sourceCollections.length
-        : sourceCollections.size;
-
     // https://github.com/estuary/flow-gitpod-base/blob/main/init.sh
     // This object is consumed by GitPod init.sh and has to stay in sync
-    const settingsPart = `FLOW_SETTINGS=${encodeURIComponent(
+    const settingsPart = `F_S=${encodeURIComponent(
         Buffer.from(
             JSON.stringify({
                 id: draftId,
                 rt: token,
-
-                // TODO - (GitPod) These all seem unused in the script but need to double check
-                dl: derivationLanguage,
-                tt: sourceCollectionCount > 1 ? 'multi' : 'single',
-                cn: computedEntityName,
             })
         ).toString('base64')
     )}`;
 
-    return `${GIT_POD_URL}${settingsPart}${GIT_REPO_PATH}`;
+    // Adding a bit of unique value here - this is not consumed by the workspace
+    const uniqueProps = `ENTITY=${encodeURIComponent(computedEntityName)}`;
+
+    return `${GIT_POD_URL}${uniqueProps},${settingsPart}${GIT_REPO_PATH}`;
 };
