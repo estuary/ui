5c66e48e924256d022b7455bf369921a1fe2e6af
Travis Jenkins <travis@estuary.dev>
2025-09-05 15:59:09 -0400

    Removing the under dev marker

--- src/components/shared/Entity/Details/History/index.tsx
+++ src/components/shared/Entity/Details/History/index.tsx
@@ -4,13 +4,11 @@ import ListAndDetails from 'src/components/editor/ListAndDetails';
 import DiffViewer from 'src/components/shared/Entity/Details/History/DiffViewer';
 import PublicationList from 'src/components/shared/Entity/Details/History/PublicationList';
 import { HEIGHT } from 'src/components/shared/Entity/Details/History/shared';
-import UnderDev from 'src/components/shared/UnderDev';
 import { getEditorTotalHeight } from 'src/utils/editor-utils';
 
 function History() {
     return (
         <Box>
-            <UnderDev />
             <ListAndDetails
                 displayBorder
                 height={getEditorTotalHeight(HEIGHT)}
