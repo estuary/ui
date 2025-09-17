693cc5c40fede0095dd90d9e4e3b704c99ce4ac4
Travis Jenkins <travis@estuary.dev>
2025-09-17 10:18:52 -0400

    Polling for alert count

--- src/components/shared/AlertsAreActiveBadge/index.tsx
+++ src/components/shared/AlertsAreActiveBadge/index.tsx
@@ -1,6 +1,8 @@
 import type { AlertsAreActiveBadgeProps } from 'src/components/shared/AlertsAreActiveBadge/types';
 import type { AlertsVariables, LatestAlertQueryResponse } from 'src/types/gql';
 
+import { useEffect } from 'react';
+
 import { Badge, badgeClasses } from '@mui/material';
 
 import { gql, useQuery } from 'urql';
@@ -15,14 +17,25 @@ const LatestAlertQuery = gql<LatestAlertQueryResponse, AlertsVariables>`
     }
 `;
 
+const POLLING_INTERVAL = 10000;
+
 function AlertsAreActiveBadge({ children, prefix }: AlertsAreActiveBadgeProps) {
-    const [{ fetching, data, error }] = useQuery({
+    const [{ fetching, data, error }, reexecuteQuery] = useQuery({
         query: LatestAlertQuery,
         variables: { prefix },
         pause: !prefix,
     });
 
-    console.log('data', data);
+    useEffect(() => {
+        if (fetching) return;
+
+        // Set up to refetch in one second, if the query is idle
+        const timerId = setTimeout(() => {
+            reexecuteQuery({ requestPolicy: 'network-only' });
+        }, POLLING_INTERVAL);
+
+        return () => clearTimeout(timerId);
+    }, [fetching, reexecuteQuery]);
 
     const activeAlertCount = data?.alerts.edges.length ?? 0;
     const hasActiveAlerts = activeAlertCount > 0;
@@ -33,7 +46,6 @@ function AlertsAreActiveBadge({ children, prefix }: AlertsAreActiveBadgeProps) {
             color={error || hasActiveAlerts ? 'warning' : 'success'}
             invisible={fetching}
             overlap="rectangular"
-            // showZero // TODO (Alert History) - decide if we want to show zero or not
             sx={{
                 [`& .${badgeClasses.badge}`]: {
                     right: -6,
