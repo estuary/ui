f7c715ce69e3f038b89734310a3053c493a1897a
Travis Jenkins <travis@estuary.dev>
2025-09-18 13:38:24 -0400

    Removing translate component

--- src/components/shared/ErrorDialog/OauthWindowOpenerMissing.tsx
+++ src/components/shared/ErrorDialog/OauthWindowOpenerMissing.tsx
@@ -1,37 +1,45 @@
 import { Button, Stack, Typography } from '@mui/material';
 
-import { FormattedMessage } from 'react-intl';
+import { useIntl } from 'react-intl';
 
 import ErrorDialog from 'src/components/shared/ErrorDialog/index';
 
 const ARIA_NAME = 'OauthWindowOpenerMissing';
 
 function OauthWindowOpenerMissing() {
+    const intl = useIntl();
+
     return (
         <ErrorDialog
             body={
                 <Stack spacing={2}>
                     <Typography>
-                        <FormattedMessage id="oauth.windowOpener.error.message1" />
+                        {intl.formatMessage({
+                            id: 'oauth.windowOpener.error.message1',
+                        })}
                     </Typography>
                     <Typography>
-                        <FormattedMessage id="oauth.windowOpener.error.message2" />
+                        {intl.formatMessage({
+                            id: 'oauth.windowOpener.error.message2',
+                        })}
                     </Typography>
                 </Stack>
             }
-            bodyTitle={<FormattedMessage id="oauth.windowOpener.error.title" />}
+            bodyTitle={intl.formatMessage({
+                id: 'oauth.windowOpener.error.title',
+            })}
             cta={
                 <Button
                     onClick={() => {
                         window.close();
                     }}
                 >
-                    <FormattedMessage id="cta.close" />
+                    {intl.formatMessage({ id: 'cta.close' })}
                 </Button>
             }
-            dialogTitle={
-                <FormattedMessage id="oauth.windowOpener.error.dialog.title" />
-            }
+            dialogTitle={intl.formatMessage({
+                id: 'oauth.windowOpener.error.dialog.title',
+            })}
             name={ARIA_NAME}
         />
     );
