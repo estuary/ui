ffa1b115a5c655b6ce778ae6a36e60970d8730ec
Kiahna Tucker <tucker.kiahna@gmail.com>
2025-09-24 14:10:28 -0400

    Placeholder: pivoting

--- src/stores/Binding/slices/FieldSelection.ts
+++ src/stores/Binding/slices/FieldSelection.ts
@@ -80,6 +80,7 @@ export interface StoreWithFieldSelection {
         bindingUUIDs: string[],
         serverUpdateFailed?: boolean
     ) => void;
+    setSingleGroupBy: (bindingUUID: string, targetKeys: string[]) => void;
 
     selectionAlgorithm: SelectionAlgorithm | null;
     setSelectionAlgorithm: (
@@ -223,6 +224,29 @@ export const getStoreWithFieldSelectionSettings = (
         );
     },
 
+    setMultiSelection: (bindingUUID, targetFields, targetMode) => {
+        set(
+            produce((state: BindingState) => {
+                const evaluatedStatus = getHydrationStatus(
+                    state.selections[bindingUUID].status
+                );
+
+                state.selections[bindingUUID].hydrating =
+                    isHydrating(evaluatedStatus);
+                state.selections[bindingUUID].status = evaluatedStatus;
+
+                Object.values(state.selections[bindingUUID].value)
+                    .filter(({ field }) => targetFields.includes(field))
+                    .forEach(({ field }) => {
+                        state.selections[bindingUUID].value[field].mode =
+                            targetMode;
+                    });
+            }),
+            false,
+            'Multiple Field Selections Set'
+        );
+    },
+
     setRecommendFields: (bindingUUID, value) => {
         set(
             produce((state: BindingState) => {
@@ -253,26 +277,19 @@ export const getStoreWithFieldSelectionSettings = (
         );
     },
 
-    setMultiSelection: (bindingUUID, targetFields, targetMode) => {
+    setSingleGroupBy: (bindingUUID, targetKeys) => {
         set(
             produce((state: BindingState) => {
-                const evaluatedStatus = getHydrationStatus(
-                    state.selections[bindingUUID].status
+                Object.keys(state.selections[bindingUUID].value).forEach(
+                    (field) => {
+                        state.selections[bindingUUID].value[
+                            field
+                        ].groupBy.explicit = targetKeys.includes(field);
+                    }
                 );
-
-                state.selections[bindingUUID].hydrating =
-                    isHydrating(evaluatedStatus);
-                state.selections[bindingUUID].status = evaluatedStatus;
-
-                Object.values(state.selections[bindingUUID].value)
-                    .filter(({ field }) => targetFields.includes(field))
-                    .forEach(({ field }) => {
-                        state.selections[bindingUUID].value[field].mode =
-                            targetMode;
-                    });
             }),
             false,
-            'Multiple Field Selections Set'
+            'Single GroupBy Set'
         );
     },
 
