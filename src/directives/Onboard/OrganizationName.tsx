3d3a975efb8db66682fb6ba990ac60c3c5179855
Travis Jenkins <travis@estuary.dev>
2025-10-20 10:09:09 -0400

    Guide users away from using `test` in Organization Names (#1782)
    * Starting to add in
    
    * Fixing some stuff in the store
    Cleaning up the store
    Breaking out the warning/error stuff
    
    * replacing translation
    
    * Adding a warning of entering nested values
    When the name is not required allowing prefix to take more space
    
    * Tweaking content
    
    * checking lower case to make case not matter
    
    * content tweaks based on feedback
    
    * PR: comment
    
    * Code clean up, moving types, etc.
    
    * replace ternary
    
    * color default to `marktext` so needs unset

--- src/directives/Onboard/OrganizationName.tsx
+++ src/directives/Onboard/OrganizationName.tsx
@@ -1,6 +1,8 @@
+import type { OrganizationNameFieldProps } from 'src/directives/Onboard/types';
+
 import { FormControl, FormLabel, TextField } from '@mui/material';
 
-import { FormattedMessage, useIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 
 import {
     useOnboardingStore_nameInvalid,
@@ -9,11 +11,7 @@ import {
     useOnboardingStore_setRequestedTenant,
 } from 'src/directives/Onboard/Store/hooks';
 
-interface Props {
-    forceError?: boolean;
-}
-
-function OrganizationNameField({ forceError }: Props) {
+function OrganizationNameField({ forceError }: OrganizationNameFieldProps) {
     const intl = useIntl();
 
     // Onboarding Store
@@ -33,7 +31,7 @@ function OrganizationNameField({ forceError }: Props) {
     return (
         <FormControl error={showError}>
             <FormLabel id="origin" required sx={{ mb: 1, fontSize: 20 }}>
-                <FormattedMessage id="tenant.input.label" />
+                {intl.formatMessage({ id: 'tenant.input.label' })}
             </FormLabel>
 
             <TextField
@@ -47,7 +45,7 @@ function OrganizationNameField({ forceError }: Props) {
                             : 'tenant.expectations',
                 })}
                 id="requestedTenant"
-                label={<FormattedMessage id="common.tenant.creationForm" />}
+                label={intl.formatMessage({ id: 'common.tenant.creationForm' })}
                 onChange={(event) => handlers.update(event.target.value)}
                 placeholder={intl.formatMessage({
                     id: 'tenant.input.placeholder',
