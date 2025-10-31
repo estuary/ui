73d7f991da61a206eb06097c1aaa69585586d1b9
Travis Jenkins <travis@estuary.dev>
2025-10-31 15:03:41 -0400

    Some minor cleaning up

--- src/components/fieldSelection/RefreshStatus.tsx
+++ src/components/fieldSelection/RefreshStatus.tsx
@@ -1,6 +1,6 @@
 import { Collapse } from '@mui/material';
 
-import { FormattedMessage } from 'react-intl';
+import { useIntl } from 'react-intl';
 
 import { useEditorStore_id } from 'src/components/editor/Store/hooks';
 import AlertBox from 'src/components/shared/AlertBox';
@@ -10,12 +10,13 @@ interface Props {
 }
 
 function RefreshStatus({ show }: Props) {
+    const intl = useIntl();
     const draftId = useEditorStore_id();
 
     return (
         <Collapse in={Boolean(!draftId || show)} unmountOnExit>
             <AlertBox short severity="info">
-                <FormattedMessage id="fieldSelection.refresh.alert" />
+                {intl.formatMessage({ id: 'fieldSelection.refresh.alert' })}
             </AlertBox>
         </Collapse>
     );
