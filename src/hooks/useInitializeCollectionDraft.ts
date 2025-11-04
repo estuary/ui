edc9f38f47091ab8075c9f14ebe3a8cbad96b5e0
Travis Jenkins <travis@estuary.dev>
2025-11-04 13:07:51 -0500

    missing hook `def`

--- src/hooks/useInitializeCollectionDraft.ts
+++ src/hooks/useInitializeCollectionDraft.ts
@@ -263,6 +263,7 @@ function useInitializeCollectionDraft() {
             setDraftId,
             setLocalDraftId,
             setPersistedDraftId,
+            upsertCollection,
         ]
     );
 
