a39cec9cdc3503bb3cb109e5687f6ceeb6cde76e
Travis Jenkins <travis@estuary.dev>
2025-11-17 09:26:34 -0500

    Refresh `token` for `gql`  (#1812)
    * This is deprecated and throwing errors in chrome
    
    * Properly refreshing token from GQL
    
    * Removing testing code
    
    * PR: commenting
    
    * PR: reducing early to 1 minute as these should only last an hour in prod

--- src/context/URQL.tsx
+++ src/context/URQL.tsx
@@ -2,24 +2,31 @@ import type { BaseComponentProps } from 'src/types';
 
 import { useMemo } from 'react';
 
+import { useShallow } from 'zustand/react/shallow';
+
 import { authExchange } from '@urql/exchange-auth';
 import { cacheExchange } from '@urql/exchange-graphcache';
 import { requestPolicyExchange } from '@urql/exchange-request-policy';
 import { DateTime } from 'luxon';
 import { Client, fetchExchange, Provider } from 'urql';
 
+import { supabaseClient } from 'src/context/GlobalProviders';
 import { useUserStore } from 'src/context/User/useUserContextStore';
 import useDataFetchErrorHandling from 'src/hooks/useDataFetchErrorHandling';
+import { logRocketEvent } from 'src/services/shared';
 import { getAuthHeader } from 'src/utils/misc-utils';
 
 function UrqlConfigProvider({ children }: BaseComponentProps) {
     const { checkIfAuthInvalid, forceUserToSignOut } =
         useDataFetchErrorHandling();
 
-    const [accessToken, expiresAt] = useUserStore((state) => [
-        state.session?.access_token,
-        state.session?.expires_at,
-    ]);
+    const [accessToken, expiresAt, refreshToken] = useUserStore(
+        useShallow((state) => [
+            state.session?.access_token,
+            state.session?.expires_at,
+            state.session?.refresh_token,
+        ])
+    );
 
     const gqlClient = useMemo(() => {
         return new Client({
@@ -60,14 +67,20 @@ function UrqlConfigProvider({ children }: BaseComponentProps) {
                             return operation;
                         },
                         willAuthError() {
-                            if (expiresAt && accessToken) {
-                                return (
-                                    DateTime.now() >=
-                                    DateTime.fromSeconds(expiresAt)
-                                );
+                            // Really overkill to check for the refresh token as
+                            //  it is not fully required. However, it feels like
+                            //  if _anything_ is missing we should go ahead and
+                            //  make sure we're good with access.
+                            if (!expiresAt || !accessToken || !refreshToken) {
+                                return true;
                             }
 
-                            return true;
+                            return (
+                                DateTime.now() >=
+                                DateTime.fromSeconds(expiresAt).minus({
+                                    minutes: 1, // Expire it just a bit early
+                                })
+                            );
                         },
                         didAuthError(error) {
                             if (
@@ -83,14 +96,41 @@ function UrqlConfigProvider({ children }: BaseComponentProps) {
                             );
                         },
                         async refreshAuth() {
-                            return forceUserToSignOut('gql');
+                            // Only care about failures here.
+                            //  The data returned is the new session. However, we will consume
+                            //  that with `onAuthStateChange` in `src/context/User/index.tsx`
+                            const { error } =
+                                await supabaseClient.auth.refreshSession(
+                                    refreshToken
+                                        ? {
+                                              refresh_token: refreshToken,
+                                          }
+                                        : undefined
+                                );
+
+                            logRocketEvent('Auth', {
+                                refreshFailed: Boolean(error),
+                                refreshStatus: error?.message ?? 'success',
+                            });
+
+                            if (error) {
+                                return forceUserToSignOut('gql');
+                            }
+
+                            return Promise.resolve();
                         },
                     };
                 }),
                 fetchExchange,
             ],
         });
-    }, [accessToken, checkIfAuthInvalid, expiresAt, forceUserToSignOut]);
+    }, [
+        accessToken,
+        checkIfAuthInvalid,
+        expiresAt,
+        forceUserToSignOut,
+        refreshToken,
+    ]);
 
     return <Provider value={gqlClient}>{children}</Provider>;
 }
