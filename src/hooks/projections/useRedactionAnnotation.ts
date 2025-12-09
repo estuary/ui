39e0a59acd8e997d3e4641eae3fc7640d46960ae
Kiahna Tucker <tucker.kiahna@gmail.com>
2025-12-09 11:44:17 -0500

    Shorten useRedactionAnnotation error reasons in LR events

--- src/hooks/projections/useRedactionAnnotation.ts
+++ src/hooks/projections/useRedactionAnnotation.ts
@@ -36,7 +36,7 @@ export const useRedactionAnnotation = () => {
                     errored: true,
                     operation: 'redact',
                     pointer,
-                    reason: 'essential client collection data missing',
+                    reason: 'collection-data-missing',
                     strategy,
                 });
 
@@ -55,7 +55,7 @@ export const useRedactionAnnotation = () => {
                     errored: true,
                     operation: 'redact',
                     pointer,
-                    reason: 'failed to recursively set redaction annotation',
+                    reason: 'annotation-definition-failed',
                     strategy,
                 });
 
@@ -73,7 +73,7 @@ export const useRedactionAnnotation = () => {
                     errored: true,
                     operation: 'redact',
                     pointer,
-                    reason: 'failed to update server',
+                    reason: 'server-update-failed',
                     strategy,
                 });
 
