ea44a3a8e0a4aab63d029a0384ecf0f4abd3047f
Travis Jenkins <travis@estuary.dev>
2025-12-10 15:34:00 -0500

    Tweaking content and showing status

--- src/components/tables/RowActions/Shared/Progress/GroupedProgress.tsx
+++ src/components/tables/RowActions/Shared/Progress/GroupedProgress.tsx
@@ -1,14 +1,15 @@
+import type { AlertColor } from '@mui/material';
 import type { GroupedProgressProps } from 'src/components/tables/RowActions/Shared/types';
 
 import { Stack, Typography } from '@mui/material';
 
 import { useIntl } from 'react-intl';
 
+import AlertBox from 'src/components/shared/AlertBox';
 import ChipList from 'src/components/shared/ChipList';
 import ErrorViewer from 'src/components/tables/RowActions/Shared/Progress/ErrorViewer';
 import LogViewer from 'src/components/tables/RowActions/Shared/Progress/LogViewer';
 import useRowActionProgress from 'src/components/tables/RowActions/Shared/Progress/useRowActionProgress';
-import { OutlinedChip } from 'src/styledComponents/chips/OutlinedChip';
 
 function GroupedProgress({
     name,
@@ -31,6 +32,15 @@ function GroupedProgress({
             runningIntlKey,
             successIntlKey,
         });
+
+    const severity: AlertColor =
+        color === 'success'
+            ? 'success'
+            : color === 'error'
+              ? 'error'
+              : color === 'warning'
+                ? 'warning'
+                : 'info';
 
     return (
         <Stack
@@ -58,17 +68,22 @@ function GroupedProgress({
                     <Stack direction="row" spacing={1}>
                         {active ? (
                             <Typography variant="h6" component="span">
-                                {name} {statusIndicator}
+                                {statusIndicator} {name}
                             </Typography>
                         ) : (
-                            <OutlinedChip
-                                disableCursor
-                                color={color}
-                                variant="outlined"
-                                label={intl.formatMessage({
+                            <AlertBox severity={severity} short>
+                                {intl.formatMessage({
                                     id: labelIntlKey,
                                 })}
-                            />
+                            </AlertBox>
+                            // <OutlinedChip
+                            //     disableCursor
+                            //     color={color}
+                            //     variant="outlined"
+                            //     label={intl.formatMessage({
+                            //         id: labelIntlKey,
+                            //     })}
+                            // />
                         )}
                     </Stack>
 
