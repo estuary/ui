812b8ca578bd11e315f50ff08910e89eac521c49
Greg Shear <greg@estuary.dev>
2025-12-11 13:29:40 -0500

    remove data plane table columns, split up CIDR blocks into to columns

--- src/components/tables/DataPlanes/Rows.tsx
+++ src/components/tables/DataPlanes/Rows.tsx
@@ -1,12 +1,20 @@
 import type { BaseDataPlaneQuery } from 'src/api/dataPlanes';
 
-import { Stack, TableCell, TableRow, useTheme } from '@mui/material';
+import { useMemo, useState } from 'react';
+
+import { Collapse, Stack, TableCell, TableRow, useTheme } from '@mui/material';
+
+import ExpandRowButton from '../cells/logs/ExpandRowButton';
+import { InfoCircle } from 'iconoir-react';
 
 import SingleLineCode from 'src/components/content/SingleLineCode';
+import TechnicalEmphasis from 'src/components/derivation/Create/TechnicalEmphasis';
 import CopyToClipboardButton from 'src/components/shared/buttons/CopyToClipboardButton';
 import CopyCidrBlocks from 'src/components/shared/CopyCidrBlocks';
 import DataPlane from 'src/components/shared/Entity/DataPlane';
 import { getEntityTableRowSx } from 'src/context/Theme';
+import { CollapsibleGroup } from 'src/forms/renderers/CollapsibleGroup';
+import useParseCidrBlocks from 'src/hooks/useParseCidrBlocks';
 import {
     formatDataPlaneName,
     generateDataPlaneOption,
@@ -24,20 +32,36 @@ interface RowProps {
 
 function Row({ row, rowSx }: RowProps) {
     const dataPlaneOption = generateDataPlaneOption(row);
+    const parseCidrBlocks = useParseCidrBlocks();
+
+    const [expanded, setExpanded] = useState(false);
+
+    const splitCidrBlocks = useMemo(
+        () => parseCidrBlocks(row.cidr_blocks),
+        [row.cidr_blocks, parseCidrBlocks]
+    );
 
     return (
-        <TableRow hover sx={rowSx}>
+        <TableRow
+            hover
+            sx={{
+                ...rowSx,
+                'cursor': 'pointer',
+                '& .info-icon': {
+                    opacity: 0,
+                    transition: 'opacity 0.15s ease-in-out',
+                },
+                '&:hover .info-icon': {
+                    opacity: 1,
+                },
+            }}
+            onClick={() => setExpanded(!expanded)}
+        >
             <TableCell>
-                {Boolean(dataPlaneOption.dataPlaneName) ? (
-                    <Stack
-                        direction="row"
-                        spacing={1}
-                        sx={{
-                            justifyContent: 'space-between',
-                            minWidth: 'fit-content',
-                            whiteSpace: 'nowrap',
-                        }}
-                    >
+                <Stack direction="row" alignItems="center" spacing={1}>
+                    <InfoCircle className="info-icon" fontSize={12} />
+
+                    {Boolean(dataPlaneOption.dataPlaneName) ? (
                         <DataPlane
                             dataPlaneName={dataPlaneOption.dataPlaneName}
                             formattedSuffix={formatDataPlaneName(
@@ -47,35 +71,14 @@ function Row({ row, rowSx }: RowProps) {
                             logoSize={30}
                             scope={dataPlaneOption.scope}
                         />
-                        <CopyToClipboardButton
-                            writeValue={dataPlaneOption.dataPlaneName.whole}
-                        />
-                    </Stack>
-                ) : null}
+                    ) : null}
+                </Stack>
             </TableCell>
             <TableCell>
-                {row.aws_iam_user_arn ? (
-                    <SingleLineCode compact value={row.aws_iam_user_arn} />
-                ) : null}
+                <TechnicalEmphasis>{splitCidrBlocks.ipv4}</TechnicalEmphasis>
             </TableCell>
             <TableCell>
-                {row.gcp_service_account_email ? (
-                    <SingleLineCode
-                        compact
-                        value={row.gcp_service_account_email}
-                    />
-                ) : null}
-            </TableCell>
-            <TableCell>
-                <CopyCidrBlocks cidrBlocks={row.cidr_blocks} />
-            </TableCell>
-            <TableCell>
-                {row.data_plane_fqdn ? (
-                    <SingleLineCode
-                        compact
-                        value={`${OPENID_HOST}/${row.data_plane_fqdn}`}
-                    />
-                ) : null}
+                <TechnicalEmphasis>{splitCidrBlocks.ipv6}</TechnicalEmphasis>
             </TableCell>
         </TableRow>
     );
--- src/components/tables/DataPlanes/shared.ts
+++ src/components/tables/DataPlanes/shared.ts
@@ -10,19 +10,11 @@ export const columns: TableColumns[] = [
         headerIntlKey: 'data.name',
     },
     {
-        field: 'aws_iam_user_arn',
-        headerIntlKey: 'data.awsIamUserArn',
+        field: null, // not sortable
+        headerIntlKey: 'data.cidr_ipv4',
     },
     {
-        field: 'gcp_service_account_email',
-        headerIntlKey: 'data.gcpServiceAccount',
-    },
-    {
-        field: 'cidr_blocks',
-        headerIntlKey: 'data.cidr',
-    },
-    {
-        field: 'data_plane_fqdn',
-        headerIntlKey: 'data.idProvider',
+        field: null, // not sortable
+        headerIntlKey: 'data.cidr_ipv6',
     },
 ];
