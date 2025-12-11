be0049e53b65a8263f885e71ed866c7a2c01a11e
Travis Jenkins <travis@estuary.dev>
2025-12-11 17:11:49 -0500

    Update src/lang/en-US/Authentication.ts
    Co-authored-by: Greg Shear <greg@estuary.dev>

--- src/lang/en-US/Authentication.ts
+++ src/lang/en-US/Authentication.ts
@@ -125,7 +125,7 @@ export const Authentication: Record<string, string> = {
     'tenant.origin.radio.other.label': `Other`,
 
     'tenant.grantDirective.header': `You've been invited to a ${CommonMessages['terms.tenant']}`,
-    'tenant.grantDirective.message': `You're invited to receive {grantedCapability} access to the following tenant :`,
+    'tenant.grantDirective.message': `You're invited to receive {grantedCapability} access to the following tenant:`,
 
     'tenant.grantDirective.error.header': `Unable to Provision Access`,
     'tenant.grantDirective.error.message': `A problem was encountered provisioning access to the requested tenant. The access link could have been single-use or revoked by an administrator of the tenant.`,
