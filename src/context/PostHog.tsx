145ced2550456d05c26c0b6290f72602bf887c9a
Travis Jenkins <travis@estuary.dev>
2026-01-14 09:53:03 -0500

    Removing comment as it probably makes more sense to just do a reverse proxy

--- src/context/PostHog.tsx
+++ src/context/PostHog.tsx
@@ -16,16 +16,6 @@ const postHogSettings = getPostHogSettings();
         api_host: postHogSettings.apiHost,
         defaults: '2025-05-24',
         cookieless_mode: 'on_reject',
-        // TODO (PostHog)
-        // https://posthog.com/docs/advanced/content-security-policy#supporting-nonce-directives
-        // prepare_external_dependency_script: (script) => {
-        //     script.nonce = '<your-nonce-value>';
-        //     return script;
-        // },
-        // prepare_external_dependency_stylesheet: (stylesheet) => {
-        //     stylesheet.nonce = '<your-nonce-value>';
-        //     return stylesheet;
-        // },
     });
 })();
 
