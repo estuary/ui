4b980559bb9f3673e0f4e01915cc4ae03ba2a6f5
Greg Shear <greg@estuary.dev>
2026-02-17 18:15:39 -0500

    formatting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: [
-        '@storybook/addon-a11y',
-        '@storybook/addon-docs',
-    ],
+    addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
     framework: '@storybook/react-vite',
 };
 
