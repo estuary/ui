08153b5d8239aca05b76a74e0ef91a43a0ed9860
Kiahna Tucker <tucker.kiahna@gmail.com>
2026-02-24 09:02:43 -0500

    Add comment

--- src/utils/entity-utils.ts
+++ src/utils/entity-utils.ts
@@ -9,6 +9,8 @@ import {
     specContainsDerivation,
 } from 'src/utils/misc-utils';
 
+// Allowing field selection validation to be retried a single time
+// in the event additional resources are not available initially.
 export const MAX_FIELD_SELECTION_VALIDATION_ATTEMPTS = 1;
 
 export const updateShardDisabled = (draftSpec: any, enabling: boolean) => {
