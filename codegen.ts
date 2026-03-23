802db91bcfc351d602140215032d087c03ea476a
Greg Shear <greg@estuary.dev>
2026-03-23 11:16:57 -0400

    remove dotenv, fix codegen config

---
+++ codegen.ts
@@ -0,0 +1,29 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+import { loadEnvFile } from 'node:process';
+
+loadEnvFile(process.env.LOCAL ? '.env.development.local' : '.env');
+
+const config: CodegenConfig = {
+    schema: process.env.VITE_GQL_URL,
+    documents: 'src/**/*.{ts,tsx}',
+    generates: {
+        './src/gql-types/schema.graphql': {
+            plugins: ['schema-ast'], // converts the json response from introspection query to a .graphql file
+        },
+        './src/gql-types/': {
+            preset: 'client',
+            presetConfig: {
+                // fragmentMasking hides fields outside of the component that defines the fragment
+                // - turning off is easier to work with, and it doesn't matter because we're not using any fragments yet
+                fragmentMasking: false,
+            },
+            config: {
+                // generates string unions instead of enums
+                enumsAsTypes: true,
+            },
+        },
+    },
+};
+
+export default config;
