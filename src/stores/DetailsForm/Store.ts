326faa2b53c7b9877d355f9690088ebdc355de5a
Travis Jenkins <travis@estuary.dev>
2026-04-07 16:29:23 -0400

    I think we are good to remove this - gonna test it out

--- src/stores/DetailsForm/Store.ts
+++ src/stores/DetailsForm/Store.ts
@@ -1,9 +1,4 @@
-import type {
-    DataPlaneOption,
-    Details,
-    DetailsFormState,
-} from 'src/stores/DetailsForm/types';
-import type { ConnectorVersionEvaluationOptions } from 'src/utils/connector-utils';
+import type { DetailsFormState } from 'src/stores/DetailsForm/types';
 import type { StoreApi } from 'zustand';
 import type { NamedSet } from 'zustand/middleware';
 
@@ -13,12 +8,6 @@ import { devtools } from 'zustand/middleware';
 import produce from 'immer';
 import { isEmpty } from 'lodash';
 
-import { getConnectors_detailsFormTestPage } from 'src/api/connectors';
-import { getLiveSpecs_detailsForm } from 'src/api/liveSpecsExt';
-import { GlobalSearchParams } from 'src/hooks/searchParams/useGlobalSearchParams';
-import { logRocketEvent } from 'src/services/shared';
-import { CustomEvents } from 'src/services/types';
-import { DATA_PLANE_SETTINGS } from 'src/settings/dataPlanes';
 import { initialDetails } from 'src/stores/DetailsForm/shared';
 import {
     fetchErrors,
@@ -31,79 +20,77 @@ import {
     getInitialHydrationData,
     getStoreWithHydrationSettings,
 } from 'src/stores/extensions/Hydration';
-import { getConnectorMetadata } from 'src/utils/connector-utils';
-import { defaultDataPlaneSuffix } from 'src/utils/env-utils';
 import { hasLength } from 'src/utils/misc-utils';
 import { devtoolsOptions } from 'src/utils/store-utils';
 import { NAME_RE } from 'src/validation';
 
 const STORE_KEY = 'Details Form';
 
-const getConnectorImage = async (
-    connectorId: string,
-    existingImageTag?: ConnectorVersionEvaluationOptions['existingImageTag']
-): Promise<Details['data']['connectorImage'] | null> => {
-    const { data, error } =
-        await getConnectors_detailsFormTestPage(connectorId);
+// const getConnectorImage = async (
+//     connectorId: string,
+//     existingImageTag?: ConnectorVersionEvaluationOptions['existingImageTag']
+// ): Promise<Details['data']['connectorImage'] | null> => {
+//     const { data, error } =
+//         await getConnectors_detailsFormTestPage(connectorId);
 
-    if (!error && data && data.length > 0) {
-        const connector = data[0];
+//     if (!error && data && data.length > 0) {
+//         const connector = data[0];
 
-        const options: ConnectorVersionEvaluationOptions | undefined =
-            existingImageTag ? { connectorId, existingImageTag } : undefined;
+//         const options: ConnectorVersionEvaluationOptions | undefined =
+//             existingImageTag ? { connectorId, existingImageTag } : undefined;
 
-        return getConnectorMetadata(connector, options);
-    }
+//         return getConnectorMetadata(connector, options);
+//     }
 
-    return null;
-};
+//     return null;
+// };
 
-const getDataPlane = (
-    dataPlaneOptions: DataPlaneOption[],
-    dataPlaneId: string | null
-): Details['data']['dataPlane'] | null => {
-    const selectedOption = dataPlaneId
-        ? dataPlaneOptions.find(({ id }) => id === dataPlaneId)
-        : undefined;
+// const getDataPlane = (
+//     dataPlaneOptions: DataPlaneOption[],
+//     dataPlaneId: string | null
+// ): Details['data']['dataPlane'] | null => {
+//     const selectedOption = dataPlaneId
+//         ? dataPlaneOptions.find(({ id }) => id === dataPlaneId)
+//         : undefined;
 
-    if (selectedOption) {
-        return selectedOption;
-    }
+//     if (selectedOption) {
+//         return selectedOption;
+//     }
 
-    // TODO (private data plane) - we need to add support for allowing tenants to configure their
-    //  preferred data plane.
+//     // TODO (private data plane) - we need to add support for allowing tenants to configure their
+//     //  preferred data plane.
 
-    // If we are not trying to find a specific data plane and there is only one option
-    //  and it is private we are pretty safe in prefilling that one.
-    if (
-        !dataPlaneId &&
-        dataPlaneOptions.length === 1 &&
-        dataPlaneOptions[0].dataPlaneName.whole.includes(
-            DATA_PLANE_SETTINGS.private.prefix
-        )
-    ) {
-        logRocketEvent(CustomEvents.DATA_PLANE_SELECTOR, {
-            defaultedPrivate: true,
-        });
-        return dataPlaneOptions[0];
-    }
+//     // If we are not trying to find a specific data plane and there is only one option
+//     //  and it is private we are pretty safe in prefilling that one.
+//     if (
+//         !dataPlaneId &&
+//         dataPlaneOptions.length === 1 &&
+//         dataPlaneOptions[0].dataPlaneName.whole.includes(
+//             DATA_PLANE_SETTINGS.private.prefix
+//         )
+//     ) {
+//         logRocketEvent(CustomEvents.DATA_PLANE_SELECTOR, {
+//             defaultedPrivate: true,
+//         });
+//         return dataPlaneOptions[0];
+//     }
 
-    // Try to find the default public data plane
-    const defaultOption = dataPlaneOptions.find(
-        ({ dataPlaneName }) =>
-            dataPlaneName.whole ===
-            `${DATA_PLANE_SETTINGS.public.prefix}${defaultDataPlaneSuffix}`
-    );
+//     // Try to find the default public data plane
+//     const defaultOption = dataPlaneOptions.find(
+//         ({ dataPlaneName }) =>
+//             dataPlaneName.whole ===
+//             `${DATA_PLANE_SETTINGS.public.prefix}${defaultDataPlaneSuffix}`
+//     );
 
-    if (dataPlaneId) {
-        logRocketEvent(CustomEvents.DATA_PLANE_SELECTOR, {
-            targetDataPlaneId: dataPlaneId,
-            defaultDataPlaneId: defaultOption?.id,
-        });
-    }
+//     if (dataPlaneId) {
+//         logRocketEvent(CustomEvents.DATA_PLANE_SELECTOR, {
+//             targetDataPlaneId: dataPlaneId,
+//             defaultDataPlaneId: defaultOption?.id,
+//         });
+//     }
 
-    return defaultOption ?? null;
-};
+//     return defaultOption ?? null;
+// };
 
 const getInitialStateData = (): Pick<
     DetailsFormState,
@@ -286,105 +273,106 @@ export const getInitialState = (
     },
 
     hydrateState: async (workflow, dataPlaneOptions): Promise<void> => {
-        const searchParams = new URLSearchParams(window.location.search);
-        const connectorId = searchParams.get(GlobalSearchParams.CONNECTOR_ID);
-        const dataPlaneId = searchParams.get(GlobalSearchParams.DATA_PLANE_ID);
-        const liveSpecId = searchParams.get(GlobalSearchParams.LIVE_SPEC_ID);
+        console.error('DETAILS FORM HYDRATE STATE WAS CALLED');
+        // const searchParams = new URLSearchParams(window.location.search);
+        // const connectorId = searchParams.get(GlobalSearchParams.CONNECTOR_ID);
+        // const dataPlaneId = searchParams.get(GlobalSearchParams.DATA_PLANE_ID);
+        // const liveSpecId = searchParams.get(GlobalSearchParams.LIVE_SPEC_ID);
 
-        const createWorkflow =
-            workflow === 'capture_create' ||
-            workflow === 'materialization_create';
+        // const createWorkflow =
+        //     workflow === 'capture_create' ||
+        //     workflow === 'materialization_create';
 
-        if (connectorId) {
-            if (createWorkflow) {
-                const connectorImage = await getConnectorImage(connectorId);
-                const dataPlane = getDataPlane(dataPlaneOptions, dataPlaneId);
+        // if (connectorId) {
+        //     if (createWorkflow) {
+        //         const connectorImage = await getConnectorImage(connectorId);
+        //         const dataPlane = getDataPlane(dataPlaneOptions, dataPlaneId);
 
-                if (connectorImage && dataPlane === null) {
-                    get().setDetails_connector(connectorImage);
+        //         if (connectorImage && dataPlane === null) {
+        //             get().setDetails_connector(connectorImage);
 
-                    const {
-                        data: { entityName },
-                        errors,
-                    } = initialDetails;
+        //             const {
+        //                 data: { entityName },
+        //                 errors,
+        //             } = initialDetails;
 
-                    get().setPreviousDetails({
-                        data: { entityName, connectorImage },
-                        errors,
-                    });
-                } else if (connectorImage && dataPlane !== null) {
-                    get().setDetails_connector(connectorImage);
+        //             get().setPreviousDetails({
+        //                 data: { entityName, connectorImage },
+        //                 errors,
+        //             });
+        //         } else if (connectorImage && dataPlane !== null) {
+        //             get().setDetails_connector(connectorImage);
 
-                    const {
-                        data: { entityName },
-                        errors,
-                    } = initialDetails;
+        //             const {
+        //                 data: { entityName },
+        //                 errors,
+        //             } = initialDetails;
 
-                    get().setDetails_dataPlane(dataPlane);
-                    get().setPreviousDetails({
-                        data: { entityName, connectorImage, dataPlane },
-                        errors,
-                    });
-                } else {
-                    get().setHydrationErrorsExist(true);
-                }
-            } else if (liveSpecId) {
-                const { data, error } =
-                    await getLiveSpecs_detailsForm(liveSpecId);
+        //             get().setDetails_dataPlane(dataPlane);
+        //             get().setPreviousDetails({
+        //                 data: { entityName, connectorImage, dataPlane },
+        //                 errors,
+        //             });
+        //         } else {
+        //             get().setHydrationErrorsExist(true);
+        //         }
+        //     } else if (liveSpecId) {
+        //         const { data, error } =
+        //             await getLiveSpecs_detailsForm(liveSpecId);
 
-                if (!error && data && data.length > 0) {
-                    const {
-                        catalog_name,
-                        connector_image_tag,
-                        data_plane_id,
-                    } = data[0];
+        //         if (!error && data && data.length > 0) {
+        //             const {
+        //                 catalog_name,
+        //                 connector_image_tag,
+        //                 data_plane_id,
+        //             } = data[0];
 
-                    const connectorImage = await getConnectorImage(
-                        connectorId,
-                        connector_image_tag
-                    );
+        //             const connectorImage = await getConnectorImage(
+        //                 connectorId,
+        //                 connector_image_tag
+        //             );
 
-                    const dataPlane = getDataPlane(
-                        dataPlaneOptions,
-                        data_plane_id
-                    );
+        //             const dataPlane = getDataPlane(
+        //                 dataPlaneOptions,
+        //                 data_plane_id
+        //             );
 
-                    if (connectorImage && dataPlane !== null) {
-                        const hydratedDetails: Details = {
-                            data: {
-                                entityName: catalog_name,
-                                connectorImage,
-                                dataPlane,
-                            },
-                        };
+        //             if (connectorImage && dataPlane !== null) {
+        //                 const hydratedDetails: Details = {
+        //                     data: {
+        //                         entityName: catalog_name,
+        //                         connectorImage,
+        //                         dataPlane,
+        //                     },
+        //                 };
 
-                        get().setDetails(hydratedDetails);
-                        get().setPreviousDetails(hydratedDetails);
-                    } else {
-                        get().setHydrationErrorsExist(true);
-                    }
-                } else {
-                    get().setHydrationErrorsExist(true);
-                }
-            } else if (workflow === 'test_json_forms') {
-                get().setDetails_connector({
-                    id: connectorId,
-                    iconPath: '',
-                    imageName: '',
-                    imagePath: '',
-                    imageTag: '',
-                    connectorId,
-                });
-                get().setHydrationErrorsExist(true);
-            }
-        } else {
-            logRocketEvent(CustomEvents.CONNECTOR_VERSION_MISSING);
-            // TODO (details hydration) should really show an error here
-            // get().setHydrationError(
-            //     'Unable to locate selected connector. If the issue persists, please contact support.'
-            // );
-            // get().setHydrationErrorsExist(true);
-        }
+        //                 get().setDetails(hydratedDetails);
+        //                 get().setPreviousDetails(hydratedDetails);
+        //             } else {
+        //                 get().setHydrationErrorsExist(true);
+        //             }
+        //         } else {
+        //             get().setHydrationErrorsExist(true);
+        //         }
+        //     } else if (workflow === 'test_json_forms') {
+        //         get().setDetails_connector({
+        //             id: connectorId,
+        //             iconPath: '',
+        //             imageName: '',
+        //             imagePath: '',
+        //             imageTag: '',
+        //             connectorId,
+        //         });
+        //         get().setHydrationErrorsExist(true);
+        //     }
+        // } else {
+        //     logRocketEvent(CustomEvents.CONNECTOR_VERSION_MISSING);
+        //     // TODO (details hydration) should really show an error here
+        //     // get().setHydrationError(
+        //     //     'Unable to locate selected connector. If the issue persists, please contact support.'
+        //     // );
+        //     // get().setHydrationErrorsExist(true);
+        // }
     },
 
     resetState: () => {
