4fd13708d67d6c474c9e5f637756c3132a833cdc
Travis Jenkins <travis@estuary.dev>
2026-04-21 12:42:45 -0400

    Showing the backfill warning even for individual bindings

--- src/components/editor/Bindings/Backfill/index.tsx
+++ src/components/editor/Bindings/Backfill/index.tsx
@@ -1,10 +1,8 @@
 import type { BackfillProps } from 'src/components/editor/Bindings/Backfill/types';
 
-import { FormattedMessage } from 'react-intl';
-
 import BackfillButton from 'src/components/editor/Bindings/Backfill/BackfillButton';
+import BackfillDescription from 'src/components/editor/Bindings/Backfill/BackfillDescription';
 import SectionWrapper from 'src/components/editor/Bindings/Backfill/SectionWrapper';
-import { useEntityType } from 'src/context/EntityContext';
 import { useEntityWorkflow_Editing } from 'src/context/Workflow';
 
 export default function Backfill({
@@ -12,7 +10,6 @@ export default function Backfill({
     collection,
     collectionEnabled,
 }: BackfillProps) {
-    const entityType = useEntityType();
     const isEdit = useEntityWorkflow_Editing();
 
     const showBackfillButton = isEdit && bindingIndex > -1 && collectionEnabled;
@@ -24,11 +21,7 @@ export default function Backfill({
         >
             <BackfillButton
                 bindingIndex={bindingIndex}
-                description={
-                    <FormattedMessage
-                        id={`workflows.collectionSelector.manualBackfill.message.${entityType}`}
-                    />
-                }
+                description={<BackfillDescription />}
             />
         </SectionWrapper>
     ) : null;
