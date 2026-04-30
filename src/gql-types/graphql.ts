ee599841f1e51f42bfed4e8187535ba9ee4d5bd0
Travis Jenkins <travis@estuary.dev>
2026-04-30 09:29:24 -0400

    codegen update

--- src/gql-types/graphql.ts
+++ src/gql-types/graphql.ts
@@ -301,6 +301,8 @@ export type Connector = {
    * spec that should be used when configuring newly created tasks.
    */
   defaultSpec?: Maybe<ConnectorSpec>;
+  /** A string that contains a list of connector details (latency, batch, etc.) */
+  detail?: Maybe<Scalars['String']['output']>;
   /** Link to an external site with more information about the endpoint */
   externalUrl: Scalars['String']['output'];
   /** Unique id of the connector */
@@ -309,14 +311,10 @@ export type Connector = {
   imageName: Scalars['String']['output'];
   /** The connector's logo image, represented as a URL per locale */
   logoUrl?: Maybe<Scalars['String']['output']>;
-  /** A longform description of this connector */
-  longDescription?: Maybe<Scalars['String']['output']>;
   /** The protocol of this connector (capture or materialization). */
   protocol?: Maybe<ConnectorProto>;
   /** Whether this connector should appear in a promoted position in connector listings */
   recommended: Scalars['Boolean']['output'];
-  /** Brief human readable description, at most a few sentences */
-  shortDescription?: Maybe<Scalars['String']['output']>;
   /** Look up the spec for a specific image tag of this connector. */
   spec?: Maybe<ConnectorSpec>;
   /** The title, a few words at most */
