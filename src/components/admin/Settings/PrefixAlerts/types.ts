acbad783fa879eb51cb65f83e420dda3c6b8c563
Kiahna Tucker <tucker.kiahna@gmail.com>
2026-07-06 07:45:01 -0400

    Keep MutableAlertSubscription local to types file

--- src/components/admin/Settings/PrefixAlerts/types.ts
+++ src/components/admin/Settings/PrefixAlerts/types.ts
@@ -62,7 +62,7 @@ export interface GlobalSettingProps<T> {
     targetSetting: AlertConfigKeys;
 }
 
-export interface MutableAlertSubscription extends ReducedAlertSubscription {
+interface MutableAlertSubscription extends ReducedAlertSubscription {
     id: string;
     viewing: boolean;
     deleted?: boolean;
