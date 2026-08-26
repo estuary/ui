6c45b9a5bb846d801d2aac3826b0b0648334374b
Adrian Cortez <adrian@estuary.dev>
2026-08-26 12:18:34 -0400

    convert DataPlanesTable to use paginated gql query

--- src/components/tables/DataPlanes/index.tsx
+++ src/components/tables/DataPlanes/index.tsx
@@ -1,91 +1,176 @@
-import { useMemo } from 'react';
+import type { SxProps } from '@mui/material';
+import type { TableState } from 'src/types';
+import type { CombinedError } from 'urql';
 
-import { getDataPlanesForTable } from 'src/api/dataPlanes';
-import Rows from 'src/components/tables/DataPlanes/Rows';
+import { useCallback, useEffect, useMemo } from 'react';
+
 import {
-    columns,
-    selectableTableStoreName,
-} from 'src/components/tables/DataPlanes/shared';
+    Box,
+    Table,
+    TableContainer,
+    TableFooter,
+    TablePagination,
+    TableRow,
+    Toolbar,
+} from '@mui/material';
+
+import Rows from 'src/components/tables/DataPlanes/Rows';
+import { columns } from 'src/components/tables/DataPlanes/shared';
 import ToggleDataPlaneScope from 'src/components/tables/DataPlanes/ToggleDataPlaneScope';
-import EntityTable from 'src/components/tables/EntityTable';
+import EntityTableBody from 'src/components/tables/EntityTable/TableBody';
+import EntityTableHeader from 'src/components/tables/EntityTable/TableHeader';
 import { useDataPlaneScope } from 'src/context/DataPlaneScopeContext';
+import { useDataPlanesQuery } from 'src/hooks/dataPlanes/useDataPlanes';
+import { useCursorPagination } from 'src/hooks/useCursorPagination';
 import { DATA_PLANE_SETTINGS } from 'src/settings/dataPlanes';
 import { TablePrefixes, useTableState } from 'src/stores/Tables/hooks';
-import TableHydrator from 'src/stores/Tables/Hydrator';
 import { useTenantStore } from 'src/stores/Tenant';
+import { TableStatuses } from 'src/types';
+
+const toolbarStyle: SxProps = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+};
+
+const tableContainerStyle: SxProps = {
+    mb: 2,
+};
+
+const tableStyle: SxProps = {
+    minWidth: 350,
+};
+
+function getTableStatus<T>(data: T[], loading: boolean, error?: CombinedError) {
+    if (loading) {
+        return TableStatuses.LOADING;
+    }
+
+    if (error) {
+        return error.networkError
+            ? TableStatuses.NETWORK_FAILED
+            : TableStatuses.TECHNICAL_DIFFICULTIES;
+    }
+
+    return data.length > 0
+        ? TableStatuses.DATA_FETCHED
+        : TableStatuses.NO_EXISTING_DATA;
+}
 
 function DataPlanesTable() {
-    const {
-        pagination,
-        setPagination,
-        rowsPerPage,
-        setRowsPerPage,
-        searchQuery,
-        setSearchQuery,
-        sortDirection,
-        setSortDirection,
-        columnToSort,
-        setColumnToSort,
-    } = useTableState(TablePrefixes.dataPlanes, 'data_plane_name', 'asc');
+    const { rowsPerPage, setRowsPerPage } = useTableState(
+        TablePrefixes.dataPlanes,
+        'name',
+        'asc'
+    );
+    const { currentPage, cursor, goToPage, onPageChange } =
+        useCursorPagination();
 
     const selectedTenant = useTenantStore((state) => state.selectedTenant);
-    const { dataPlaneScope } = useDataPlaneScope();
+    const { dataPlaneScope, setScope } = useDataPlaneScope();
+    const { dataPlanes, fetching, error, pageInfo } = useDataPlanesQuery({
+        filter: {
+            tenant: selectedTenant,
+            public: {
+                eq: dataPlaneScope === 'public',
+            },
+        },
+        limit: rowsPerPage,
+        cursor,
+    });
 
-    const dataPlaneName = useMemo(() => {
-        if (!selectedTenant) {
-            return null;
-        }
+    const tableState = useMemo<TableState>(() => {
+        const status = getTableStatus(dataPlanes, fetching, error);
+        return { status };
+    }, [error, fetching, dataPlanes]);
 
-        return `${DATA_PLANE_SETTINGS[dataPlaneScope].prefix}${
-            dataPlaneScope === 'private' ? selectedTenant : ''
-        }`;
-    }, [dataPlaneScope, selectedTenant]);
+    const noExistingDataContentIds = useMemo(() => {
+        return DATA_PLANE_SETTINGS[dataPlaneScope].table
+            .noExistingDataContentIds;
+    }, [dataPlaneScope]);
 
-    const query = useMemo(() => {
-        return dataPlaneName
-            ? getDataPlanesForTable(dataPlaneName, pagination, searchQuery, [
-                  {
-                      col: columnToSort,
-                      direction: sortDirection,
-                  },
-              ])
-            : null;
-    }, [columnToSort, dataPlaneName, pagination, searchQuery, sortDirection]);
+    const rows = useMemo(() => {
+        return dataPlanes.length > 0 ? <Rows data={dataPlanes} /> : null;
+    }, [dataPlanes]);
+
+    const slotProps = useMemo(() => {
+        const previousButtonDisabled = !pageInfo?.hasPreviousPage;
+        const nextButtonDisabled = !pageInfo?.hasNextPage;
+        return {
+            actions: {
+                previousButton: {
+                    disabled: previousButtonDisabled,
+                },
+                nextButton: {
+                    disabled: nextButtonDisabled,
+                },
+            },
+        };
+    }, [pageInfo?.hasNextPage, pageInfo?.hasPreviousPage]);
+
+    const handlePageChange = useCallback(
+        (event: any, newPage: number) => {
+            onPageChange(event, newPage, pageInfo?.endCursor);
+        },
+        [onPageChange, pageInfo?.endCursor]
+    );
+
+    const handleRowsPerPageChange = useCallback(
+        (event: any) => {
+            const newLimit = parseInt(event.target.value, 10);
+            onPageChange(event, 0, pageInfo?.endCursor);
+            setRowsPerPage(newLimit);
+        },
+        [onPageChange, setRowsPerPage, pageInfo?.endCursor]
+    );
+
+    const labelDisplayedRows = useCallback(
+        ({ from }: { from: number }) => {
+            return dataPlanes.length > 0
+                ? `${from} – ${from + dataPlanes.length - 1}`
+                : '';
+        },
+        [dataPlanes.length]
+    );
 
     return (
-        <TableHydrator
-            disableQueryParamHack
-            query={query}
-            selectableTableStoreName={selectableTableStoreName}
-        >
-            <EntityTable
-                noExistingDataContentIds={
-                    DATA_PLANE_SETTINGS[dataPlaneScope].table
-                        .noExistingDataContentIds
-                }
-                columns={columns}
-                renderTableRows={(data) => <Rows data={data} />}
-                rowsPerPage={rowsPerPage}
-                setRowsPerPage={setRowsPerPage}
-                pagination={pagination}
-                setPagination={setPagination}
-                searchQuery={searchQuery}
-                setSearchQuery={setSearchQuery}
-                sortDirection={sortDirection}
-                setSortDirection={setSortDirection}
-                columnToSort={columnToSort}
-                setColumnToSort={setColumnToSort}
-                header={null}
-                filterLabel={
-                    DATA_PLANE_SETTINGS[dataPlaneScope].table.filterIntlKey
-                }
-                selectableTableStoreName={selectableTableStoreName}
-                tableAriaLabelKey="admin.dataPlanes.table.aria.label"
-                showToolbar
-                hideFilter
-                toolbar={<ToggleDataPlaneScope />}
-            />
-        </TableHydrator>
+        <Box data-public>
+            <Toolbar sx={toolbarStyle} disableGutters>
+                <ToggleDataPlaneScope />
+            </Toolbar>
+
+            <TableContainer component={Box} sx={tableContainerStyle}>
+                <Table
+                    sx={tableStyle}
+                    size="small"
+                    aria-label="Data Planes Table"
+                >
+                    <EntityTableHeader columns={columns} />
+
+                    <EntityTableBody
+                        rows={rows}
+                        columns={columns}
+                        loading={fetching}
+                        tableState={tableState}
+                        noExistingDataContentIds={noExistingDataContentIds}
+                    />
+
+                    <TableFooter>
+                        <TableRow>
+                            <TablePagination
+                                count={-1}
+                                page={currentPage}
+                                rowsPerPage={rowsPerPage}
+                                onPageChange={handlePageChange}
+                                onRowsPerPageChange={handleRowsPerPageChange}
+                                labelDisplayedRows={labelDisplayedRows}
+                                slotProps={slotProps}
+                            />
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+            </TableContainer>
+        </Box>
     );
 }
 
